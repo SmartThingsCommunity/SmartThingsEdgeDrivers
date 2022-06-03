8dd68ad95223a88e0bc6247c6e475b884c768a37
sbkimaqara <sbkim@aqara.kr>
2022-06-03 11:28:16 +0900

    Aqara zigbee switch

--- drivers/SmartThings/zigbee-switch/src/init.lua
+++ drivers/SmartThings/zigbee-switch/src/init.lua
@@ -78,6 +78,7 @@ local zigbee_switch_driver_template = {
     capabilities.motionSensor
   },
   sub_drivers = {
+    require("aqara"),
     require("ezex"),
     require("rexense"),
     require("sinope"),
---
+++ drivers/SmartThings/zigbee-switch/src/test/test_aqara_switch.lua
@@ -0,0 +1,76 @@
+-- Copyright 2022 SmartThings
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+local test = require "integration_test"
+local clusters = require "st.zigbee.zcl.clusters"
+local zigbee_test_utils = require "integration_test.zigbee_test_utils"
+local t_utils = require "integration_test.utils"
+
+local OnOff = clusters.OnOff
+
+local mock_device = test.mock_device.build_test_zigbee_device(
+  {
+    profile = t_utils.get_profile_definition("basic-switch.yml"),
+    zigbee_endpoints = {
+      [1] = {
+        id = 1,
+        manufacturer = "LUMI",
+        model = "lumi.switch.b1laus01",
+        server_clusters = {0x0000, 0x0003, 0x0006}
+      }
+    }
+  }
+)
+
+zigbee_test_utils.prepare_zigbee_env_info()
+local function test_init()
+  test.mock_device.add_test_device(mock_device)
+  zigbee_test_utils.init_noop_health_check_timer()
+end
+
+test.set_test_init_function(test_init)
+
+test.register_message_test(
+  "Capability command On should be handled",
+  {
+    {
+      channel = "capability",
+      direction = "receive",
+      message = { mock_device.id, { capability = "switch", component = "main", command = "on", args = { } } }
+    },
+    {
+      channel = "zigbee",
+      direction = "send",
+      message = { mock_device.id, OnOff.server.commands.On(mock_device) }
+    }
+  }
+)
+
+test.register_message_test(
+  "Capability command Off should be handled",
+  {
+    {
+      channel = "capability",
+      direction = "receive",
+      message = { mock_device.id, { capability = "switch", component = "main", command = "off", args = { } } }
+    },
+    {
+      channel = "zigbee",
+      direction = "send",
+      message = { mock_device.id, OnOff.server.commands.Off(mock_device) }
+    }
+  }
+)
+
+test.run_registered_tests()
