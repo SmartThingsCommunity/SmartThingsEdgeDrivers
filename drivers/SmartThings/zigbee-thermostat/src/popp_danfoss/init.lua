986239a552718deefb00182eb5055deaf8f541e9
Nikita <n.kuchta@samsung.com>
2022-06-14 12:56:16 +0200

    fix the way voltage set is handling and delete thermostat_heating_setpoint_handler

--- drivers/SmartThings/zigbee-thermostat/src/popp_danfoss/init.lua
+++ drivers/SmartThings/zigbee-thermostat/src/popp_danfoss/init.lua
@@ -1,14 +1,8 @@
 local clusters = require "st.zigbee.zcl.clusters"
 local capabilities = require "st.capabilities"
-local utils = require "st.utils"
-local Thermostat = clusters.Thermostat
+local battery_defaults = require "st.zigbee.defaults.battery_defaults"
 local PowerConfiguration = clusters.PowerConfiguration
-local PowerSource = capabilities.powerSource
-local Battery = capabilities.battery
 local ThermostatMode = capabilities.thermostatMode
-
-local BAT_MIN = 24.0
-local BAT_MAX = 32.0
 
 local POPP_DANFOSS_THERMOSTAT_FINGERPRINTS = {
   { mfr = "D5X84YU", model = "eT093WRO" },
@@ -24,26 +18,7 @@ local is_popp_danfoss_thermostat = function(opts, driver, device)
   return false
 end
 
-local thermostat_heating_setpoint_handler = function(driver, device, value)
-  device:emit_event(capabilities.thermostatHeatingSetpoint.heatingSetpoint({value = value.value/100.0, unit = "C"}))
-end
-
-local battery_voltage_handler = function(driver, device, battery_voltage)
-  if (battery_voltage.value == 0) then -- this means we're plugged in
-    device:emit_event(PowerSource.powerSource.mains())
-    device:emit_event(Battery.battery(100))
-  else
-    local perc_value = utils.round((battery_voltage.value - BAT_MIN)/(BAT_MAX - BAT_MIN) * 100)
-    device:emit_event(Battery.battery(utils.clamp_value(perc_value, 0, 100)))
-  end
-end
-
 local do_configure = function(self, device)
-  device:configure()
-  device:send(PowerConfiguration.attributes.BatteryVoltage:configure_reporting(device, 30, 21600, 1))
-end
-
-local info_changed = function(self, device)
   device:emit_event(ThermostatMode.supportedThermostatModes({"heat"}))
 end
 
@@ -51,17 +26,14 @@ local popp_danfoss_thermostat = {
   NAME = "POPP Danfoss Thermostat Handler",
   zigbee_handlers = {
     attr = {
-      [Thermostat.ID] = {
-        [Thermostat.attributes.OccupiedHeatingSetpoint.ID] = thermostat_heating_setpoint_handler
-      },
       [PowerConfiguration.ID] = {
-        [PowerConfiguration.attributes.BatteryVoltage.ID] = battery_voltage_handler
+        [PowerConfiguration.attributes.BatteryVoltage.ID] = battery_defaults.battery_volt_attr_handler
       }
     }
   },
   lifecycle_handlers = {
     doConfigure = do_configure,
-    infoChanged = info_changed
+    init = battery_defaults.build_linear_voltage_init(2.4, 3.2)
   },
   can_handle = is_popp_danfoss_thermostat
 }
