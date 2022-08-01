d61ca150fd4c18ffda4977abec2e25b2e0f90dce
dsobierajsk <d.sobierajsk@partner.samsung.com>
2022-08-01 15:40:10 +0200

    [CHAD-8538] Ported additional fingerprint of the 'Ikea Button DTH' for the 'eWeLink WB01'

---
+++ drivers/SmartThings/zigbee-button/src/ewelink/init.lua
@@ -0,0 +1,57 @@
+-- Copyright 2022 SmartThings
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+local capabilities = require "st.capabilities"
+local clusters = require "st.zigbee.zcl.clusters"
+
+local OnOff = clusters.OnOff
+local button = capabilities.button.button
+
+local EWELINK_BUTTON_FINGERPRINTS = {
+  { mfr = "eWeLink", model = "WB01" }
+}
+
+local function can_handle_ewelink_button(opts, driver, device, ...)
+  for _, fingerprint in ipairs(EWELINK_BUTTON_FINGERPRINTS) do
+    if device:get_manufacturer() == fingerprint.mfr and device:get_model() == fingerprint.model then
+      return true
+    end
+  end
+  return false
+end
+
+local function button_handler(event)
+  return function(driver, device, value, zb_rx)
+    device:emit_event(event)
+  end
+end
+
+local ewelink_button = {
+  NAME = "eWeLink Button",
+  zigbee_handlers = {
+    cluster = {
+      [OnOff.ID] = {
+        [OnOff.server.commands.On.ID] = button_handler(button.double()),
+        [OnOff.server.commands.Off.ID] = button_handler(button.held()),
+        [OnOff.server.commands.OffWithEffect.ID] = button_handler(button.pushed()),
+        [OnOff.server.commands.OnWithRecallGlobalScene.ID] = button_handler(button.pushed()),
+        [OnOff.server.commands.OnWithTimedOff.ID] = button_handler(button.pushed()),
+        [OnOff.server.commands.Toggle.ID] = button_handler(button.pushed())
+      }
+    }
+  },
+  can_handle = can_handle_ewelink_button
+}
+
+return ewelink_button
