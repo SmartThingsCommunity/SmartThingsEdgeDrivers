48aa93899555fd85a0b5ca2c541620ff221ee27d
sbkimaqara <sbkim@aqara.kr>
2022-09-19 13:44:40 +0900

    Removed same configurations

--- drivers/SmartThings/zigbee-humidity-sensor/src/aqara/init.lua
+++ drivers/SmartThings/zigbee-humidity-sensor/src/aqara/init.lua
@@ -12,22 +12,6 @@ local FINGERPRINTS = {
 
 local configuration = {
   {
-    cluster = TemperatureMeasurement.ID,
-    attribute = TemperatureMeasurement.attributes.MeasuredValue.ID,
-    minimum_interval = 30,
-    maximum_interval = 300,
-    data_type = TemperatureMeasurement.attributes.MeasuredValue.base_type,
-    reportable_change = 1
-  },
-  {
-    cluster = RelativeHumidity.ID,
-    attribute = RelativeHumidity.attributes.MeasuredValue.ID,
-    minimum_interval = 30,
-    maximum_interval = 3600,
-    data_type = RelativeHumidity.attributes.MeasuredValue.base_type,
-    reportable_change = 1
-  },
-  {
     cluster = PowerConfiguration.ID,
     attribute = PowerConfiguration.attributes.BatteryVoltage.ID,
     minimum_interval = 30,
