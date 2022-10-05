9cca7f0892d951b8b056de6934ffd47283a37fd7
Nikita <n.kuchta@samsung.com>
2022-10-05 10:45:00 +0200

    Revert "Revert "parent/child zigbee_dual_metering_switch""
    This reverts commit 24430a20c44dc1b797071aaf7f3c3397ade27763.

--- drivers/SmartThings/zigbee-switch/src/zigbee-dual-metering-switch/init.lua
+++ drivers/SmartThings/zigbee-switch/src/zigbee-dual-metering-switch/init.lua
@@ -1,57 +1,47 @@
--- Copyright 2022 SmartThings
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---     http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
+local capabilities = require "st.capabilities"
+local ZigbeeDriver = require "st.zigbee"
+local defaults = require "st.zigbee.defaults"
+local device_lib = require "st.device"
 
-local ZIGBEE_DUAL_METERING_SWITCH_FINGERPRINT = {
-  {mfr = "Aurora", model = "DoubleSocket50AU"}
-}
-
-local function can_handle_zigbee_dual_metering_switch(opts, driver, device, ...)
-  for _, fingerprint in ipairs(ZIGBEE_DUAL_METERING_SWITCH_FINGERPRINT) do
-    if device:get_manufacturer() == fingerprint.mfr and device:get_model() == fingerprint.model then
-      return true
+local function added(driver, device, event)
+  -- Only create children for the actual Zigbee device and not the children
+  if device.network_type == device_lib.NETWORK_TYPE_ZIGBEE then
+    for i = 2,5,1 do
+      local name = string.format("%s outlet %d", device.label, i)
+      local metadata = {
+        type = "EDGE_CHILD",
+        label = name,
+        profile = "switch-power-2",
+        parent_device_id = device.id,
+        parent_assigned_child_key = string.format("%02X", i),
+        vendor_provided_label = name,
+      }
+      driver:try_create_device(metadata)
     end
   end
-  return false
 end
 
-local function endpoint_to_component(device, endpoint)
-  if endpoint == 2 then
-    return "switch1"
-  else
-    return "main"
+local function find_child(parent, ep_id)
+  return parent:get_child_by_parent_assigned_key(string.format("%02X", ep_id))
+end
+
+local function init(driver, device, event)
+  if device.network_type == device_lib.NETWORK_TYPE_ZIGBEE then
+    device:set_find_child(find_child)
   end
-end
-
-local function component_to_endpoint(device, component)
-  if component == "switch1" then
-    return 2
-  else
-    return 1
-  end
-end
-
-local function map_components(self, device)
-  device:set_endpoint_to_component_fn(endpoint_to_component)
-  device:set_component_to_endpoint_fn(component_to_endpoint)
 end
 
 local zigbee_dual_metering_switch = {
-  NAME = "zigbee dual metering switch",
-  lifecycle_handlers = {
-    init = map_components
+  supported_capabilities = {
+    capabilities.switch,
+    capabilities.powerMeter
   },
-  can_handle = can_handle_zigbee_dual_metering_switch
+  lifecycle_handlers = {
+    added = added,
+    init =  init,
+  },
 }
 
-return zigbee_dual_metering_switch
+defaults.register_for_default_handlers(zigbee_dual_metering_switch, zigbee_dual_metering_switch.supported_capabilities)
+local zigbee_light_switch = ZigbeeDriver("zigbee_dual_metering_switch_child_device", zigbee_dual_metering_switch)
+zigbee_light_switch:run()
