742482ec4f1f025bb44d8d3ad036b02e69ed539f
Nikita <n.kuchta@samsung.com>
2022-10-05 10:01:42 +0200

    re-port to parent/child: zigbee multi switch power

---
+++ drivers/SmartThings/zigbee-dual-metering-switch/src/test/test_dual_metering_switch.lua
@@ -0,0 +1,162 @@
+local test = require "integration_test"
+local clusters = require "st.zigbee.zcl.clusters"
+local OnOff = clusters.OnOff
+local capabilities = require "st.capabilities"
+local zigbee_test_utils = require "integration_test.zigbee_test_utils"
+local t_utils = require "integration_test.utils"
+
+local profile = t_utils.get_profile_definition("switch-power-parent-child.yml")
+
+local mock_parent_device = test.mock_device.build_test_zigbee_device(
+  {
+    profile = profile,
+    zigbee_endpoints = {
+      [1] = {
+        id = 1,
+        manufacturer = "Aurora",
+        model = "DoubleSocket50AU",
+        server_clusters = {0x0019}
+      },
+      [2] = {
+        id = 2,
+        manufacturer = "Aurora",
+        model = "DoubleSocket50AU",
+        server_clusters = {0x0019}
+      }
+    },
+    fingerprinted_endpoint_id = 0x01
+  }
+)
+
+local mock_first_child_device = test.mock_device.build_test_child_device({
+  profile = profile,
+  device_network_id = string.format("%04X:%02X", mock_parent_device:get_short_address(), 1),
+  parent_device_id = mock_parent_device.id,
+  parent_assigned_child_key = string.format("%02X", 1)
+})
+
+local mock_second_child_device = test.mock_device.build_test_child_device({
+  profile = profile,
+  device_network_id = string.format("%04X:%02X", mock_parent_device:get_short_address(), 2),
+  parent_device_id = mock_parent_device.id,
+  parent_assigned_child_key = string.format("%02X", 2)
+})
+
+zigbee_test_utils.prepare_zigbee_env_info()
+
+local function test_init()
+  test.mock_device.add_test_device(mock_parent_device)
+  test.mock_device.add_test_device(mock_first_child_device)
+  test.mock_device.add_test_device(mock_second_child_device)
+  zigbee_test_utils.init_noop_health_check_timer()
+end
+
+test.set_test_init_function(test_init)
+
+test.register_message_test(
+  "Reported on off status should be handled: on ep 1",
+  {
+    {
+      channel = "zigbee",
+      direction = "receive",
+      message = { mock_parent_device.id, OnOff.attributes.OnOff:build_test_attr_report(mock_parent_device,
+        true):from_endpoint(0x01) }
+    },
+    {
+      channel = "capability",
+      direction = "send",
+      message = mock_first_child_device:generate_test_message("main",  capabilities.switch.switch.on())
+    }
+  }
+)
+
+test.register_message_test(
+  "Reported on off status should be handled: on ep 2",
+  {
+    {
+      channel = "zigbee",
+      direction = "receive",
+      message = { mock_parent_device.id, OnOff.attributes.OnOff:build_test_attr_report(mock_parent_device,
+        true):from_endpoint(0x02) }
+    },
+    {
+      channel = "capability",
+      direction = "send",
+      message = mock_second_child_device:generate_test_message("main",  capabilities.switch.switch.on())
+    }
+  }
+)
+
+test.register_message_test(
+  "Reported on off status should be handled: off ep 1",
+  {
+    {
+      channel = "zigbee",
+      direction = "receive",
+      message = { mock_parent_device.id, OnOff.attributes.OnOff:build_test_attr_report(mock_parent_device,
+        false):from_endpoint(0x01) }
+    },
+    {
+      channel = "capability",
+      direction = "send",
+      message = mock_first_child_device:generate_test_message("main",  capabilities.switch.switch.off())
+    }
+  }
+)
+
+test.register_message_test(
+  "Reported on off status should be handled: off ep 2",
+  {
+    {
+      channel = "zigbee",
+      direction = "receive",
+      message = { mock_parent_device.id, OnOff.attributes.OnOff:build_test_attr_report(mock_parent_device,
+        false):from_endpoint(0x02) }
+    },
+    {
+      channel = "capability",
+      direction = "send",
+      message = mock_second_child_device:generate_test_message("main",  capabilities.switch.switch.off())
+    }
+  }
+)
+
+test.register_message_test(
+  "ActivePower Report should be handled: Sensor value is in W, capability attribute value is in W, ep 1",
+  {
+    {
+      channel = "zigbee",
+      direction = "receive",
+      message = {
+        mock_parent_device.id,
+        ElectricalMeasurement.attributes.ActivePower:build_test_attr_report(mock_parent_device,27):from_endpoint(0x01)
+      }
+    },
+    {
+      channel = "capability",
+      direction = "send",
+      message = mock_first_child_device:generate_test_message("main",  capabilities.powerMeter.power({ value = 27.0, unit = "W" }))
+    }
+  }
+)
+
+test.register_message_test(
+  "ActivePower Report should be handled: Sensor value is in W, capability attribute value is in W, ep 2",
+  {
+    {
+      channel = "zigbee",
+      direction = "receive",
+      message = {
+        mock_parent_device.id,
+        ElectricalMeasurement.attributes.ActivePower:build_test_attr_report(mock_parent_device,27):from_endpoint(0x02)
+      }
+    },
+    {
+      channel = "capability",
+      direction = "send",
+      message = mock_second_child_device:generate_test_message("main",  capabilities.powerMeter.power({ value = 27.0, unit = "W" }))
+    }
+  }
+)
+
+test.run_registered_tests()
