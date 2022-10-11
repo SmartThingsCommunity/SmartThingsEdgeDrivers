1eaa3fb2acd90a34cd2592dcf74fbf2afc1a85a9
greens <steven.green@smartthings.com>
2022-10-11 13:14:57 -0500

    Release matter drivers

---
+++ drivers/SmartThings/matter-switch/src/init.lua
@@ -0,0 +1,294 @@
+-- Copyright 2022 SmartThings
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+local capabilities = require "st.capabilities"
+local log = require "log"
+local clusters = require "st.matter.clusters"
+-- TODO remove require hack from sdk renames
+local ColorTemperatureAttribute = pcall(function() return clusters.ColorControl.attributes.ColorTemperature end) and
+                                  clusters.ColorControl.attributes.ColorTemperature or
+                                  clusters.ColorControl.attributes.ColorTemperatureMireds
+local MatterDriver = require "st.matter.driver"
+local utils = require "st.utils"
+
+local MOST_RECENT_TEMP = "mostRecentTemp"
+local RECEIVED_X = "receivedX"
+local RECEIVED_Y = "receivedY"
+local HUESAT_SUPPORT = "huesatSupport"
+local CONVERSION_CONSTANT = 1000000
+
+local function convert_huesat_st_to_matter(val)
+  return math.floor((val * 0xFE) / 100.0 + 0.5)
+end
+
+local function component_to_endpoint(device, component_id)
+  local ep_num = component_id:match("switch(%d)")
+  return ep_num and tonumber(ep_num) or device.MATTER_DEFAULT_ENDPOINT
+end
+
+local function endpoint_to_component(device, ep)
+  local switch_comp = string.format("switch%d", ep)
+  if device.profile.components[switch_comp] ~= nil then
+    return switch_comp
+  else
+    return "main"
+  end
+end
+
+local function device_init(driver, device)
+  log.info_with({hub_logs=true}, "device init")
+  device:set_component_to_endpoint_fn(component_to_endpoint)
+  device:set_endpoint_to_component_fn(endpoint_to_component)
+  device:subscribe()
+end
+
+local function device_removed(driver, device)
+  log.info("device removed")
+end
+
+local function handle_switch_on(driver, device, cmd)
+  local endpoint_id = device:component_to_endpoint(cmd.component)
+  local req = clusters.OnOff.server.commands.On(device, endpoint_id)
+  device:send(req)
+end
+
+local function handle_switch_off(driver, device, cmd)
+  local endpoint_id = device:component_to_endpoint(cmd.component)
+  local req = clusters.OnOff.server.commands.Off(device, endpoint_id)
+  device:send(req)
+end
+
+local function handle_set_level(driver, device, cmd)
+  local endpoint_id = device:component_to_endpoint(cmd.component)
+  local level = math.floor(cmd.args.level/100.0 * 254)
+  local req = clusters.LevelControl.server.commands.MoveToLevelWithOnOff(device, endpoint_id, level, cmd.args.rate or 0, 0 ,0)
+  device:send(req)
+end
+
+--TODO could be moved to st.utils if made more generally useful
+local tbl_contains = function(t, val)
+  for _, v in pairs(t) do
+    if v == val then
+      return true
+    end
+  end
+  return false
+end
+
+local function handle_set_color(driver, device, cmd)
+  local endpoint_id = device:component_to_endpoint(cmd.component)
+  local req = nil
+  local huesat_endpoints = device:get_endpoints(clusters.ColorControl.ID, {feature_bitmap = clusters.ColorControl.FeatureMap.HUE_AND_SATURATION})
+  if tbl_contains(huesat_endpoints, endpoint_id) then
+    local hue = convert_huesat_st_to_matter(cmd.args.color.hue)
+    local sat = convert_huesat_st_to_matter(cmd.args.color.saturation)
+    req = clusters.ColorControl.server.commands.MoveToHueAndSaturation(device, endpoint_id, hue, sat, 0, 0, 0)
+  else
+    local x, y, _ = utils.safe_hsv_to_xy(cmd.args.color.hue, cmd.args.color.saturation)
+    req = clusters.ColorControl.server.commands.MoveToColor(device, endpoint_id, x, y, 0, 0, 0)
+  end
+  device:send(req)
+end
+
+local function handle_set_hue(driver, device, cmd)
+  local endpoint_id = device:component_to_endpoint(cmd.component)
+  local req = nil
+  local huesat_endpoints = device:get_endpoints(clusters.ColorControl.ID, {feature_bitmap = clusters.ColorControl.FeatureMap.HUE_AND_SATURATION})
+  if tbl_contains(huesat_endpoints, endpoint_id) then
+    local hue = convert_huesat_st_to_matter(cmd.args.hue)
+    req = clusters.ColorControl.server.commands.MoveToHue(device, endpoint_id, hue, 0, 0, 0, 0)
+    device:send(req)
+  else
+    log.warn("Device does not support huesat features on its color control cluster")
+ end
+end
+
+local function handle_set_saturation(driver, device, cmd)
+  local endpoint_id = device:component_to_endpoint(cmd.component)
+  local req = nil
+  local huesat_endpoints = device:get_endpoints(clusters.ColorControl.ID, {feature_bitmap = clusters.ColorControl.FeatureMap.HUE_AND_SATURATION})
+  if tbl_contains(huesat_endpoints, endpoint_id) then
+    local sat = convert_huesat_st_to_matter(cmd.args.saturation)
+    req = clusters.ColorControl.server.commands.MoveToSaturation(device, endpoint_id, sat, 0, 0, 0)
+    device:send(req)
+  else
+    log.warn("Device does not support huesat features on its color control cluster")
+  end
+end
+
+local function handle_set_color_temperature(driver, device, cmd)
+  local endpoint_id = device:component_to_endpoint(cmd.component)
+  local temp_in_mired = utils.round(CONVERSION_CONSTANT/cmd.args.temperature)
+  local req = clusters.ColorControl.server.commands.MoveToColorTemperature(device, endpoint_id, temp_in_mired, 0, 0, 0)
+  device:set_field(MOST_RECENT_TEMP, cmd.args.temperature)
+  device:send(req)
+end
+
+local function handle_refresh(driver, device, cmd)
+  --Note: no endpoint specified indicates a wildcard endpoint
+  local req = clusters.OnOff.attributes.OnOff:read(device)
+  device:send(req)
+end
+
+-- Fallback handler for responses that dont have their own handler
+local function matter_handler(driver, device, response_block)
+  log.info(string.format("Fallback handler for %s", response_block))
+end
+
+local function on_off_attr_handler(driver, device, ib, response)
+  if ib.data.value then
+    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.switch.switch.on())
+  else
+    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.switch.switch.off())
+  end
+end
+
+local function level_attr_handler(driver, device, ib, response)
+  if ib.data.value ~= nil then
+    local level = math.floor((ib.data.value / 254.0 * 100) + 0.5)
+    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.switchLevel.level(level))
+  end
+end
+
+local function hue_attr_handler(driver, device, ib, response)
+  if ib.data.value ~= nil then
+    local hue = math.floor((ib.data.value / 0xFE * 100) + 0.5)
+    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.colorControl.hue(hue))
+  end
+end
+
+local function sat_attr_handler(driver, device, ib, response)
+  if ib.data.value ~= nil then
+    local sat = math.floor((ib.data.value / 0xFE * 100) + 0.5)
+    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.colorControl.saturation(sat))
+  end
+end
+
+local function temp_attr_handler(driver, device, ib, response)
+  if ib.data.value ~= nil then
+    local temp = utils.round(CONVERSION_CONSTANT/ib.data.value)
+    local most_recent_temp = device:get_field(MOST_RECENT_TEMP)
+    -- this is to avoid rounding errors from the round-trip conversion of Kelvin to mireds
+    if most_recent_temp ~= nil and
+      most_recent_temp >= utils.round(CONVERSION_CONSTANT/(ib.data.value - 1)) and
+      most_recent_temp <= utils.round(CONVERSION_CONSTANT/(ib.data.value + 1)) then
+        temp = most_recent_temp
+    end
+    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.colorTemperature.colorTemperature(temp))
+  end
+end
+
+local function x_attr_handler(driver, device, ib, response)
+  local y = device:get_field(RECEIVED_Y)
+  --TODO it is likely that both x and y attributes are in the response (not guaranteed though)
+  -- if they are we can avoid setting fields on the device.
+  if y == nil then
+    device:set_field(RECEIVED_X, ib.data.value)
+  else
+    local x = ib.data.value
+    local h, s, _ = utils.safe_xy_to_hsv(x, y)
+    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.colorControl.hue(h))
+    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.colorControl.saturation(s))
+    device:set_field(RECEIVED_Y, nil)
+  end
+end
+
+local function y_attr_handler(driver, device, ib, response)
+  local x = device:get_field(RECEIVED_X)
+  if x == nil then
+    device:set_field(RECEIVED_Y, ib.data.value)
+  else
+    local y = ib.data.value
+    local h, s, _ = utils.safe_xy_to_hsv(x, y)
+    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.colorControl.hue(h))
+    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.colorControl.saturation(s))
+    device:set_field(RECEIVED_X, nil)
+  end
+end
+
+--TODO setup configure handler to read this attribute.
+local function color_cap_attr_handler(driver, device, ib, response)
+  if ib.data.value ~= nil then
+    if ib.data.value & 0x1 then
+      device:set_field(HUESAT_SUPPORT, true)
+    end
+  end
+end
+
+local matter_driver_template = {
+  lifecycle_handlers = {
+    init = device_init,
+    removed = device_removed,
+  },
+  matter_handlers = {
+    attr = {
+      [clusters.OnOff.ID] = {
+        [clusters.OnOff.attributes.OnOff.ID] = on_off_attr_handler,
+      },
+      [clusters.LevelControl.ID] = {
+        [clusters.LevelControl.attributes.CurrentLevel.ID] = level_attr_handler
+      },
+      [clusters.ColorControl.ID] = {
+        [clusters.ColorControl.attributes.CurrentHue.ID] = hue_attr_handler,
+        [clusters.ColorControl.attributes.CurrentSaturation.ID] = sat_attr_handler,
+        [ColorTemperatureAttribute.ID] = temp_attr_handler,
+        [clusters.ColorControl.attributes.CurrentX.ID] = x_attr_handler,
+        [clusters.ColorControl.attributes.CurrentY.ID] = y_attr_handler,
+        [clusters.ColorControl.attributes.ColorCapabilities.ID] = color_cap_attr_handler,
+      }
+    },
+    fallback = matter_handler,
+  },
+  subscribed_attributes = {
+    [capabilities.switch.ID] = {
+      clusters.OnOff.attributes.OnOff
+    },
+    [capabilities.switchLevel.ID] = {
+      clusters.LevelControl.attributes.CurrentLevel
+    },
+    [capabilities.colorControl.ID] = {
+      clusters.ColorControl.attributes.CurrentHue,
+      clusters.ColorControl.attributes.CurrentSaturation,
+      clusters.ColorControl.attributes.CurrentX,
+      clusters.ColorControl.attributes.CurrentY,
+    },
+    [capabilities.colorTemperature.ID] = {
+      ColorTemperatureAttribute,
+    },
+  },
+  capability_handlers = {
+    [capabilities.switch.ID] = {
+      [capabilities.switch.commands.on.NAME] = handle_switch_on,
+      [capabilities.switch.commands.off.NAME] = handle_switch_off,
+    },
+    [capabilities.switchLevel.ID] = {
+      [capabilities.switchLevel.commands.setLevel.NAME] = handle_set_level
+    },
+    [capabilities.refresh.ID] = {
+      [capabilities.refresh.commands.refresh.NAME] = handle_refresh,
+    },
+    [capabilities.colorControl.ID] = {
+      [capabilities.colorControl.commands.setColor.NAME] = handle_set_color,
+      [capabilities.colorControl.commands.setHue.NAME] = handle_set_hue,
+      [capabilities.colorControl.commands.setSaturation.NAME] = handle_set_saturation,
+    },
+    [capabilities.colorTemperature.ID] = {
+      [capabilities.colorTemperature.commands.setColorTemperature.NAME] = handle_set_color_temperature,
+    },
+  },
+}
+
+local matter_driver = MatterDriver("matter-switch", matter_driver_template)
+log.info_with({hub_logs=true}, string.format("Starting %s driver, with dispatcher: %s", matter_driver.NAME, matter_driver.matter_dispatcher))
+matter_driver:run()
