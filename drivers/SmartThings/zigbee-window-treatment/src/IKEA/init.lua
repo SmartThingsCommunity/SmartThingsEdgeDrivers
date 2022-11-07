915f70b1144acedb14b4833f3dcf596802cbae02
Lukas Junior <l.junior@insta.de>
2022-11-07 11:39:37 +0100

    Updated Nexentro Devices: * Added Pushbutton Interface as Range Extender * Integrated Blinds to IKEA Driver * Added genericTWBulb fingerprint

--- drivers/SmartThings/zigbee-window-treatment/src/IKEA/init.lua
+++ drivers/SmartThings/zigbee-window-treatment/src/IKEA/init.lua
@@ -20,44 +20,50 @@ local WindowCovering = zcl_clusters.WindowCovering
 local SHADE_SET_STATUS = "shade_set_status"
 
 local function current_position_attr_handler(driver, device, value, zb_rx)
-  local level = 100 - value.value
-  local current_level = device:get_latest_state("main", capabilities.windowShadeLevel.ID, capabilities.windowShadeLevel.shadeLevel.NAME)
-  local windowShade = capabilities.windowShade.windowShade
-  if level == 0 then
-    device:emit_event(windowShade.closed())
-    device:emit_event(capabilities.windowShadeLevel.shadeLevel(0))
-  elseif level == 100 then
-    device:emit_event(windowShade.open())
+  if value.value == 255 then
+    local windowShade = capabilities.windowShade.windowShade
+    device:emit_event(windowShade.unknown())
     device:emit_event(capabilities.windowShadeLevel.shadeLevel(100))
   else
-    if current_level ~= level or current_level == nil then
-      current_level = current_level or 0
-      device:emit_event(capabilities.windowShadeLevel.shadeLevel(level))
-      local event = nil
-      if current_level ~= level then
-        event = current_level < level and windowShade.opening() or windowShade.closing()
+    local level = 100 - value.value
+    local current_level = device:get_latest_state("main", capabilities.windowShadeLevel.ID, capabilities.windowShadeLevel.shadeLevel.NAME)
+    local windowShade = capabilities.windowShade.windowShade
+    if level == 0 then
+      device:emit_event(windowShade.closed())
+      device:emit_event(capabilities.windowShadeLevel.shadeLevel(0))
+    elseif level == 100 then
+      device:emit_event(windowShade.open())
+      device:emit_event(capabilities.windowShadeLevel.shadeLevel(100))
+    else
+      if current_level ~= level or current_level == nil then
+        current_level = current_level or 0
+        device:emit_event(capabilities.windowShadeLevel.shadeLevel(level))
+        local event = nil
+        if current_level ~= level then
+          event = current_level < level and windowShade.opening() or windowShade.closing()
+        end
+        if event ~= nil then
+          device:emit_event(event)
+        end
       end
-      if event ~= nil then
-        device:emit_event(event)
+      local set_status_timer = device:get_field(SHADE_SET_STATUS)
+      if set_status_timer then
+        device.thread:cancel_timer(set_status_timer)
+        device:set_field(SHADE_SET_STATUS, nil)
       end
-    end
-    local set_status_timer = device:get_field(SHADE_SET_STATUS)
-    if set_status_timer then
-      device.thread:cancel_timer(set_status_timer)
-      device:set_field(SHADE_SET_STATUS, nil)
-    end
-    local set_window_shade_status = function()
-      local current_level = device:get_latest_state("main", capabilities.windowShadeLevel.ID, capabilities.windowShadeLevel.shadeLevel.NAME)
-      if current_level == 0 then
-        device:emit_event(windowShade.closed())
-      elseif current_level == 100 then
-        device:emit_event(windowShade.open())
-      else
-        device:emit_event(windowShade.partially_open())
+      local set_window_shade_status = function()
+        local current_level = device:get_latest_state("main", capabilities.windowShadeLevel.ID, capabilities.windowShadeLevel.shadeLevel.NAME)
+        if current_level == 0 then
+          device:emit_event(windowShade.closed())
+        elseif current_level == 100 then
+          device:emit_event(windowShade.open())
+        else
+          device:emit_event(windowShade.partially_open())
+        end
       end
+      set_status_timer = device.thread:call_with_delay(1, set_window_shade_status)
+      device:set_field(SHADE_SET_STATUS, set_status_timer)
     end
-    set_status_timer = device.thread:call_with_delay(1, set_window_shade_status)
-    device:set_field(SHADE_SET_STATUS, set_status_timer)
   end
 end
 
@@ -94,7 +100,7 @@ local ikea_window_treatment = {
     }
   },
   can_handle = function(opts, driver, device, ...)
-    return device:get_manufacturer() == "IKEA of Sweden"
+    return device:get_manufacturer() == "IKEA of Sweden" or device:get_manufacturer() == "Insta GmbH"
   end
 }
 
