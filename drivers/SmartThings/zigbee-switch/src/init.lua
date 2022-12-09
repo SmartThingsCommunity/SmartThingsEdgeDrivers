3cefd56deea09b7b308e0478a47832954717804d
Niels Roche <nroche.z-wave@outlook.de>
2022-12-09 15:11:55 +0100

    power consumption added

--- drivers/SmartThings/zigbee-switch/src/init.lua
+++ drivers/SmartThings/zigbee-switch/src/init.lua
@@ -94,7 +94,8 @@ local zigbee_switch_driver_template = {
     require("rgbw-bulb"),
     require("zll-dimmer-bulb"),
     require("zigbee-switch-power"),
-    require("ge-link-bulb")
+    require("ge-link-bulb"),
+    require("robb")
   },
   lifecycle_handlers = {
     init = device_init,
