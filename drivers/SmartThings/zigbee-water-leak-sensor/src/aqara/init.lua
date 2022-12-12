b0e639852e9c6174c69bf2f6ac2ad99e03e23efb
sbkimaqara <sbkim@aqara.kr>
2022-12-12 14:39:14 +0900

    Add configuration

--- drivers/SmartThings/zigbee-water-leak-sensor/src/aqara/init.lua
+++ drivers/SmartThings/zigbee-water-leak-sensor/src/aqara/init.lua
@@ -1,20 +1,28 @@
--- Copyright 2022 SmartThings
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---     http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
+local capabilities = require "st.capabilities"
+local clusters = require "st.zigbee.zcl.clusters"
+local cluster_base = require "st.zigbee.cluster_base"
+local data_types = require "st.zigbee.data_types"
 local battery_defaults = require "st.zigbee.defaults.battery_defaults"
+
+local PowerConfiguration = clusters.PowerConfiguration
+
+local MFG_CODE = 0x115F
+local PRIVATE_CLUSTER_ID = 0xFCC0
+local PRIVATE_ATTRIBUTE_ID = 0x0009
 
 local FINGERPRINTS = {
   { mfr = "LUMI", model = "lumi.flood.agl02" }
+}
+
+local CONFIGURATIONS = {
+  {
+    cluster = PowerConfiguration.ID,
+    attribute = PowerConfiguration.attributes.BatteryVoltage.ID,
+    minimum_interval = 30,
+    maximum_interval = 3600,
+    data_type = PowerConfiguration.attributes.BatteryVoltage.base_type,
+    reportable_change = 1
+  }
 }
 
 local function is_aqara_products(opts, driver, device)
@@ -26,10 +34,33 @@ local function is_aqara_products(opts, driver, device)
   return false
 end
 
+local function write_private_attribute(device, cluster_id, attribute_id, data_type, value)
+  device:send(cluster_base.write_manufacturer_specific_attribute(device, cluster_id, attribute_id, MFG_CODE, data_type,
+    value))
+end
+
+local function device_added(driver, device)
+  device:emit_event(capabilities.waterSensor.water.dry())
+  device:emit_event(capabilities.battery.battery(100))
+
+  -- Set private attribute
+  write_private_attribute(device, PRIVATE_CLUSTER_ID, PRIVATE_ATTRIBUTE_ID, data_types.Uint8, 1)
+end
+
+local function device_init(driver, device)
+  battery_defaults.build_linear_voltage_init(2.6, 3.0)(driver, device)
+
+  for _, attribute in ipairs(CONFIGURATIONS) do
+    device:add_configured_attribute(attribute)
+    device:add_monitored_attribute(attribute)
+  end
+end
+
 local aqara_contact_handler = {
   NAME = "Aqara water leak sensor",
   lifecycle_handlers = {
-    init = battery_defaults.build_linear_voltage_init(2.6, 3.0),
+    init = device_init,
+    added = device_added
   },
   can_handle = is_aqara_products
 }
