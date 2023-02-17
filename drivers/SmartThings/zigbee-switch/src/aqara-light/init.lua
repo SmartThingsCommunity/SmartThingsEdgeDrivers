ffd3189831f6776fef0f9208243db96d70a89149
sbkimaqara <sbkim@aqara.kr>
2023-02-17 17:21:46 +0900

    Refactoring doConfigure handler code

--- drivers/SmartThings/zigbee-switch/src/aqara-light/init.lua
+++ drivers/SmartThings/zigbee-switch/src/aqara-light/init.lua
@@ -23,6 +23,25 @@ local FINGERPRINTS = {
   { mfr = "LUMI", model = "lumi.light.acn004" }
 }
 
+local preference_message_map = {
+  [restorePowerState.ID] = function(device, value)
+    return cluster_base.write_manufacturer_specific_attribute(device, PRIVATE_CLUSTER_ID,
+      RESTORE_POWER_STATE_ATTRIBUTE_ID, MFG_CODE, data_types.Boolean, value)
+  end,
+  [turnOffIndicatorLight.ID] = function(device, value)
+    return cluster_base.write_manufacturer_specific_attribute(device, PRIVATE_CLUSTER_ID,
+      TURN_OFF_INDICATOR_ATTRIBUTE_ID, MFG_CODE, data_types.Boolean, value)
+  end,
+  [lightFadeInTimeInSec.ID] = function(device, value)
+    local raw_value = value * 10 -- value unit: 1sec, transition time unit: 100ms
+    return Level.attributes.OnTransitionTime:write(device, raw_value)
+  end,
+  [lightFadeOutTimeInSec.ID] = function(device, value)
+    local raw_value = value * 10 -- value unit: 1sec, transition time unit: 100ms
+    return Level.attributes.OffTransitionTime:write(device, raw_value)
+  end
+}
+
 local function is_aqara_products(opts, driver, device)
   for _, fingerprint in ipairs(FINGERPRINTS) do
     if device:get_manufacturer() == fingerprint.mfr and device:get_model() == fingerprint.model then
@@ -39,31 +58,18 @@ local function do_refresh(self, device)
 end
 
 local function device_info_changed(driver, device, event, args)
-  if device.preferences ~= nil then
-    if device.preferences[restorePowerState.ID] ~= nil and
-        device.preferences[restorePowerState.ID] ~= args.old_st_store.preferences[restorePowerState.ID] then
-      device:send(cluster_base.write_manufacturer_specific_attribute(device, PRIVATE_CLUSTER_ID,
-        RESTORE_POWER_STATE_ATTRIBUTE_ID, MFG_CODE, data_types.Boolean, device.preferences[restorePowerState.ID]))
-    end
-
-    if device.preferences[turnOffIndicatorLight.ID] ~= nil and
-        device.preferences[turnOffIndicatorLight.ID] ~= args.old_st_store.preferences[turnOffIndicatorLight.ID] then
-      device:send(cluster_base.write_manufacturer_specific_attribute(device, PRIVATE_CLUSTER_ID,
-        TURN_OFF_INDICATOR_ATTRIBUTE_ID, MFG_CODE, data_types.Boolean, device.preferences[turnOffIndicatorLight.ID]))
-    end
-
-    if device.preferences[lightFadeInTimeInSec.ID] ~= nil and
-        device.preferences[lightFadeInTimeInSec.ID] ~= args.old_st_store.preferences[lightFadeInTimeInSec.ID] then
-      local raw_value = device.preferences[lightFadeInTimeInSec.ID] -- seconds
-      raw_value = raw_value * 10 -- unit: 100ms
-      device:send(Level.attributes.OnTransitionTime:write(device, raw_value))
-    end
-
-    if device.preferences[lightFadeOutTimeInSec.ID] ~= nil and
-        device.preferences[lightFadeOutTimeInSec.ID] ~= args.old_st_store.preferences[lightFadeOutTimeInSec.ID] then
-      local raw_value = device.preferences[lightFadeOutTimeInSec.ID] -- seconds
-      raw_value = raw_value * 10 -- unit: 100ms
-      device:send(Level.attributes.OffTransitionTime:write(device, raw_value))
+  local preferences = device.preferences
+  local old_preferences = args.old_st_store.preferences
+  if preferences ~= nil then
+    for id, attr in pairs(preference_message_map) do
+      local old_value = old_preferences[id]
+      local value = preferences[id]
+      if value ~= nil and value ~= old_value then
+        local write_message = attr(device, value)
+        if write_message ~= nil then
+          device:send(write_message)
+        end
+      end
     end
   end
 end
@@ -75,10 +81,6 @@ local function do_configure(self, device)
 end
 
 local function device_added(driver, device, event)
-  device:emit_event(capabilities.switch.switch.on())
-  device:emit_event(capabilities.switchLevel.level(100))
-  device:emit_event(capabilities.colorTemperature.colorTemperature(5000))
-
   device:send(cluster_base.write_manufacturer_specific_attribute(device,
     PRIVATE_CLUSTER_ID, PRIVATE_ATTRIBUTE_ID, MFG_CODE, data_types.Uint8, 1)) -- private
 
