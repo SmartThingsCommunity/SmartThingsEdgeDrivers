152eed4e469f03345ec05238c5d8f3d29b2a04fc
Niels Roche <nroche.z-wave@outlook.de>
2023-02-18 07:17:31 +0100

    updated to latest suggestions

--- drivers/SmartThings/zigbee-button/src/zigbee-multi-button/robb/wireless_smarrt_switch_8.lua
+++ drivers/SmartThings/zigbee-button/src/zigbee-multi-button/robb/wireless_smarrt_switch_8.lua
@@ -9,30 +9,18 @@ local PowerConfiguration = zcl_clusters.PowerConfiguration
 
 local log = require "log"
 local utils = require "st.utils"
-local json = require("dkjson")
 
 local capabilities = require "st.capabilities"
 local battery_defaults = require "st.zigbee.defaults.battery_defaults"
 
+local button_utils = require "button_utils"
+
 local SWITCH8_GROUP_CONFIGURE = "is_group_configured"
 local SWITCH8_NUM_ENDPOINT = 0x04
 
-local BUTTON_CP_ON_ENDPOINT_MAP = {
-  [1] = "button1",
-  [2] = "button3",
-  [3] = "button5",
-  [4] = "button7"
-}
-
-local BUTTON_CP_OFF_ENDPOINT_MAP = {
-  [1] = "button2",
-  [2] = "button4",
-  [3] = "button6",
-  [4] = "button8"
-}
-
 local WIRELESS_REMOTE_FINGERPRINTS = {
-  { mfr = "ROBB smarrt", model = "ROB_200-007-0" }
+  { mfr = "ROBB smarrt", model = "ROB_200-007-0" },
+  { mfr = "ROBB smarrt", model = "ROB_200-008-0" }
 }
 
 local function can_handle(opts, driver, device, ...)
@@ -44,56 +32,15 @@ local function can_handle(opts, driver, device, ...)
   return false
 end
 
---[[ local function component_to_endpoint(device, component_id)
-  local ep_ini = device.fingerprinted_endpoint_id
-
-  log.debug("### component_to_endpoint")
-
-  if component_id == "main" then
-    return ep_ini
-  else
-    local ep_num = component_id:match("button(%d)")
-    if ep_num == "1" then
-      return ep_ini
-    elseif ep_num == "2" then
-      return ep_ini
-    elseif ep_num == "3" then
-      return ep_ini + 1
-    elseif ep_num == "4" then
-      return ep_ini + 1
-    end
-  end
-
-  local ep_num = component_id:match("button(%d)")
-  return ep_num and tonumber(ep_num) or device.fingerprinted_endpoint_id
-end ]]
-
---[[ local function endpoint_to_component(device, ep)
-  local ep_ini = device.fingerprinted_endpoint_id
-
-  log.debug("### endpoint_to_component")
-
-  if ep == ep_ini then
-    return { "main", "button1", "button2" }
-  else
-    if ep == ep_ini + 1 then
-      return { "button3", "button4" }
-         elseif ep == ep_ini then
-      return "button2"
-    elseif ep == ep_ini + 1 then
-      return "button3"
-    elseif ep == ep_ini + 1 then
-      return "button4"
-    end
-  end
-end ]]
 
 local function component_to_endpoint(device, component_id)
+  log.debug('### component_id:' .. component_id)
   local ep_num = component_id:match("button(%d)")
   return { ep_num and tonumber(ep_num) }
 end
 
 local function endpoint_to_component(device, ep)
+  log.debug('### ep:' .. ep)
   local button_comp = string.format("button%d", ep)
   if device.profile.components[button_comp] ~= nil then
     return button_comp
@@ -102,37 +49,10 @@ local function endpoint_to_component(device, ep)
   end
 end
 
-local function build_button_handler(MAPPING, pressed_type)
-  return function(driver, device, zb_rx)
-    --local bytes = zb_rx.body
-
-    log.debug('### zbrx:' .. utils.stringify_table(zb_rx, 'body', true))
-    --[[ local button_num = bytes:byte(2) + 1
-    local button_name = "button" .. button_num
-    log.debug('### button_name:' .. button_name)]]
-
-    local additional_fields = {
-      state_change = true
-    }
-    local event = pressed_type(additional_fields)
-    local button_name = MAPPING[zb_rx.address_header.src_endpoint.value]
-    local comp = device.profile.components[button_name]
-    if comp ~= nil then
-      device:emit_component_event(comp, event)
-    else
-      log.warn("Attempted to emit button event for unknown button: " .. button_name)
-    end
-  end
-end
 
 local device_init = function(driver, device)
-  device:set_component_to_endpoint_fn(component_to_endpoint)
-  device:set_endpoint_to_component_fn(endpoint_to_component)
-
-  --[[ log.debug("### new device info start: ")
-  local dev = driver.device_api.get_device_info(device.id)
-  log.debug(utils.stringify_table(dev, "new dev table", true))
-  log.debug("### new device info end ") ]]
+  --[[ device:set_component_to_endpoint_fn(component_to_endpoint)
+  device:set_endpoint_to_component_fn(endpoint_to_component) ]]
 
   battery_defaults.build_linear_voltage_init(2.1, 3.0)
 end
@@ -140,58 +60,66 @@ end
 local do_configuration = function(self, device)
   device:send(device_management.build_bind_request(device, PowerConfiguration.ID, self.environment_info.hub_zigbee_eui))
   device:send(PowerConfiguration.attributes.BatteryVoltage:configure_reporting(device, 30, 21600, 1))
-
-  device:send(device_management.build_bind_request(device, Level.ID, self.environment_info.hub_zigbee_eui):to_endpoint(0x01))
-  device:send(device_management.build_bind_request(device, OnOff.ID, self.environment_info.hub_zigbee_eui):to_endpoint(0x01))
-  device:send(device_management.build_bind_request(device, Level.ID, self.environment_info.hub_zigbee_eui):to_endpoint(0x02))
-  device:send(device_management.build_bind_request(device, OnOff.ID, self.environment_info.hub_zigbee_eui):to_endpoint(0x02))
-  device:send(device_management.build_bind_request(device, Level.ID, self.environment_info.hub_zigbee_eui):to_endpoint(0x03))
-  device:send(device_management.build_bind_request(device, OnOff.ID, self.environment_info.hub_zigbee_eui):to_endpoint(0x03))
-  device:send(device_management.build_bind_request(device, Level.ID, self.environment_info.hub_zigbee_eui):to_endpoint(0x04))
-  device:send(device_management.build_bind_request(device, OnOff.ID, self.environment_info.hub_zigbee_eui):to_endpoint(0x04))
-  
-  --device:send(OnOff.attributes.OnOff:configure_reporting(device, 0, 600, 1):to_endpoint(endpoint))
-  --[[ device:send(device_management.build_bind_request(device, PowerConfiguration.ID, self.environment_info.hub_zigbee_eui))
-  device:send(PowerConfiguration.attributes.BatteryVoltage:configure_reporting(device, 30, 21600, 1))
-  for endpoint = 1, SWITCH8_NUM_ENDPOINT do
-    device:send(device_management.build_bind_request(device, Level.ID, self.environment_info.hub_zigbee_eui):to_endpoint(endpoint))
-    device:send(device_management.build_bind_request(device, OnOff.ID, self.environment_info.hub_zigbee_eui):to_endpoint(endpoint))
+  for endpoint = 1,SWITCH8_NUM_ENDPOINT do
+    device:send(device_management.build_bind_request(device, Level.ID, self.environment_info.hub_zigbee_eui):to_endpoint(
+      endpoint))
+    device:send(device_management.build_bind_request(device, OnOff.ID, self.environment_info.hub_zigbee_eui):to_endpoint(
+      endpoint))
   end
+
   device:send(OnOff.attributes.OnOff:configure_reporting(device, 0, 600, 1))
 
   device:send(Basic.attributes.DeviceEnabled:write(device, true))
   if not self.datastore[SWITCH8_GROUP_CONFIGURE] then
     -- Configure adding hub to group once
-    self:add_hub_to_zigbee_group(0x0D01)
-    self:add_hub_to_zigbee_group(0x0D02)
-    self:add_hub_to_zigbee_group(0x0D03)
-    self:add_hub_to_zigbee_group(0x0D04)
+    self:add_hub_to_zigbee_group(0xE902)
+    self:add_hub_to_zigbee_group(0xE903)
+    self:add_hub_to_zigbee_group(0xE904)
     self.datastore[SWITCH8_GROUP_CONFIGURE] = true
-  end ]]
+  end
+end
+
+
+
+local EP_BUTTON_ON_COMPONENT_MAP = {
+  [0x01] = 1,
+  [0x02] = 3,
+  [0x03] = 5,
+  [0x04] = 7
+}
+
+local EP_BUTTON_OFF_COMPONENT_MAP = {
+  [0x01] = 2,
+  [0x02] = 4,
+  [0x03] = 6,
+  [0x04] = 8
+}
+
+local function attr_on_handler(driver, device, zb_rx)
+  button_utils.send_pushed_or_held_button_event_if_applicable(device,
+    EP_BUTTON_ON_COMPONENT_MAP[zb_rx.address_header.src_endpoint.value])
+end
+
+local function attr_off_handler(driver, device, zb_rx)
+  button_utils.send_pushed_or_held_button_event_if_applicable(device,
+    EP_BUTTON_OFF_COMPONENT_MAP[zb_rx.address_header.src_endpoint.value])
 end
 
 local robb_wireless_8_control = {
   NAME = "ROBB Wireless 8 Remote Control",
-  supported_capabilities = {
-    capabilities.battery,
-    capabilities.switch,
-  },
   lifecycle_handlers = {
     init = device_init,
-    -- init = battery_defaults.build_linear_voltage_init(2.1, 3.0),
-    -- added = added_handler,
     doConfigure = do_configuration
   },
   zigbee_handlers = {
     cluster = {
-      --[[ [Level.ID] = {
-        [Level.server.commands.MoveWithOnOff.ID] = build_button_handler(BUTTON_CP_ON_ENDPOINT_MAP),
-        [Level.server.commands.StopWithOnOff.ID] = build_button_handler(BUTTON_CP_OFF_ENDPOINT_MAP)
-      }, ]]
+      [Level.ID] = {
+        [Level.server.commands.MoveWithOnOff.ID] = attr_on_handler,
+        [Level.server.commands.StopWithOnOff.ID] = attr_off_handler
+      },
       [OnOff.ID] = {
-        [OnOff.server.commands.Off.ID] = build_button_handler(BUTTON_CP_OFF_ENDPOINT_MAP,
-          capabilities.button.button.pushed),
-        [OnOff.server.commands.On.ID] = build_button_handler(BUTTON_CP_ON_ENDPOINT_MAP, capabilities.button.button.pushed)
+        [OnOff.server.commands.Off.ID] = attr_off_handler,
+        [OnOff.server.commands.On.ID] = attr_on_handler
       }
     }
   },
