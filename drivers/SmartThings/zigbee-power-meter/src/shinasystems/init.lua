3bc2ea87a72224e048f37275ae994d9866c3efe4
Shina System Co Ltd <sihas.iot@gmail.com>
2023-03-30 18:12:55 +0900

    Add variable declaration
    Add variable declaration

--- drivers/SmartThings/zigbee-power-meter/src/shinasystems/init.lua
+++ drivers/SmartThings/zigbee-power-meter/src/shinasystems/init.lua
@@ -12,7 +12,13 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
+local capabilities = require "st.capabilities"
+local ZigbeeDriver = require "st.zigbee"
+local defaults = require "st.zigbee.defaults"
 local constants = require "st.zigbee.constants"
+local clusters = require "st.zigbee.zcl.clusters"
+local ElectricalMeasurement = clusters.ElectricalMeasurement
+local SimpleMetering = clusters.SimpleMetering
 
 local ZIGBEE_POWER_METER_FINGERPRINTS = {
   { model = "PMM-300Z1" },
