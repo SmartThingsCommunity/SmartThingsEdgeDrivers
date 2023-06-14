5296e1039095e8c678cb6f830cff556af5e39be9
weihuan1111 <99949392+weihuan1111@users.noreply.github.com>
2023-06-14 15:22:30 +0800

    Update init.lua

--- drivers/SmartThings/zigbee-button/src/thirdreality/init.lua
+++ drivers/SmartThings/zigbee-button/src/thirdreality/init.lua
@@ -19,28 +19,28 @@ local device_management = require "st.zigbee.device_management"
 local zcl_commands = require "st.zigbee.zcl.global_commands"
 
 local function present_value_attr_handler(driver, device, value, zb_rx)
-    local event
-    local additional_fields = {
-      state_change = true
-    }
-    if value.value == 0x0001 then
-      event = capabilities.button.button.pushed(additional_fields)
-      device:emit_event(event)
-    end
-    if value.value == 0x0002 then
-        event = capabilities.button.button.double(additional_fields)
-        device:emit_event(event)
-    end
-    if value.value == 0x0000 then
-        event = capabilities.button.button.held(additional_fields)
-        device:emit_event(event)
-    end
+  local event
+  local additional_fields = {
+    state_change = true
+  }
+  if value.value == 0x0001 then
+    event = capabilities.button.button.pushed(additional_fields)
+    device:emit_event(event)
   end
+  if value.value == 0x0002 then
+    event = capabilities.button.button.double(additional_fields)
+    device:emit_event(event)
+  end
+  if value.value == 0x0000 then
+    event = capabilities.button.button.held(additional_fields)
+    device:emit_event(event)
+  end
+end
 
 local function device_added(driver, device)
-    device:emit_event(capabilities.button.supportedButtonValues({ "pushed", "double", "held" }))
-    device:emit_event(capabilities.button.numberOfButtons({value = 1}))
-  end
+  device:emit_event(capabilities.button.supportedButtonValues({ "pushed", "double", "held" }))
+  device:emit_event(capabilities.button.numberOfButtons({value = 1}))
+end
 
 local thirdreality_device_handler = {
   NAME = "ThirdReality Smart Button",
