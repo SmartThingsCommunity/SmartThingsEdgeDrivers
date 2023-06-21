8f626e8f4f9d21105e49fa6dcd7e32fef50927c1
sengled-desi <desi.luo@sengled.com>
2023-06-21 13:52:39 +0800

    modify SmartThings/zigbee-motion-sensor/src/init.lua and SmartThings/zigbee-contact/src/init.lua

--- drivers/SmartThings/zigbee-motion-sensor/src/init.lua
+++ drivers/SmartThings/zigbee-motion-sensor/src/init.lua
@@ -42,7 +42,8 @@ local zigbee_motion_driver = {
     require("centralite"),
     require("smartthings"),
     require("smartsense"),
-    require("thirdreality")
+	require("thirdreality"),
+    require("sengled")
   },
   additional_zcl_profiles = {
     [0xFC01] = true
