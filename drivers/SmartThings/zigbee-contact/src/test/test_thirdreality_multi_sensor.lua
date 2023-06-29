99ec2193943c629d1ce5cfa1f43176ebb230649e
weihuan1111 <99949392+weihuan1111@users.noreply.github.com>
2023-06-29 08:58:23 +0800

    Update test_thirdreality_multi_sensor.lua

--- drivers/SmartThings/zigbee-contact/src/test/test_thirdreality_multi_sensor.lua
+++ drivers/SmartThings/zigbee-contact/src/test/test_thirdreality_multi_sensor.lua
@@ -7,8 +7,6 @@ local zigbee_test_utils = require "integration_test.zigbee_test_utils"
 local cluster_base = require "st.zigbee.cluster_base"
 local data_types = require "st.zigbee.data_types"
 
-local IASZone = clusters.IASZone
-
 local mock_device = test.mock_device.build_test_zigbee_device(
   {
     profile = t_utils.get_profile_definition("thirdreality-multi-sensor.yml"),
