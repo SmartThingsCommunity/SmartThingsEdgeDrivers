1edbe430b98473a172421759d7d878a68d842314
weihuan1111 <99949392+weihuan1111@users.noreply.github.com>
2023-06-30 08:33:36 +0800

    Update init.lua

--- drivers/SmartThings/zigbee-contact/src/multi-sensor/thirdreality-multi/init.lua
+++ drivers/SmartThings/zigbee-contact/src/multi-sensor/thirdreality-multi/init.lua
@@ -1,3 +1,17 @@
+-- Copyright 2023 SmartThings
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 local zcl_commands = require "st.zigbee.zcl.global_commands"
 local multi_utils = require "multi-sensor/multi_utils"
 
