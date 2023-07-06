8d22364c0c32d05de7567c1d86a58ceba6b6d0f8
weihuan1111 <99949392+weihuan1111@users.noreply.github.com>
2023-07-06 09:50:13 +0800

    Update test_thirdreality_multi_function_night_light.lua

--- drivers/SmartThings/zigbee-switch/src/test/test_thirdreality_multi_function_night_light.lua
+++ drivers/SmartThings/zigbee-switch/src/test/test_thirdreality_multi_function_night_light.lua
@@ -2,6 +2,7 @@ local capabilities = require "st.capabilities"
 local clusters = require "st.zigbee.zcl.clusters"
 local t_utils = require "integration_test.utils"
 local test = require "integration_test"
+local data_types = require "st.zigbee.data_types"
 local zigbee_test_utils = require "integration_test.zigbee_test_utils"
 local OnOff = clusters.OnOff
 local IlluminanceMeasurement = clusters.IlluminanceMeasurement
@@ -69,11 +70,11 @@ test.register_coroutine_test(
   "Reported motion should be handled: active",
   function()
     local attr_report_data = {
-      { 0x0002, data_types.Int16.ID, 0x0001}
+      { 0x0002, data_types.Int16.ID, MOTION_DETECT}
     }
     test.socket.zigbee:__queue_receive({
       mock_device.id,
-      zigbee_test_utils.build_attribute_report(mock_device, 0xFC00, attr_report_data, 0x110A)
+      zigbee_test_utils.build_attribute_report(mock_device, THIRDREALITY_MOTION_CLUSTER, attr_report_data, 0x110A)
     })
     test.socket.capability:__expect_send(mock_device:generate_test_message("main", capabilities.motionSensor.motion.active()))
   end
@@ -83,11 +84,11 @@ test.register_coroutine_test(
   "Reported motion should be handled: inactive",
   function()
     local attr_report_data = {
-      { 0x0002, data_types.Int16.ID, 0x0000}
+      { 0x0002, data_types.Int16.ID, MOTION_NO_DETECT}
     }
     test.socket.zigbee:__queue_receive({
       mock_device.id,
-      zigbee_test_utils.build_attribute_report(mock_device, 0xFC00, attr_report_data, 0x110A)
+      zigbee_test_utils.build_attribute_report(mock_device, THIRDREALITY_MOTION_CLUSTER, attr_report_data, 0x110A)
     })
     test.socket.capability:__expect_send(mock_device:generate_test_message("main", capabilities.motionSensor.motion.inactive()))
   end
