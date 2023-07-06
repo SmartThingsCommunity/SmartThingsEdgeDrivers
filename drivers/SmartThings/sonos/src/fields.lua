855f13a0017045bbbcdf6c1c284a8b19eb0e33cd
Doug Stephen <doug.stephen@smartthings.com>
2023-07-06 13:12:45 -0500

    fix: Allow SSDP "rediscovery" to run indefinitely
    This moves the SSDP Rediscovery process (re-scanning during added or
    init) on to a cosock task, and replaces the upper bound on attempts
    with retry-until-success logic.
    
    We have to move this on to a task if we want to retry until we succeed
    otherwise we'd be blocking the device's thread in the lifecycle handlers.

--- drivers/SmartThings/sonos/src/fields.lua
+++ drivers/SmartThings/sonos/src/fields.lua
@@ -4,6 +4,7 @@ local Fields = {}
 ---@enum SonosPlayerFields
 Fields.SonosPlayerFields = {
   _IS_INIT = "init",
+  _IS_SCANNING = "scanning",
   CONNECTION = "conn",
   HOUSEHOULD_ID = "householdId",
   PLAYER_ID = "playerId",
