e9889329e8f2752212fdcf5da608948c05df1465
Carter Swedal <carter.swedal@smartthings.com>
2023-07-11 20:14:37 -0500

    Sonos crash fix
    runtime error: [string "cosock.lua"]:296: [string "api/sonos_connection.lua"]:210: attempt to index a nil value

--- drivers/SmartThings/sonos/src/api/sonos_connection.lua
+++ drivers/SmartThings/sonos/src/api/sonos_connection.lua
@@ -89,10 +89,24 @@ local function _open_coordinator_socket(sonos_conn, household_id, self_player_id
         "Could not look up coordinator info for player %s: %s", sonos_conn.device.label, err
       )
     )
+    return
   end
 
   if coordinator_id ~= self_player_id then
-    local coordinator = sonos_conn.driver.sonos:get_household(household_id).players[coordinator_id]
+    local household = sonos_conn.driver.sonos:get_household(household_id)
+    if household == nil then
+      log.error(string.format("Cannot open coordinator socket, houshold doesn't exist: %s", household_id))
+      return
+    end
+
+    local coordinator = household.players[coordinator_id]
+    if coordinator == nil then
+      log.error(st_utils.stringify_table(
+        {household = sonos_conn.driver.sonos:get_household(household_id)}, string.format("Coordinator doesn't exist for player: %s", sonos_conn.device.label), false
+      ))
+      return
+    end
+
     _, err = Router.open_socket_for_player(coordinator_id, coordinator.websocketUrl)
 
     if err ~= nil then
@@ -207,7 +221,15 @@ function SonosConnection.new(driver, device)
       elseif header.type == "groupVolume" then
         log.trace(string.format("GroupVolume type message for %s", device_name))
         if body.volume and (body.muted ~= nil) then
-          local group = self.driver.sonos:get_household(header.householdId).groups[header.groupId] or { playerIds = {} }
+          local household = self.driver.sonos:get_household(header.householdId)
+          if household == nil or household.groups == nil then
+            log.error(st_utils.stringify_table(
+              {response_body = msg.data, household = household or header.householdId},
+              "Received groupVolume message for non-existent household or household groups dont exist", false
+            ))
+            return
+          end
+          local group = household.groups[header.groupId] or { playerIds = {} }
           for _, player_id in ipairs(group.playerIds) do
             local device_for_player = self.driver._player_id_to_device[player_id]
             --- we've seen situations where these messages can be processed while a device
@@ -220,7 +242,15 @@ function SonosConnection.new(driver, device)
         end
       elseif header.type == "playbackStatus" then
         log.trace(string.format("PlaybackStatus type message for %s", device_name))
-        local group = self.driver.sonos:get_household(header.householdId).groups[header.groupId] or { playerIds = {} }
+        local household = self.driver.sonos:get_household(header.householdId)
+        if household == nil or household.groups == nil then
+          log.error(st_utils.stringify_table(
+            {response_body = msg.data, household = household or header.householdId},
+            "Received playbackStatus message for non-existent household or household groups dont exist", false
+          ))
+          return
+        end
+        local group = household.groups[header.groupId] or { playerIds = {} }
         for _, player_id in ipairs(group.playerIds) do
           local device_for_player = self.driver._player_id_to_device[player_id]
           --- we've seen situations where these messages can be processed while a device
@@ -232,7 +262,15 @@ function SonosConnection.new(driver, device)
         end
       elseif header.type == "metadataStatus" then
         log.trace(string.format("MetadataStatus type message for %s", device_name))
-        local group = self.driver.sonos:get_household(header.householdId).groups[header.groupId] or { playerIds = {} }
+        local household = self.driver.sonos:get_household(header.householdId)
+        if household == nil or household.groups == nil then
+          log.error(st_utils.stringify_table(
+            {response_body = msg.data, household = household or header.householdId},
+            "Received metadataStatus message for non-existent household or household groups dont exist", false
+          ))
+          return
+        end
+        local group = household.groups[header.groupId] or { playerIds = {} }
         for _, player_id in ipairs(group.playerIds) do
           local device_for_player = self.driver._player_id_to_device[player_id]
           --- we've seen situations where these messages can be processed while a device
