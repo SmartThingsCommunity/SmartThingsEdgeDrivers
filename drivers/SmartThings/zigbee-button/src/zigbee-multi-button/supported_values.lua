f2a714248e0d9b82ce6d36aeb9081bbbf76e9ad9
Liu Yiding <liuyd.fnst@fujitsu.com>
2023-07-17 11:41:35 +0800

    Add WALL HERO 30-buttons screen
    Signed-off-by: Liu Yiding <liuyd.fnst@fujitsu.com>

--- drivers/SmartThings/zigbee-button/src/zigbee-multi-button/supported_values.lua
+++ drivers/SmartThings/zigbee-button/src/zigbee-multi-button/supported_values.lua
@@ -72,6 +72,13 @@ local devices = {
     SUPPORTED_BUTTON_VALUES = { "pushed" },
     NUMBER_OF_BUTTONS = 12
   },
+  BUTTON_PUSH_30 = {
+    MATCHING_MATRIX = {
+      { mfr = "WALL HERO", model = "A4" }
+    },
+    SUPPORTED_BUTTON_VALUES = { "pushed" },
+    NUMBER_OF_BUTTONS = 30
+  },
   BUTTON_PUSH_HELD_DOUBLE_1 = {
     MATCHING_MATRIX = {
       { mfr = "ShinaSystem", model = "BSM-300Z" },
