2cf6fe099e3a0dcdf036a00fd09ced8d80b62e51
Trey Cozic <trey.cozic@leviton.com>
2023-07-28 10:52:24 -0500

    cutting out a couple more things that are handled in the generic driver

--- drivers/SmartThings/zwave-switch/src/leviton-ZWxxx/init.lua
+++ drivers/SmartThings/zwave-switch/src/leviton-ZWxxx/init.lua
@@ -16,42 +16,9 @@ local function can_handle_leviton_zwxxx(opts, driver, device, ...)
 	return false
 end
 
-local update_preferences = function(driver, device, args)
-	local prefs = preferences.get_device_parameters(device)
-	if prefs ~= nil then
-		for id, value in pairs(device.preferences) do
-			if not (args and args.old_st_store) or (args.old_st_store.preferences[id] ~= value and prefs and prefs[id]) then
-				local new_parameter_value = preferences.to_numeric_value(device.preferences[id])
-				device:set_field(id, args.old_st_store.preferences[id], { persist = true })
-				device:send(Configuration:Set({
-					parameter_number = prefs[id].parameter_number,
-					size = prefs[id].size,
-					configuration_value = new_parameter_value
-				}))
-			end
-		end
-	else
-		print("update_preferences(): prefs is nil!")
-	end
-end
-
-local function init_dev(self, device, event, args)
-	if preferencesMap ~= nil then
-		device:set_update_preferences_fn(update_preferences)
-		local preferences = preferencesMap.get_device_parameters(device)
-		if preferences == nil then
-			print("init_dev(): preferences is nil!")
-			return
-		end
-	else
-		print("preferencesMap is nil!")
-	end
-end
-
 local function device_added(self, device, event, args)
 	local preferences = preferencesMap.get_device_parameters(device)
 	for id, pref in pairs(preferences) do
-		device:set_field(id, pref.parameter_number, { persist = true })
 		device:send(Configuration:Get({ parameter_number = pref.parameter_number }))
 	end
 end
@@ -63,11 +30,11 @@ end
 --- @param event table
 --- @param args
 local function info_changed(driver, device, event, args)
-	local preferences = preferencesMap.get_device_parameters(device)
-	for id, value in pairs(preferences) do
+	local parameters = preferencesMap.get_device_parameters(device)
+	for id, value in pairs(parameters) do
 		local new_parameter_value = preferencesMap.to_numeric_value(device.preferences[id])
-		if new_parameter_value ~= value and preferences then
-			local pref = preferences[id]
+		if new_parameter_value ~= value and parameters then
+			local pref = parameters[id]
 			local new_parameter_value = preferencesMap.to_numeric_value(device.preferences[id])
 			device:send(Configuration:Set({
 				parameter_number = pref.parameter_number,
@@ -80,7 +47,6 @@ end
 
 local leviton_zwxxx = {
 	lifecycle_handlers = {
-		init = init_dev,
 		infoChanged = info_changed,
 		added = device_added,
 	},
