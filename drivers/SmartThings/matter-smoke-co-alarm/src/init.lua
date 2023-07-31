c2300a26d86cc00266a6f38d0f41e3bc828c0145
Youngho Yoon <34558998+yhoyoon@users.noreply.github.com>
2023-07-31 19:40:48 +0900

    Implement custom cluster version smoke-co-alarm
    Signed-off-by: Youngho Yoon <34558998+yhoyoon@users.noreply.github.com>

--- drivers/SmartThings/matter-smoke-co-alarm/src/init.lua
+++ drivers/SmartThings/matter-smoke-co-alarm/src/init.lua
@@ -19,13 +19,30 @@ local clusters = require "st.matter.clusters"
 local log = require "log"
 local utils = require "st.utils"
 
+local batteryAlertID = "spacewonder52282.batteryAlert"
+local deviceMutedID = "spacewonder52282.deviceMuted"
+local endOfServiceAlertID = "spacewonder52282.endOfServiceAlert"
+local expressedStateID = "spacewonder52282.expressedState"
+local batteryAlert = capabilities[batteryAlertID]
+local deviceMuted = capabilities[deviceMutedID]
+local endOfServiceAlert = capabilities[endOfServiceAlertID]
+local expressedState = capabilities[expressedStateID]
+
 local function device_init(driver, device)
   device:subscribe()
 end
 
 local function device_added(driver, device)
-  -- device:send(clusters.Thermostat.attributes.ControlSequenceOfOperation:read(device))
-  -- device:send(clusters.FanControl.attributes.FanModeSequence:read(device))
+  device:send(clusters.SmokeCoAlarm.attributes.ExpressedState:read(device))
+  device:send(clusters.SmokeCoAlarm.attributes.SmokeState:read(device))
+  device:send(clusters.SmokeCoAlarm.attributes.COState:read(device))
+  device:send(clusters.SmokeCoAlarm.attributes.BatteryAlert:read(device))
+  device:send(clusters.SmokeCoAlarm.attributes.DeviceMuted:read(device))
+  device:send(clusters.SmokeCoAlarm.attributes.TestInProgress:read(device))
+  device:send(clusters.SmokeCoAlarm.attributes.HardwareFaultAlert:read(device))
+  device:send(clusters.SmokeCoAlarm.attributes.EndOfServiceAlert:read(device))
+  device:send(clusters.TemperatureMeasurement.attributes.MeasuredValue:read(device))
+  device:send(clusters.RelativeHumidityMeasurement.attributes.MeasuredValue:read(device))
 end
 
 -- Matter Handlers --
@@ -42,21 +59,74 @@ local function humidity_attr_handler(driver, device, ib, response)
   device:emit_event_for_endpoint(ib.endpoint_id, capabilities.relativeHumidityMeasurement.humidity(humidity))
 end
 
+local function expressed_state_event_handler(driver, device, ib, response)
+  local state = ib.data.value
+  if state == 0 then -- Normal
+    device:emit_event_for_endpoint(ib.endpoint_id, expressedState.expressedState.normal())
+  elseif state == 1 then -- SmokeAlarm
+    device:emit_event_for_endpoint(ib.endpoint_id, expressedState.expressedState.smokeAlarm())
+  elseif state == 2 then -- COAlarm
+    device:emit_event_for_endpoint(ib.endpoint_id, expressedState.expressedState.coAlarm())
+  elseif state == 3 then -- BatteryAlert
+    device:emit_event_for_endpoint(ib.endpoint_id, expressedState.expressedState.batteryAlert())
+  elseif state == 4 then -- Testing
+    device:emit_event_for_endpoint(ib.endpoint_id, expressedState.expressedState.testing())
+  elseif state == 5 then -- HardwareFault
+    device:emit_event_for_endpoint(ib.endpoint_id, expressedState.expressedState.hardwareFault())
+  elseif state == 6 then -- EndOfService
+    device:emit_event_for_endpoint(ib.endpoint_id, expressedState.expressedState.endOfService())
+  elseif state == 7 then -- InterconnectSmoke
+    device:emit_event_for_endpoint(ib.endpoint_id, expressedState.expressedState.interconnectSmoke())
+  elseif state == 8 then -- InterconnectCO
+    device:emit_event_for_endpoint(ib.endpoint_id, expressedState.expressedState.interconnectCO())
+  end
+end
+
 local function smoke_state_event_handler(driver, device, ib, response)
   local state = ib.data.value
-  if state == 0 then -- normal
+  if state == 0 then -- Normal
     device:emit_event_for_endpoint(ib.endpoint_id, capabilities.smokeDetector.smoke.clear())
-  elseif state == 1 or state == 2 then -- warning or critical
+  elseif state == 1 or state == 2 then -- Warning or Critical
     device:emit_event_for_endpoint(ib.endpoint_id, capabilities.smokeDetector.smoke.detected())
   end
 end
 
 local function co_state_event_handler(driver, device, ib, response)
   local state = ib.data.value
-  if state == 0 then -- normal
+  if state == 0 then -- Normal
     device:emit_event_for_endpoint(ib.endpoint_id, capabilities.smokeDetector.carbonMonoxideDetector.clear())
-  elseif state == 1 or state == 2 then -- warning or critical
+  elseif state == 1 or state == 2 then -- Warning or Critical
     device:emit_event_for_endpoint(ib.endpoint_id, capabilities.smokeDetector.carbonMonoxideDetector.detected())
+  end
+end
+
+local function battery_alert_event_handler(driver, device, ib, response)
+  local state = ib.data.value
+  if state == 0 then -- Normal
+    device:emit_event_for_endpoint(ib.endpoint_id, batteryAlert.batteryAlert.normal())
+  elseif state == 1 then -- Warning
+    device:emit_event_for_endpoint(ib.endpoint_id, batteryAlert.batteryAlert.warning())
+  elseif state == 2 then -- Critical
+    device:emit_event_for_endpoint(ib.endpoint_id, batteryAlert.batteryAlert.critical())
+  end
+end
+
+local function device_muted_event_handler(driver, device, ib, response)
+  local state = ib.data.value
+  if state == 0 then -- NotMuted
+    device:emit_event_for_endpoint(ib.endpoint_id, deviceMuted.deviceMuted.notMuted())
+  elseif state == 1 then -- Muted
+    device:emit_event_for_endpoint(ib.endpoint_id, deviceMuted.deviceMuted.muted())
+  end
+end
+
+local function test_in_progress_event_handler(driver, device, ib, response)
+  if ib.data.value then
+    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.smokeDetector.smoke.tested())
+    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.smokeDetector.carbonMonoxideDetector.tested())
+  else
+    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.smokeDetector.smoke.clear())
+    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.smokeDetector.carbonMonoxideDetector.clear())
   end
 end
 
@@ -68,13 +138,12 @@ local function hardware_fault_alert_event_handler(driver, device, ib, response)
   end
 end
 
-local function test_in_progress_event_handler(driver, device, ib, response)
-  if ib.data.value then
-    device:emit_event_for_endpoint(ib.endpoint_id, ccapabilities.smokeDetector.smoke.tested())
-    device:emit_event_for_endpoint(ib.endpoint_id, ccapabilities.smokeDetector.carbonMonoxideDetector.tested())
-  else
-    device:emit_event_for_endpoint(ib.endpoint_id, ccapabilities.smokeDetector.smoke.clear())
-    device:emit_event_for_endpoint(ib.endpoint_id, ccapabilities.smokeDetector.carbonMonoxideDetector.clear())
+local function end_of_service_alert_event_handler(driver, device, ib, response)
+  local state = ib.data.value
+  if state == 0 then -- Normal
+    device:emit_event_for_endpoint(ib.endpoint_id, endOfServiceAlert.endOfServiceAlert.normal())
+  elseif state == 1 then -- Expired
+    device:emit_event_for_endpoint(ib.endpoint_id, endOfServiceAlert.endOfServiceAlert.expired())
   end
 end
 
@@ -86,10 +155,14 @@ local matter_driver_template = {
   matter_handlers = {
     attr = {
       [clusters.SmokeCoAlarm.ID] = {
+        [clusters.SmokeCoAlarm.attributes.ExpressedState.ID] = expressed_state_event_handler,
         [clusters.SmokeCoAlarm.attributes.SmokeState.ID] = smoke_state_event_handler,
         [clusters.SmokeCoAlarm.attributes.COState.ID] = co_state_event_handler,
+        [clusters.SmokeCoAlarm.attributes.BatteryAlert.ID] = battery_alert_event_handler,
+        [clusters.SmokeCoAlarm.attributes.DeviceMuted.ID] = device_muted_event_handler,
         [clusters.SmokeCoAlarm.attributes.TestInProgress.ID] = test_in_progress_event_handler,
         [clusters.SmokeCoAlarm.attributes.HardwareFaultAlert.ID] = hardware_fault_alert_event_handler,
+        [clusters.SmokeCoAlarm.attributes.EndOfServiceAlert.ID] = end_of_service_alert_event_handler,
       },
       [clusters.TemperatureMeasurement.ID] = {
         [clusters.TemperatureMeasurement.attributes.MeasuredValue.ID] = temp_event_handler(capabilities.temperatureMeasurement.temperature),
@@ -100,6 +173,9 @@ local matter_driver_template = {
     }
   },
   subscribed_attributes = {
+    [expressedStateID] = {
+      clusters.SmokeCoAlarm.attributes.ExpressedState.ID
+    },
     [capabilities.smokeDetector.ID] = {
       clusters.SmokeCoAlarm.attributes.SmokeState,
       clusters.SmokeCoAlarm.attributes.TestInProgress,
@@ -108,8 +184,17 @@ local matter_driver_template = {
       clusters.SmokeCoAlarm.attributes.COState,
       clusters.SmokeCoAlarm.attributes.TestInProgress, -- is it possible?
     },
+    [batteryAlertID] = {
+      clusters.SmokeCoAlarm.attributes.BatteryAlert.ID
+    },
+    [deviceMutedID] = {
+      clusters.SmokeCoAlarm.attributes.DeviceMuted.ID
+    },
     [capabilities.tamperAlert.ID] = {
       clusters.SmokeCoAlarm.attributes.HardwareFaultAlert
+    },
+    [endOfServiceAlertID] = {
+      clusters.SmokeCoAlarm.attributes.EndOfServiceAlert.ID
     },
     [capabilities.temperatureMeasurement.ID] = {
       clusters.Thermostat.attributes.LocalTemperature,
