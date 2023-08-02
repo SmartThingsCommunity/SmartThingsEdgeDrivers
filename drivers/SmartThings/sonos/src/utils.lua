2dad6ed90d1c5d4b15ce0f3cf4a0b341fdc3cb77
Doug Stephen <doug.stephen@smartthings.com>
2023-08-02 13:37:45 -0500

    fix: Periodic SSDP scan to detect IP changes
    Similarly to Hue, this adds a periodic scan for detecting changes in IP address
    at runtime after a device has onboarded.
    
    This refactor isn't as surgical as the Hue one because the Sonos topology is
    simpler, so we didn't do a deep gutting of the discovery logic and left it
    mostly untouched, so we still actively perform SSDP scans during discovery.
    
    Relaxing this for Sonos is likely best left to a refactor after we move the
    SSDP APIs in to hubcore and expose them via the lua libs.
    
    Fixes: CHAD-11472

--- drivers/SmartThings/sonos/src/utils.lua
+++ drivers/SmartThings/sonos/src/utils.lua
@@ -93,4 +93,40 @@ function utils.labeled_socket_builder(label)
   return make_socket
 end
 
+--- From https://gist.github.com/sapphyrus/fd9aeb871e3ce966cc4b0b969f62f539
+--- MIT licensed
+function utils.deep_table_eq(tbl1, tbl2)
+  if tbl1 == tbl2 then
+    return true
+  elseif type(tbl1) == "table" and type(tbl2) == "table" then
+    for key1, value1 in pairs(tbl1) do
+      local value2 = tbl2[key1]
+
+      if value2 == nil then
+        -- avoid the type call for missing keys in tbl2 by directly comparing with nil
+        return false
+      elseif value1 ~= value2 then
+        if type(value1) == "table" and type(value2) == "table" then
+          if not utils.deep_table_eq(value1, value2) then
+            return false
+          end
+        else
+          return false
+        end
+      end
+    end
+
+    -- check for missing keys in tbl1
+    for key2, _ in pairs(tbl2) do
+      if tbl1[key2] == nil then
+        return false
+      end
+    end
+
+    return true
+  end
+
+  return false
+end
+
 return utils
