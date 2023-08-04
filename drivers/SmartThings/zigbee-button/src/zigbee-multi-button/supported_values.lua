b72f1646520e72de3a6814eca45751f49d51a9ef
Liu Yiding <liuyd.fnst@fujitsu.com>
2023-08-04 13:20:17 +0800

    Add HEIMAN HS6SSB-W-EF-3.0 Scene Panel
    Signed-off-by: Liu Yiding <liuyd.fnst@fujitsu.com>

--- drivers/SmartThings/zigbee-button/src/zigbee-multi-button/supported_values.lua
+++ drivers/SmartThings/zigbee-button/src/zigbee-multi-button/supported_values.lua
@@ -46,7 +46,8 @@ local devices = {
   },
   BUTTON_PUSH_3 = {
     MATCHING_MATRIX = {
-      { mfr = "SOMFY", model = "Situo 1 Zigbee" }
+      { mfr = "SOMFY", model = "Situo 1 Zigbee" },
+      { mfr = "HEIMAN", model = "HS6SSB-W-EF-3.0" },
     },
     SUPPORTED_BUTTON_VALUES = { "pushed" },
     NUMBER_OF_BUTTONS = 3
