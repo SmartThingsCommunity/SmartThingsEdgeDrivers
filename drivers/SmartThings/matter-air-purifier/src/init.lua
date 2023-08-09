416517ca5452c8c53c8df0cf8d2bbe632a81ffad
Youngho Yoon <34558998+yhoyoon@users.noreply.github.com>
2023-08-09 18:44:43 +0900

    Add matter-air-purifier skeleton
    Signed-off-by: Youngho Yoon <34558998+yhoyoon@users.noreply.github.com>

---
+++ drivers/SmartThings/matter-air-purifier/src/init.lua
@@ -0,0 +1,43 @@
+-- Copyright 2023 SmartThings
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+local MatterDriver = require "st.matter.driver"
+local capabilities = require "st.capabilities"
+local clusters = require "st.matter.clusters"
+
+local log = require "log"
+
+local function device_init(driver, device)
+  device:subscribe()
+end
+
+-- Matter Handlers --
+
+local matter_driver_template = {
+  lifecycle_handlers = {
+    init = device_init,
+  },
+  matter_handlers = {
+    attr = {
+    }
+  },
+  subscribed_attributes = {
+  },
+  capability_handlers = {
+  },
+}
+
+local matter_driver = MatterDriver("matter-air-purifier", matter_driver_template)
+log.info_with({hub_logs=true}, string.format("Starting %s driver, with dispatcher: %s", matter_driver.NAME, matter_driver.matter_dispatcher))
+matter_driver:run()
