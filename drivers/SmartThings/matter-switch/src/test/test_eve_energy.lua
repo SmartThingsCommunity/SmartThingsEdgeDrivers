d2b3c73d4077c89b60cdeb71392580000542ec5f
Alexandre Colucci <git-no-reply@colucci.fr>
2023-09-01 11:41:01 +0200

    Add unit test to check that the timer created in create_poll_schedule triggers the function requestData(). This function will read the standard and custom attributes from the device.

--- drivers/SmartThings/matter-switch/src/test/test_eve_energy.lua
+++ drivers/SmartThings/matter-switch/src/test/test_eve_energy.lua
@@ -17,7 +17,9 @@ local capabilities = require "st.capabilities"
 local t_utils = require "integration_test.utils"
 
 local clusters = require "st.matter.clusters"
+local cluster_base = require "st.matter.cluster_base"
 
+local PRIVATE_CLUSTER_ID = 0x130AFC01
 local PRIVATE_ATTR_ID_WATT = 0x130A000A
 local PRIVATE_ATTR_ID_WATT_ACCUMULATED = 0x130A000B
 
@@ -42,6 +44,12 @@ local mock_device = test.mock_device.build_test_matter_device({
       clusters = {
         {
           cluster_id = clusters.OnOff.ID,
+          cluster_type = "SERVER",
+          cluster_revision = 1,
+          feature_map = 0, --u32 bitmap
+        },
+        {
+          cluster_id = PRIVATE_CLUSTER_ID,
           cluster_type = "SERVER",
           cluster_revision = 1,
           feature_map = 0, --u32 bitmap
@@ -123,7 +131,44 @@ test.register_coroutine_test(
     test.socket.capability:__expect_send(
       mock_device:generate_test_message("main", capabilities.energyMeter.energy({ value = 0.0, unit = "Wh" }))
     )
+
+    test.wait_for_events()
   end
+)
+
+
+test.register_coroutine_test(
+  "Check that the timer created in create_poll_schedule properly reads the device in requestData",
+  function()
+    test.mock_time.advance_time(60000) -- Ensure that the timer created in create_poll_schedule triggers
+    test.socket.matter:__set_channel_ordering("relaxed")
+
+    test.socket.matter:__expect_send({ mock_device.id, clusters.OnOff.attributes.OnOff:read(mock_device) })
+    test.socket.matter:__expect_send({ mock_device.id,
+      cluster_base.read(mock_device, 0x01, PRIVATE_CLUSTER_ID, PRIVATE_ATTR_ID_WATT, nil) })
+    test.socket.matter:__expect_send({ mock_device.id,
+      cluster_base.read(mock_device, 0x01, PRIVATE_CLUSTER_ID, PRIVATE_ATTR_ID_WATT_ACCUMULATED, nil) })
+
+    test.wait_for_events()
+  end,
+  {
+    test_init = function()
+      local cluster_subscribe_list = {
+        clusters.OnOff.attributes.OnOff,
+      }
+      test.socket.matter:__set_channel_ordering("relaxed")
+      local subscribe_request = cluster_subscribe_list[1]:subscribe(mock_device)
+      for i, cluster in ipairs(cluster_subscribe_list) do
+        if i > 1 then
+          subscribe_request:merge(cluster:subscribe(mock_device))
+        end
+      end
+      test.socket.matter:__expect_send({ mock_device.id, subscribe_request })
+      test.mock_device.add_test_device(mock_device)
+
+      test.timer.__create_and_queue_test_time_advance_timer(60, "interval", "create_poll_schedule")
+    end
+  }
 )
 
 test.run_registered_tests()
