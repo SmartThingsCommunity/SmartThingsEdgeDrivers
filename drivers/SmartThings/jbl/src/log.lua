9822df587e5aed8ecc1e16902e56526e353f78c9
Doug Stephen <doug.stephen@smartthings.com>
2023-09-22 12:06:50 -0500

    Ignore _envlibrequire in luacheck

--- drivers/SmartThings/jbl/src/log.lua
+++ drivers/SmartThings/jbl/src/log.lua
@@ -12,6 +12,8 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
+-- luacheck: globals _envlibrequire
+
 local original_log
 if _envlibrequire ~= nil then
   original_log = _envlibrequire("log")
