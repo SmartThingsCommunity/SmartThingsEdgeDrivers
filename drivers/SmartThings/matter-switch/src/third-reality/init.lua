a0f02b4db9d127694f47a94e865eb303564cd02c
Zhangyi <zhangyi@3reality.com>
2023-11-08 18:17:17 +0800

    rename profile file name -nl to illuminance-motion clean third-reality/init useless code

--- drivers/SmartThings/matter-switch/src/third-reality/init.lua
+++ drivers/SmartThings/matter-switch/src/third-reality/init.lua
@@ -1,4 +1,4 @@
--- Copyright 2022 SmartThings
+-- Copyright 2023 SmartThings
 --
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
@@ -18,20 +18,6 @@ local clusters = require "st.matter.clusters"
 local MatterDriver = require "st.matter.driver"
 local utils = require "st.utils"
 
-local MOST_RECENT_TEMP = "mostRecentTemp"
-local RECEIVED_X = "receivedX"
-local RECEIVED_Y = "receivedY"
-local HUESAT_SUPPORT = "huesatSupport"
-local CONVERSION_CONSTANT = 1000000
--- These values are taken from the min/max definined in the colorTemperature capability
-local COLOR_TEMPERATURE_KELVIN_MAX = 30000
-local COLOR_TEMPERATURE_KELVIN_MIN = 1
-local COLOR_TEMPERATURE_MIRED_MAX = CONVERSION_CONSTANT/COLOR_TEMPERATURE_KELVIN_MIN
-local COLOR_TEMPERATURE_MIRED_MIN = CONVERSION_CONSTANT/COLOR_TEMPERATURE_KELVIN_MAX
-
-local COMPONENT_TO_ENDPOINT_MAP = "__component_to_endpoint_map"
--- New profiles need to be added for devices that have more switch endpoints
-local MAX_MULTI_SWITCH_EPS = 7
 local THIRD_REALITY_MANUFACTURER_ID = 0x1407
 
 
@@ -43,115 +29,6 @@ local function is_third_reality_products(opts, driver, device)
   return false
 end
 
-local function convert_huesat_st_to_matter(val)
-  return math.floor((val * 0xFE) / 100.0 + 0.5)
-end
-
---- component_to_endpoint helper function to handle situations where
---- device does not have endpoint ids in sequential order from 1
---- In this case the function returns the lowest endpoint value that isn't 0
---- and supports the OnOff cluster. This is done to bypass the
---- BRIDGED_NODE_DEVICE_TYPE on bridged devices
-local function find_default_endpoint(device, component)
-  local res = device.MATTER_DEFAULT_ENDPOINT
-  local eps = device:get_endpoints(clusters.OnOff.ID)
-  table.sort(eps)
-  for _, v in ipairs(eps) do
-    if v ~= 0 then --0 is the matter RootNode endpoint
-      res = v
-      break
-    end
-  end
-  device.log.warn(string.format("Did not find default endpoint, will use endpoint %d instead", device.MATTER_DEFAULT_ENDPOINT))
-  return res
-end
-
-local function initialize_switch(device)
-  local switch_eps = device:get_endpoints(clusters.OnOff.ID)
-  table.sort(switch_eps)
-  local component_map = {}
-
-  -- For switch devices, the profile components follow the naming convention "switch%d",
-  -- with the exception of "main" being the first component. Each component will then map
-  -- to the next lowest endpoint that hasn't been mapped yet.
-  for i, ep in ipairs(switch_eps) do
-    if i == 1 then
-      component_map["main"] = ep
-    else
-      component_map[string.format("switch%d", i)] = ep
-    end
-  end
-
-  device:set_field(COMPONENT_TO_ENDPOINT_MAP, component_map, {persist = true})
-  -- Note: This profile switching is needed because of shortcoming in the generic fingerprints
-  -- where devices with multiple endpoints with the same device type cannot be detected
-  local num_switch_eps = #switch_eps
-  if num_switch_eps > 1 then
-    device:try_update_metadata({profile = string.format("switch-%d", math.min(num_switch_eps, MAX_MULTI_SWITCH_EPS))})
-  end
-  if num_switch_eps > MAX_MULTI_SWITCH_EPS then
-    error(string.format(
-      "Matter multi switch device will have limited function. Profile doesn't exist with %d components, max is %d",
-      num_switch_eps,
-      MAX_MULTI_SWITCH_EPS
-    ))
-  end
-end
-
-local function component_to_endpoint(device, component)
-  local map = device:get_field(COMPONENT_TO_ENDPOINT_MAP) or {}
-  if map[component] then
-    return map[component]
-  end
-  return find_default_endpoint(device, component)
-end
-
-local function endpoint_to_component(device, ep)
-  local map = device:get_field(COMPONENT_TO_ENDPOINT_MAP) or {}
-  for component, endpoint in pairs(map) do
-    if endpoint == ep then
-       return component
-    end
-  end
-  log.warn_with({hub_logs = true}, string.format("Device has more than supported number of switches (max %d), mapping excess endpoint to main component", MAX_MULTI_SWITCH_EPS))
-  return "main"
-end
-
-local function device_init(driver, device)
-  log.info_with({hub_logs=true}, "device init")
-  if not device:get_field(COMPONENT_TO_ENDPOINT_MAP) then
-    -- create endpoint to component map and switch profile as needed
-    initialize_switch(device)
-  end
-  device:set_component_to_endpoint_fn(component_to_endpoint)
-  device:set_endpoint_to_component_fn(endpoint_to_component)
-  device:subscribe()
-end
-
-local function device_removed(driver, device)
-  log.info("device removed")
-end
-
-local function handle_switch_on(driver, device, cmd)
-  local endpoint_id = device:component_to_endpoint(cmd.component)
-  --TODO use OnWithRecallGlobalScene for devices with the LT feature
-  local req = clusters.OnOff.server.commands.On(device, endpoint_id)
-  device:send(req)
-end
-
-local function handle_switch_off(driver, device, cmd)
-  local endpoint_id = device:component_to_endpoint(cmd.component)
-  local req = clusters.OnOff.server.commands.Off(device, endpoint_id)
-  device:send(req)
-end
-
-local function handle_set_level(driver, device, cmd)
-  local endpoint_id = device:component_to_endpoint(cmd.component)
-  local level = math.floor(cmd.args.level/100.0 * 254)
-  local req = clusters.LevelControl.server.commands.MoveToLevelWithOnOff(device, endpoint_id, level, cmd.args.rate or 0, 0 ,0)
-  device:send(req)
-end
-
 local function illuminance_attr_handler(driver, device, ib, response)
   local lux = math.floor(10 ^ ((ib.data.value - 1) / 10000))
   device:emit_event_for_endpoint(ib.endpoint_id, capabilities.illuminanceMeasurement.illuminance(lux))
@@ -161,188 +38,10 @@ local function occupancy_attr_handler(driver, device, ib, response)
   device:emit_event(ib.data.value == 0x01 and capabilities.motionSensor.motion.active() or capabilities.motionSensor.motion.inactive())
 end
 
---TODO could be moved to st.utils if made more generally useful
-local tbl_contains = function(t, val)
-  for _, v in pairs(t) do
-    if v == val then
-      return true
-    end
-  end
-  return false
-end
-
-local TRANSITION_TIME = 0 --1/10ths of a second
--- When sent with a command, these options mask and override bitmaps cause the command
--- to take effect when the switch/light is off.
-local OPTIONS_MASK = 0x01
-local OPTIONS_OVERRIDE = 0x01
-
-local function handle_set_color(driver, device, cmd)
-  local endpoint_id = device:component_to_endpoint(cmd.component)
-  local req
-  local huesat_endpoints = device:get_endpoints(clusters.ColorControl.ID, {feature_bitmap = clusters.ColorControl.FeatureMap.HUE_AND_SATURATION})
-  if tbl_contains(huesat_endpoints, endpoint_id) then
-    local hue = convert_huesat_st_to_matter(cmd.args.color.hue)
-    local sat = convert_huesat_st_to_matter(cmd.args.color.saturation)
-    req = clusters.ColorControl.server.commands.MoveToHueAndSaturation(device, endpoint_id, hue, sat, TRANSITION_TIME, OPTIONS_MASK, OPTIONS_OVERRIDE)
-  else
-    local x, y, _ = utils.safe_hsv_to_xy(cmd.args.color.hue, cmd.args.color.saturation)
-    req = clusters.ColorControl.server.commands.MoveToColor(device, endpoint_id, x, y, TRANSITION_TIME, OPTIONS_MASK, OPTIONS_OVERRIDE)
-  end
-  device:send(req)
-end
-
-local function handle_set_hue(driver, device, cmd)
-  local endpoint_id = device:component_to_endpoint(cmd.component)
-  local huesat_endpoints = device:get_endpoints(clusters.ColorControl.ID, {feature_bitmap = clusters.ColorControl.FeatureMap.HUE_AND_SATURATION})
-  if tbl_contains(huesat_endpoints, endpoint_id) then
-    local hue = convert_huesat_st_to_matter(cmd.args.hue)
-    local req = clusters.ColorControl.server.commands.MoveToHue(device, endpoint_id, hue, 0, TRANSITION_TIME, OPTIONS_MASK, OPTIONS_OVERRIDE)
-    device:send(req)
-  else
-    log.warn("Device does not support huesat features on its color control cluster")
- end
-end
-
-local function handle_set_saturation(driver, device, cmd)
-  local endpoint_id = device:component_to_endpoint(cmd.component)
-  local huesat_endpoints = device:get_endpoints(clusters.ColorControl.ID, {feature_bitmap = clusters.ColorControl.FeatureMap.HUE_AND_SATURATION})
-  if tbl_contains(huesat_endpoints, endpoint_id) then
-    local sat = convert_huesat_st_to_matter(cmd.args.saturation)
-    local req = clusters.ColorControl.server.commands.MoveToSaturation(device, endpoint_id, sat, TRANSITION_TIME, OPTIONS_MASK, OPTIONS_OVERRIDE)
-    device:send(req)
-  else
-    log.warn("Device does not support huesat features on its color control cluster")
-  end
-end
-
-local function handle_set_color_temperature(driver, device, cmd)
-  local endpoint_id = device:component_to_endpoint(cmd.component)
-  local temp_in_mired = utils.round(CONVERSION_CONSTANT/cmd.args.temperature)
-  local req = clusters.ColorControl.server.commands.MoveToColorTemperature(device, endpoint_id, temp_in_mired, TRANSITION_TIME, OPTIONS_MASK, OPTIONS_OVERRIDE)
-  device:set_field(MOST_RECENT_TEMP, cmd.args.temperature)
-  device:send(req)
-end
-
-local function handle_refresh(driver, device, cmd)
-  --Note: no endpoint specified indicates a wildcard endpoint
-  local req = clusters.OnOff.attributes.OnOff:read(device)
-  device:send(req)
-end
-
--- Fallback handler for responses that dont have their own handler
-local function matter_handler(driver, device, response_block)
-  log.info(string.format("Fallback handler for %s", response_block))
-end
-
-local function on_off_attr_handler(driver, device, ib, response)
-  if ib.data.value then
-    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.switch.switch.on())
-  else
-    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.switch.switch.off())
-  end
-end
-
-local function level_attr_handler(driver, device, ib, response)
-  if ib.data.value ~= nil then
-    local level = math.floor((ib.data.value / 254.0 * 100) + 0.5)
-    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.switchLevel.level(level))
-  end
-end
-
-local function hue_attr_handler(driver, device, ib, response)
-  if ib.data.value ~= nil then
-    local hue = math.floor((ib.data.value / 0xFE * 100) + 0.5)
-    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.colorControl.hue(hue))
-  end
-end
-
-local function sat_attr_handler(driver, device, ib, response)
-  if ib.data.value ~= nil then
-    local sat = math.floor((ib.data.value / 0xFE * 100) + 0.5)
-    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.colorControl.saturation(sat))
-  end
-end
-
-local function temp_attr_handler(driver, device, ib, response)
-  if ib.data.value ~= nil then
-    if (ib.data.value < COLOR_TEMPERATURE_MIRED_MIN or ib.data.value > COLOR_TEMPERATURE_MIRED_MAX) then
-      device.log.warn_with({hub_logs = true}, "Device reported color temperature %d mired outside of supported capability range", ib.data.value)
-      return
-    end
-    local temp = utils.round(CONVERSION_CONSTANT/ib.data.value)
-    local most_recent_temp = device:get_field(MOST_RECENT_TEMP)
-    -- this is to avoid rounding errors from the round-trip conversion of Kelvin to mireds
-    if most_recent_temp ~= nil and
-      most_recent_temp <= utils.round(CONVERSION_CONSTANT/(ib.data.value - 1)) and
-      most_recent_temp >= utils.round(CONVERSION_CONSTANT/(ib.data.value + 1)) then
-        temp = most_recent_temp
-    end
-    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.colorTemperature.colorTemperature(temp))
-  end
-end
-
-local color_utils = require "color_utils"
-
-local function x_attr_handler(driver, device, ib, response)
-  local y = device:get_field(RECEIVED_Y)
-  --TODO it is likely that both x and y attributes are in the response (not guaranteed though)
-  -- if they are we can avoid setting fields on the device.
-  if y == nil then
-    device:set_field(RECEIVED_X, ib.data.value)
-  else
-    local x = ib.data.value
-    local h, s, _ = color_utils.safe_xy_to_hsv(x, y)
-    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.colorControl.hue(h))
-    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.colorControl.saturation(s))
-    device:set_field(RECEIVED_Y, nil)
-  end
-end
-
-local function y_attr_handler(driver, device, ib, response)
-  local x = device:get_field(RECEIVED_X)
-  if x == nil then
-    device:set_field(RECEIVED_Y, ib.data.value)
-  else
-    local y = ib.data.value
-    local h, s, _ = color_utils.safe_xy_to_hsv(x, y)
-    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.colorControl.hue(h))
-    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.colorControl.saturation(s))
-    device:set_field(RECEIVED_X, nil)
-  end
-end
-
---TODO setup configure handler to read this attribute.
-local function color_cap_attr_handler(driver, device, ib, response)
-  if ib.data.value ~= nil then
-    if ib.data.value & 0x1 then
-      device:set_field(HUESAT_SUPPORT, true)
-    end
-  end
-end
-
 local third_reality_handler = {
   NAME = "Third Reality Handler",
-  lifecycle_handlers = {
-    init = device_init,
-    removed = device_removed
-  },
   matter_handlers = {
     attr = {
-      [clusters.OnOff.ID] = {
-        [clusters.OnOff.attributes.OnOff.ID] = on_off_attr_handler,
-      },
-      [clusters.LevelControl.ID] = {
-        [clusters.LevelControl.attributes.CurrentLevel.ID] = level_attr_handler
-      },
-      [clusters.ColorControl.ID] = {
-        [clusters.ColorControl.attributes.CurrentHue.ID] = hue_attr_handler,
-        [clusters.ColorControl.attributes.CurrentSaturation.ID] = sat_attr_handler,
-        [clusters.ColorControl.attributes.ColorTemperatureMireds.ID] = temp_attr_handler,
-        [clusters.ColorControl.attributes.CurrentX.ID] = x_attr_handler,
-        [clusters.ColorControl.attributes.CurrentY.ID] = y_attr_handler,
-        [clusters.ColorControl.attributes.ColorCapabilities.ID] = color_cap_attr_handler,
-      },
       [clusters.IlluminanceMeasurement.ID] = {
         [clusters.IlluminanceMeasurement.attributes.MeasuredValue.ID] = illuminance_attr_handler
       },
@@ -350,24 +49,8 @@ local third_reality_handler = {
         [clusters.OccupancySensing.attributes.Occupancy.ID] = occupancy_attr_handler,
       }
     },
-    fallback = matter_handler,
   },
   subscribed_attributes = {
-    [capabilities.switch.ID] = {
-      clusters.OnOff.attributes.OnOff
-    },
-    [capabilities.switchLevel.ID] = {
-      clusters.LevelControl.attributes.CurrentLevel
-    },
-    [capabilities.colorControl.ID] = {
-      clusters.ColorControl.attributes.CurrentHue,
-      clusters.ColorControl.attributes.CurrentSaturation,
-      clusters.ColorControl.attributes.CurrentX,
-      clusters.ColorControl.attributes.CurrentY,
-    },
-    [capabilities.colorTemperature.ID] = {
-      clusters.ColorControl.attributes.ColorTemperatureMireds,
-    },
     [capabilities.illuminanceMeasurement.ID] = {
       clusters.IlluminanceMeasurement.attributes.MeasuredValue
     },
@@ -375,31 +58,7 @@ local third_reality_handler = {
       clusters.OccupancySensing.attributes.Occupancy
     }
   },
-  capability_handlers = {
-    [capabilities.switch.ID] = {
-      [capabilities.switch.commands.on.NAME] = handle_switch_on,
-      [capabilities.switch.commands.off.NAME] = handle_switch_off,
-    },
-    [capabilities.switchLevel.ID] = {
-      [capabilities.switchLevel.commands.setLevel.NAME] = handle_set_level
-    },
-    [capabilities.refresh.ID] = {
-      [capabilities.refresh.commands.refresh.NAME] = handle_refresh,
-    },
-    [capabilities.colorControl.ID] = {
-      [capabilities.colorControl.commands.setColor.NAME] = handle_set_color,
-      [capabilities.colorControl.commands.setHue.NAME] = handle_set_hue,
-      [capabilities.colorControl.commands.setSaturation.NAME] = handle_set_saturation,
-    },
-    [capabilities.colorTemperature.ID] = {
-      [capabilities.colorTemperature.commands.setColorTemperature.NAME] = handle_set_color_temperature,
-    }
-  },
   supported_capabilities = {
-    capabilities.switch,
-    capabilities.switchLevel,
-    capabilities.colorControl,
-    capabilities.colorTemperature,
     capabilities.motionSensor,
     capabilities.illuminanceMeasurement
   },
