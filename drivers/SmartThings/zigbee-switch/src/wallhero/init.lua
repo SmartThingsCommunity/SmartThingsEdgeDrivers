0249339cc6d74b51bf9a9386ffe3646d2a791e35
thinkaName <144081204+thinkaName@users.noreply.github.com>
2023-11-09 10:23:32 +0800

    wallhero add model ACL-401S3I (#980)
    Co-authored-by: lelandblue <79465613+lelandblue@users.noreply.github.com>

--- drivers/SmartThings/zigbee-switch/src/wallhero/init.lua
+++ drivers/SmartThings/zigbee-switch/src/wallhero/init.lua
@@ -22,6 +22,7 @@ local Scenes = zcl_clusters.Scenes
 local FINGERPRINTS = {
   { mfr = "WALL HERO", model = "ACL-401S4I", switches = 4, buttons = 0 },
   { mfr = "WALL HERO", model = "ACL-401S8I", switches = 4, buttons = 4 },
+  { mfr = "WALL HERO", model = "ACL-401S3I", switches = 3, buttons = 0 },
   { mfr = "WALL HERO", model = "ACL-401S2I", switches = 2, buttons = 0 }
 }
 
