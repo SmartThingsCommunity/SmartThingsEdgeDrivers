2b2a02bd7927f327f37b9d42abe0c0b58c5e46e3
Ma Xinjian <maxj.fnst@fujitsu.com>
2024-03-28 17:38:58 +0800

    Move write_manufacturer_specific_attribute from added to doConfigure event
    fix issue that motion detection doesn't work after reonboarding
    
    https://smartthings.atlassian.net/browse/WASH-2068
    
    Signed-off-by: Ma Xinjian <maxj.fnst@fujitsu.com>

--- drivers/SmartThings/zigbee-motion-sensor/src/aqara/high-precision-motion/init.lua
+++ drivers/SmartThings/zigbee-motion-sensor/src/aqara/high-precision-motion/init.lua
@@ -83,7 +83,9 @@ local function added_handler(self, device)
   device:emit_event(detectionFrequency.detectionFrequency(aqara_utils.PREF_FREQUENCY_VALUE_DEFAULT, {visibility = {displayed = false}}))
   device:emit_event(sensitivityAdjustment.sensitivityAdjustment.Medium())
   device:emit_event(capabilities.battery.battery(100))
+end
 
+local function do_configure(self, device)
   device:send(cluster_base.write_manufacturer_specific_attribute(device, aqara_utils.PRIVATE_CLUSTER_ID,
     aqara_utils.PRIVATE_ATTRIBUTE_ID,
     aqara_utils.MFG_CODE, data_types.Uint8, 1))
@@ -92,7 +94,8 @@ end
 local aqara_high_precision_motion_handler = {
   NAME = "Aqara High Precision Motion Handler",
   lifecycle_handlers = {
-    added = added_handler
+    added = added_handler,
+    doConfigure = do_configure
   },
   capability_handlers = {
     [sensitivityAdjustment.ID] = {
