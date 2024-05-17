2df4f7a8fa7e0847b68a82f2e06417ac63f957cf
Cooper Towns <cooper.towns@smartthings.com>
2024-05-17 12:09:39 -0500

    Remove unneeded command and events files for AQS

--- drivers/SmartThings/matter-sensor/src/AirQuality/init.lua
+++ drivers/SmartThings/matter-sensor/src/AirQuality/init.lua
@@ -1,6 +1,5 @@
 local cluster_base = require "st.matter.cluster_base"
 local AirQualityServerAttributes = require "AirQuality.server.attributes"
-local AirQualityServerCommands = require "AirQuality.server.commands"
 local AirQualityTypes = require "AirQuality.types"
 
 local AirQuality = {}
@@ -10,7 +9,6 @@ AirQuality.NAME = "AirQuality"
 AirQuality.server = {}
 AirQuality.client = {}
 AirQuality.server.attributes = AirQualityServerAttributes:set_parent_cluster(AirQuality)
-AirQuality.server.commands = AirQualityServerCommands:set_parent_cluster(AirQuality)
 AirQuality.types = AirQualityTypes
 
 function AirQuality:get_attribute_by_id(attr_id)
@@ -26,16 +24,6 @@ function AirQuality:get_attribute_by_id(attr_id)
   end
   return nil
 end
-
-function AirQuality:get_server_command_by_id(command_id)
-  local server_id_map = {
-  }
-  if server_id_map[command_id] ~= nil then
-    return self.server.commands[server_id_map[command_id]]
-  end
-  return nil
-end
-
 
 -- Attribute Mapping
 AirQuality.attribute_direction_map = {
@@ -64,24 +52,6 @@ attribute_helper_mt.__index = function(self, key)
 end
 AirQuality.attributes = {}
 setmetatable(AirQuality.attributes, attribute_helper_mt)
-
-local command_helper_mt = {}
-command_helper_mt.__index = function(self, key)
-  local direction = AirQuality.command_direction_map[key]
-  if direction == nil then
-    error(string.format("Referenced unknown command %s on cluster %s", key, AirQuality.NAME))
-  end
-  return AirQuality[direction].commands[key]
-end
-AirQuality.commands = {}
-setmetatable(AirQuality.commands, command_helper_mt)
-
-local event_helper_mt = {}
-event_helper_mt.__index = function(self, key)
-  return AirQuality.server.events[key]
-end
-AirQuality.events = {}
-setmetatable(AirQuality.events, event_helper_mt)
 
 setmetatable(AirQuality, {__index = cluster_base})
 
