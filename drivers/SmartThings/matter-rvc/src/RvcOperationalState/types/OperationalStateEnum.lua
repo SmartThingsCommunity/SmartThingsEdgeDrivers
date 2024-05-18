34f2c06fc928a17fdbee5da4d14a3ce064edd1ca
Cooper Towns <cooper.towns@smartthings.com>
2024-05-17 22:59:10 -0500

    Add matter-rvc clusters

---
+++ drivers/SmartThings/matter-rvc/src/RvcOperationalState/types/OperationalStateEnum.lua
@@ -0,0 +1,36 @@
+local data_types = require "st.matter.data_types"
+local UintABC = require "st.matter.data_types.base_defs.UintABC"
+
+local OperationalStateEnum = {}
+local new_mt = UintABC.new_mt({NAME = "OperationalStateEnum", ID = data_types.name_to_id_map["Uint8"]}, 1)
+new_mt.__index.pretty_print = function(self)
+  local name_lookup = {
+    [self.SEEKING_CHARGER] = "SEEKING_CHARGER",
+    [self.CHARGING] = "CHARGING",
+    [self.DOCKED] = "DOCKED",
+  }
+  return string.format("%s: %s", self.field_name or self.NAME, name_lookup[self.value] or string.format("%d", self.value))
+end
+new_mt.__tostring = new_mt.__index.pretty_print
+
+new_mt.__index.SEEKING_CHARGER  = 0x40
+new_mt.__index.CHARGING  = 0x41
+new_mt.__index.DOCKED  = 0x42
+
+OperationalStateEnum.SEEKING_CHARGER  = 0x40
+OperationalStateEnum.CHARGING  = 0x41
+OperationalStateEnum.DOCKED  = 0x42
+
+OperationalStateEnum.augment_type = function(cls, val)
+  setmetatable(val, new_mt)
+end
+
+setmetatable(OperationalStateEnum, new_mt)
+
+local has_aliases, aliases = pcall(require, "st.matter.clusters.aliases.RvcOperationalState.types.OperationalStateEnum")
+if has_aliases then
+  aliases:add_to_class(OperationalStateEnum)
+end
+
+return OperationalStateEnum
+
---
+++ drivers/SmartThings/matter-rvc/src/RvcRunMode/server/commands/init.lua
@@ -0,0 +1,28 @@
+local command_mt = {}
+command_mt.__command_cache = {}
+command_mt.__index = function(self, key)
+  if command_mt.__command_cache[key] == nil then
+    local req_loc = string.format("RvcRunMode.server.commands.%s", key)
+    local raw_def = require(req_loc)
+    local cluster = rawget(self, "_cluster")
+    command_mt.__command_cache[key] = raw_def:set_parent_cluster(cluster)
+  end
+  return command_mt.__command_cache[key]
+end
+
+local RvcRunModeServerCommands = {}
+
+function RvcRunModeServerCommands:set_parent_cluster(cluster)
+  self._cluster = cluster
+  return self
+end
+
+setmetatable(RvcRunModeServerCommands, command_mt)
+
+local status, aliases = pcall(require, "st.matter.clusters.aliases.RvcRunMode.server.commands")
+if status then
+  aliases:add_to_class(RvcRunModeServerCommands)
+end
+
+return RvcRunModeServerCommands
+
