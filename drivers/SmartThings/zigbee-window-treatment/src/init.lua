1043e376620e4601522a8c81fab6c36ec19fffcb
Hari Prasad Reddy Kudamala <108659539+Kudamala2001@users.noreply.github.com>
2024-05-20 15:12:13 +0530

    Update init.lua

--- drivers/SmartThings/zigbee-window-treatment/src/init.lua
+++ drivers/SmartThings/zigbee-window-treatment/src/init.lua
@@ -36,7 +36,9 @@ local zigbee_window_treatment_driver_template = {
     require("rooms-beautiful"),
     require("axis"),
     require("yoolax"),
-    require("hanssem") },
+    require("hanssem"),
+    require("confioscc"),
+    require("confiodcc") },
   lifecycle_handlers = {
     added = added_handler
   }
