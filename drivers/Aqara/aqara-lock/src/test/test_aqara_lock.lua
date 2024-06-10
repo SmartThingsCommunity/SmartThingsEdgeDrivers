b2eb6007a9575c145a0cb4925689436f5cc03751
seojune <seojune@aqara.kr>
2024-06-10 15:40:30 +0900

    add lockAlarm capability file, change antiLockStatus properties and TC codes

--- drivers/Aqara/aqara-lock/src/test/test_aqara_lock.lua
+++ drivers/Aqara/aqara-lock/src/test/test_aqara_lock.lua
@@ -6,8 +6,12 @@ local data_types = require "st.zigbee.data_types"
 local zigbee_test_utils = require "integration_test.zigbee_test_utils"
 
 local remoteControlStatus = capabilities.remoteControlStatus
+local antiLockStatus = capabilities["stse.antiLockStatus"]
+test.add_package_capability("antiLockStatus.yaml")
 local lockCredentialInfo = capabilities["stse.lockCredentialInfo"]
 test.add_package_capability("lockCredentialInfo.yaml")
+local lockAlarm = capabilities["lockAlarm"]
+test.add_package_capability("lockAlarm.yaml")
 local Battery = capabilities.battery
 local Lock = capabilities.lock
 
@@ -47,6 +51,10 @@ test.register_coroutine_test(
     test.socket.capability:__expect_send(mock_device:generate_test_message("main",
       remoteControlStatus.remoteControlEnabled('false', { visibility = { displayed = false } })))
     test.socket.capability:__expect_send(mock_device:generate_test_message("main", Battery.battery(100)))
+    test.socket.capability:__expect_send(mock_device:generate_test_message("main",
+      lockAlarm.alarm.clear({ visibility = { displayed = false } })))
+    test.socket.capability:__expect_send(mock_device:generate_test_message("main",
+      antiLockStatus.antiLockStatus('unknown', { visibility = { displayed = false } })))
     test.socket.capability:__expect_send(mock_device:generate_test_message("main", Lock.lock("locked")))
   end
 )
@@ -60,6 +68,10 @@ test.register_coroutine_test(
     test.socket.capability:__expect_send(mock_device:generate_test_message("main",
       remoteControlStatus.remoteControlEnabled('true', { visibility = { displayed = false } })))
     test.socket.capability:__expect_send(mock_device:generate_test_message("main", Battery.battery(100)))
+    test.socket.capability:__expect_send(mock_device:generate_test_message("main",
+      lockAlarm.alarm.clear({ visibility = { displayed = false } })))
+    test.socket.capability:__expect_send(mock_device:generate_test_message("main",
+      antiLockStatus.antiLockStatus('unknown', { visibility = { displayed = false } })))
     test.socket.capability:__expect_send(mock_device:generate_test_message("main", Lock.lock("locked")))
   end
 )
