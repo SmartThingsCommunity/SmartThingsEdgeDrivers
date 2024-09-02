dec77b3e0674ad6367c1a08235333e2f3c2bfb97
“liuhongming” <hongming.liu-e0083@aqara.com>
2024-09-01 20:35:43 -0700

    Fixed known issues with Aqara Door and Window Sensor T1 and Aqara Water Leak Sensor

--- drivers/SmartThings/zigbee-contact/src/aqara/init.lua
+++ drivers/SmartThings/zigbee-contact/src/aqara/init.lua
@@ -1,30 +1,27 @@
+-- Copyright 2024 SmartThings
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+local capabilities = require "st.capabilities"
 local clusters = require "st.zigbee.zcl.clusters"
 local battery_defaults = require "st.zigbee.defaults.battery_defaults"
+local TemperatureMeasurement = (require "st.zigbee.zcl.clusters").TemperatureMeasurement
 
 local IASZone = clusters.IASZone
 local PowerConfiguration = clusters.PowerConfiguration
+local EnrollResponseCode = IASZone.types.EnrollResponseCode
 
 local FINGERPRINTS = {
   { mfr = "LUMI", model = "lumi.magnet.agl02" }
-}
-
-local CONFIGURATIONS = {
-  {
-    cluster = IASZone.ID,
-    attribute = IASZone.attributes.ZoneStatus.ID,
-    minimum_interval = 30,
-    maximum_interval = 3600,
-    data_type = IASZone.attributes.ZoneStatus.base_type,
-    reportable_change = 1
-  },
-  {
-    cluster = PowerConfiguration.ID,
-    attribute = PowerConfiguration.attributes.BatteryVoltage.ID,
-    minimum_interval = 30,
-    maximum_interval = 3600,
-    data_type = PowerConfiguration.attributes.BatteryVoltage.base_type,
-    reportable_change = 1
-  }
 }
 
 local is_aqara_products = function(opts, driver, device, ...)
@@ -38,17 +35,36 @@ end
 
 local function device_init(driver, device)
   battery_defaults.build_linear_voltage_init(2.6, 3.0)(driver, device)
+end
 
-  for _, attribute in ipairs(CONFIGURATIONS) do
-    device:add_configured_attribute(attribute)
-    device:add_monitored_attribute(attribute)
-  end
+local function device_added(driver, device)
+  device:emit_event(capabilities.contactSensor.contact.closed())
+  device:emit_event(capabilities.battery.battery(100))
+end
+
+local do_configure = function(self, device)
+  device:send(PowerConfiguration.attributes.BatteryVoltage:configure_reporting(device, 30, 3600, 1))
+  device:send(IASZone.server.commands.ZoneEnrollResponse(device, EnrollResponseCode.SUCCESS, 0x00))
+  device:refresh()
 end
 
 local aqara_contact_handler = {
   NAME = "Aqara Contact Handler",
+  zigbee_handlers = {
+    attr = {
+      [PowerConfiguration.ID] = {
+        [PowerConfiguration.attributes.BatteryPercentageRemaining.ID] = function() end
+      },
+      [TemperatureMeasurement.ID] = {
+        [TemperatureMeasurement.attributes.MinMeasuredValue.ID] = function() end,
+        [TemperatureMeasurement.attributes.MaxMeasuredValue.ID] = function() end
+      }
+    }
+  },
   lifecycle_handlers = {
-    init = device_init
+    init = device_init,
+    added = device_added,
+    doConfigure = do_configure
   },
   can_handle = is_aqara_products
 }
--- drivers/SmartThings/zigbee-water-leak-sensor/src/aqara/init.lua
+++ drivers/SmartThings/zigbee-water-leak-sensor/src/aqara/init.lua
@@ -1,28 +1,27 @@
+-- Copyright 2024 SmartThings
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
 local capabilities = require "st.capabilities"
 local clusters = require "st.zigbee.zcl.clusters"
-local cluster_base = require "st.zigbee.cluster_base"
-local data_types = require "st.zigbee.data_types"
 local battery_defaults = require "st.zigbee.defaults.battery_defaults"
-
+local TemperatureMeasurement = (require "st.zigbee.zcl.clusters").TemperatureMeasurement
 local PowerConfiguration = clusters.PowerConfiguration
+local IASZone = clusters. IASZone
+local EnrollResponseCode = IASZone.types.EnrollResponseCode
 
-local MFG_CODE = 0x115F
-local PRIVATE_CLUSTER_ID = 0xFCC0
-local PRIVATE_ATTRIBUTE_ID = 0x0009
 
 local FINGERPRINTS = {
   { mfr = "LUMI", model = "lumi.flood.agl02" }
-}
-
-local CONFIGURATIONS = {
-  {
-    cluster = PowerConfiguration.ID,
-    attribute = PowerConfiguration.attributes.BatteryVoltage.ID,
-    minimum_interval = 30,
-    maximum_interval = 3600,
-    data_type = PowerConfiguration.attributes.BatteryVoltage.base_type,
-    reportable_change = 1
-  }
 }
 
 local function is_aqara_products(opts, driver, device)
@@ -37,25 +36,35 @@ end
 local function device_added(driver, device)
   device:emit_event(capabilities.waterSensor.water.dry())
   device:emit_event(capabilities.battery.battery(100))
-
-  device:send(cluster_base.write_manufacturer_specific_attribute(device, PRIVATE_CLUSTER_ID, PRIVATE_ATTRIBUTE_ID,
-    MFG_CODE, data_types.Uint8, 1))
 end
 
 local function device_init(driver, device)
   battery_defaults.build_linear_voltage_init(2.6, 3.0)(driver, device)
+end
 
-  for _, attribute in ipairs(CONFIGURATIONS) do
-    device:add_configured_attribute(attribute)
-    device:add_monitored_attribute(attribute)
-  end
+local do_configure = function(self, device)
+  device:send(PowerConfiguration.attributes.BatteryVoltage:configure_reporting(device, 30, 3600, 1))
+  device:send(IASZone.server.commands.ZoneEnrollResponse(device, EnrollResponseCode.SUCCESS, 0x00))
+  device:refresh()
 end
 
 local aqara_contact_handler = {
   NAME = "Aqara water leak sensor",
+  zigbee_handlers = {
+    attr = {
+      [PowerConfiguration.ID] = {
+        [PowerConfiguration.attributes.BatteryPercentageRemaining.ID] = function() end
+      },
+      [TemperatureMeasurement.ID] = {
+        [TemperatureMeasurement.attributes.MinMeasuredValue.ID] = function() end,
+        [TemperatureMeasurement.attributes.MaxMeasuredValue.ID] = function() end
+      }
+    }
+  },
   lifecycle_handlers = {
     init = device_init,
-    added = device_added
+    added = device_added,
+    doConfigure = do_configure
   },
   can_handle = is_aqara_products
 }
