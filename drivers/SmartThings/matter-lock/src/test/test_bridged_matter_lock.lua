44087fd33c74c98dcfae8acd5e2dc5b7cc498aaa
Harrison Carter <harrison.carter@smartthings.com>
2024-10-01 15:19:21 -0500

    implement: check for capabilities of base-lock

--- drivers/SmartThings/matter-lock/src/test/test_bridged_matter_lock.lua
+++ drivers/SmartThings/matter-lock/src/test/test_bridged_matter_lock.lua
@@ -18,7 +18,7 @@ local t_utils = require "integration_test.utils"
 local clusters = require "st.matter.clusters"
 
 local mock_device_record = {
-  profile = t_utils.get_profile_definition("base-lock-nobattery.yml"),
+  profile = t_utils.get_profile_definition("base-lock.yml"),
   manufacturer_info = {vendor_id = 0, product_id = 0},
   endpoints = {
     {
@@ -33,7 +33,7 @@ local mock_device_record = {
     {
       endpoint_id = 10,
       clusters = {
-        {cluster_id = clusters.DoorLock.ID, cluster_type = "SERVER", feature_map = 0x0000},
+        {cluster_id = clusters.DoorLock.ID, cluster_type = "SERVER", feature_map = 0x0101},
       },
     },
   },
@@ -41,7 +41,7 @@ local mock_device_record = {
 local mock_device = test.mock_device.build_test_matter_device(mock_device_record)
 
 local mock_device_record_aqara = {
-    profile = t_utils.get_profile_definition("base-lock-nobattery.yml"),
+    profile = t_utils.get_profile_definition("lock-lockalarm-nobattery.yml"),
     manufacturer_info = {vendor_id = 0x115F, product_id = 0x2801}, -- Aqara Smart Lock U300
     endpoints = {
       {
@@ -74,8 +74,6 @@ local function test_init()
 
     local subscribe_request = clusters.DoorLock.attributes.LockState:subscribe(mock_device_aqara)
     subscribe_request:merge(clusters.DoorLock.events.DoorLockAlarm:subscribe(mock_device_aqara))
-    subscribe_request:merge(clusters.DoorLock.events.LockOperation:subscribe(mock_device_aqara))
-    subscribe_request:merge(clusters.DoorLock.events.LockUserChange:subscribe(mock_device_aqara))
     test.socket["matter"]:__expect_send({mock_device_aqara.id, subscribe_request})
     test.mock_device.add_test_device(mock_device_aqara)
 end
