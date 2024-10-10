44c6519542f034c600b8f6f42a24d1470682cf47
Gustavo Cavalcanti <g.cavalcanti@stelpro.com>
2024-10-10 09:32:47 -0400

    Deleting SAT402ZB subdriver and adding SAT402ZB to stelpro_maestrostat subdriver file

--- drivers/SmartThings/zigbee-thermostat/src/stelpro/stelpro_maestrostat.lua
+++ drivers/SmartThings/zigbee-thermostat/src/stelpro/stelpro_maestrostat.lua
@@ -21,6 +21,7 @@ local Thermostat = clusters.Thermostat
 local ThermostatUserInterfaceConfiguration = clusters.ThermostatUserInterfaceConfiguration
 
 local STELPRO_THERMOSTAT_FINGERPRINTS = {
+  { mfr = "Stelpro", model = "MaestroStat" },
   { mfr = "Stello", model = "HT402" },
 }
 
