49ce71bb6172c115531601cc4b58ada0716ef8f1
duanzd <1481633482@qq.com>
2024-11-22 17:13:56 +0800

    duanzd-20241122 test Signed-off-by: duanzd <1481633482@qq.com>

---
+++ drivers/SmartThings/duanzd/src/test/test_basic_swtich.lua
@@ -0,0 +1,88 @@
+-- Copyright 2022 SmartThings
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+local test = require "integration_test"
+local t_utils = require "integration_test.utils"
+local clusters = require "st.zigbee.zcl.clusters"
+local zigbee_test_utils = require "integration_test.zigbee_test_utils"
+
+local OnOff = clusters.OnOff
+
+local mock_device = test.mock_device.build_test_zigbee_device(
+  { profile = t_utils.get_profile_definition("duan-switch-test1.yml"),
+    fingerprinted_endpoint_id = 0x01,
+    zigbee_endpoints = {
+      [1] = {
+        id = 1,
+        manufacturer = "REXENSE",
+        model = "HY0002",
+        server_clusters = {0x0006, 0x0B04, 0x0702}
+      }
+    }
+  }
+)
+
+zigbee_test_utils.prepare_zigbee_env_info()
+
+local function test_init()
+  test.mock_device.add_test_device(mock_device)
+end
+
+test.set_test_init_function(test_init)
+
+test.register_coroutine_test(
+  "Configure should configure all necessary attributes and refresh device",
+  function()
+    test.socket.device_lifecycle:__queue_receive({ mock_device.id, "doConfigure" })
+    test.socket.zigbee:__set_channel_ordering("relaxed")
+
+    test.socket.zigbee:__expect_send({
+      mock_device.id,
+      zigbee_test_utils.build_bind_request(mock_device, zigbee_test_utils.mock_hub_eui, OnOff.ID)
+    })
+
+    test.socket.zigbee:__expect_send(
+      {
+        mock_device.id,
+        OnOff.attributes.OnOff:configure_reporting(mock_device, 0, 300, 1)
+      }
+    )
+    test.socket.zigbee:__expect_send({ mock_device.id, OnOff.attributes.OnOff:read(mock_device) })
+    mock_device:expect_metadata_update({ provisioning_state = "PROVISIONED" })
+  end
+)
+
+test.register_message_test(
+  "Refresh should read all necessary attributes",
+  {
+    {
+      channel = "capability",
+      direction = "receive",
+      message = {mock_device.id, {capability = "refresh", component = "main", command = "refresh", args = {}}}
+    },
+    {
+      channel = "zigbee",
+      direction = "send",
+      message = {
+        mock_device.id,
+        OnOff.attributes.OnOff:read(mock_device)
+      }
+    },
+  },
+  {
+    inner_block_ordering = "relaxed"
+  }
+)
+
+test.run_registered_tests()
