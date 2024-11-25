e2547bee72beea90bec7b9c3f5c753379b331296
cjswedes <carter.swedal@smartthings.com>
2024-11-25 10:30:23 -0600

    Remove healthcheck functionality from zigbee-dimmer-remote
    This functionality is not needed to keep the device online.
    When testing with the sengled dimmer remote it was discovered that the
    device needs a faster battery reporting interval than what is default to
    stay online (even with healthcheck enabled)

--- drivers/SmartThings/zigbee-dimmer-remote/src/init.lua
+++ drivers/SmartThings/zigbee-dimmer-remote/src/init.lua
@@ -15,6 +15,20 @@
 local capabilities = require "st.capabilities"
 local ZigbeeDriver = require "st.zigbee"
 local defaults = require "st.zigbee.defaults"
+local zcl_clusters = require "st.zigbee.zcl.clusters"
+
+local battery_attribute_configuration = {
+  cluster = zcl_clusters.PowerConfiguration.ID,
+  attribute = zcl_clusters.PowerConfiguration.attributes.BatteryPercentageRemaining.ID,
+  minimum_interval = 30,
+  maximum_interval = 14300, -- ~4hrs
+  data_type = zcl_clusters.PowerConfiguration.attributes.BatteryPercentageRemaining.base_type,
+  reportable_change = 1
+}
+
+local function device_init(driver, device)
+    device:add_configured_attribute(battery_attribute_configuration)
+end
 
 local zigbee_dimmer_remote_driver_template = {
     supported_capabilities = {
@@ -23,7 +37,11 @@ local zigbee_dimmer_remote_driver_template = {
         capabilities.switch,
         capabilities.switchLevel
     },
-    sub_drivers = { require("zigbee-accessory-dimmer"), require("zigbee-battery-accessory-dimmer")}
+    lifecycle_handlers = {
+      init = device_init,
+    },
+    sub_drivers = { require("zigbee-accessory-dimmer"), require("zigbee-battery-accessory-dimmer")},
+    health_check = false,
 }
 
 defaults.register_for_default_handlers(zigbee_dimmer_remote_driver_template, zigbee_dimmer_remote_driver_template.supported_capabilities)
