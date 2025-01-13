1cfa5a525ad7336fa87cdd59ac949bbec2421c26
simon3panda <340443303@qq.com>
2025-01-13 14:59:56 +0800

    Update init.lua
    add configuration part for Linxura smart controller driver

--- drivers/SmartThings/zigbee-button/src/zigbee-multi-button/linxura/init.lua
+++ drivers/SmartThings/zigbee-button/src/zigbee-multi-button/linxura/init.lua
@@ -21,7 +21,6 @@ local log = require "log"
 local device_management = require "st.zigbee.device_management"
 local zcl_commands = require "st.zigbee.zcl.global_commands"
 local zcl_clusters = require "st.zigbee.zcl.clusters"
-local supported_values = require "zigbee-multi-button.supported_values"
 
 local BUTTON1_HELD = 0x0001
 local BUTTON1_PUSHED = 0x0003
@@ -46,6 +45,16 @@ local LINXURA_BUTTON_FINGERPRINTS = {
     { mfr = "Linxura", model = "Smart Controller" }
 }
 
+local configuration = {
+    {
+        cluster = IASZone.ID,
+        attribute = IASZone.attributes.ZoneStatus.ID,
+        minimum_interval = 0,
+        maximum_interval = 3600,
+        data_type = IASZone.attributes.ZoneStatus.base_type,
+        reportable_change = 1
+    }
+}
 local is_linxura_button = function(opts, driver, device)
     for _, fingerprint in ipairs(LINXURA_BUTTON_FINGERPRINTS) do
         if device:get_manufacturer() == fingerprint.mfr and device:get_model() == fingerprint.model then
@@ -55,13 +64,11 @@ local is_linxura_button = function(opts, driver, device)
     return false
 end
 
-local function present_value_attr_handler(driver, device, zb_rx)   
+local function present_value_attr_handler(driver, device, zone_status, zb_rx)   
+    log.info("present_value_attr_handler The current value is: ", zone_status.value)
+    local status = zone_status
     
-end
 
-local function ias_zone_status_change_handler1(driver, device, zb_rx)
-    local status = zb_rx.body.zcl_body.zone_status
-    log.info("ias_zone_status_change_handler1 The current value is: ")
     local additional_fields = {
         state_change = true
     }
@@ -93,69 +100,22 @@ local function ias_zone_status_change_handler1(driver, device, zb_rx)
     end
 end
 
-local function ias_zone_status_change_handler(driver, device, zb_rx)
-    local status = zb_rx.body.zcl_body.zone_status
-    log.info("ias_zone_status_change_handler The current value is: ")
-    local additional_fields = {
-        state_change = true
-    }
-    local event
-    local mod = status.value % 6
-    if mod == 1 then
-        event = capabilities.button.button.pushed(additional_fields)
-    elseif mod == 3 then
-        event = capabilities.button.button.double(additional_fields)
-    elseif mod == 5 then
-        event = capabilities.button.button.held(additional_fields)
-    else
-        return false
-    end
 
-    if status.value == BUTTON1_HELD or status.value == BUTTON1_PUSHED or status.value == BUTTON1_DOUBLE then
-        device:emit_component_event(device.profile.components[BUTTON1], event)
-        --device:emit_event(device.profile.components[BUTTON1],event)
-        device:emit_event(event)
-    elseif status.value == BUTTON2_HELD or status.value == BUTTON2_PUSHED or status.value == BUTTON2_DOUBLE then
-        device:emit_component_event(device.profile.components[BUTTON2], event)
-        device:emit_event(event)
-    elseif status.value == BUTTON3_HELD or status.value == BUTTON3_PUSHED or status.value == BUTTON3_DOUBLE then
-        device:emit_component_event(device.profile.components[BUTTON3], event)
-        device:emit_event(event)
-    elseif status.value == BUTTON4_HELD or status.value == BUTTON4_PUSHED or status.value == BUTTON4_DOUBLE then
-        device:emit_component_event(device.profile.components[BUTTON4], event)
-        device:emit_event(event)
-    end
-end
+local function device_init(driver, device)
+    for _, attribute in ipairs(configuration) do
+      device:add_configured_attribute(attribute)
 
-local function added_handler(self, device)
-    log.info("bbbb")
-  local config = supported_values.get_device_parameters(device)
-  for _, component in pairs(device.profile.components) do
-    if config ~= nil then
-        --if config.NUMBER_OF_BUTTONS == 4 then
-        if config.NUMBER_OF_BUTTONS == 4 then
-            device:emit_component_event(component,
-                capabilities.button.supportedButtonValues(config.SUPPORTED_BUTTON_VALUES, { visibility = { displayed = false } }))
-            device:emit_component_event(component,
-                capabilities.button.numberOfButtons({ value = config.NUMBER_OF_BUTTONS }, { visibility = { displayed = false } }))
-        end
+
+      device:add_monitored_attribute(attribute)
     end
   end
-  --device:emit_event(capabilities.button.button.pushed({state_change = false}))
-end
   
-local do_configure = function(self, device)
-    log.info("configure")
-    device:configure()
-    self:add_hub_to_zigbee_group(0x0001)
-end
 
 
 local linxura_device_handler = {
     NAME = "Linxura Device Handler",
     lifecycle_handlers = {
-    doConfigure = do_configure,
-    added = added_handler
+        init = device_init,
     },
      
     zigbee_handlers = {
@@ -164,20 +124,9 @@ local linxura_device_handler = {
         [IASZone.attributes.ZoneStatus.ID] = present_value_attr_handler
         }
     },
-    cluster = {
-    [IASZone.ID] = {
-        [IASZone.client.commands.ZoneStatusChangeNotification.ID] = ias_zone_status_change_handler
-    }
-    },
-    global = {
-        [IASZone.ID] = {
-          [zcl_commands.ReportAttribute.ID] = ias_zone_status_change_handler1
-        }
-      }
     },
     
     can_handle = is_linxura_button,
-    ias_zone_configuration_method = constants.IAS_ZONE_CONFIGURE_TYPE.AUTO_ENROLL_RESPONSE
 }
 
 return linxura_device_handler
