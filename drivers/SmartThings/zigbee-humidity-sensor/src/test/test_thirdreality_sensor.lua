719a0e146cadb70790c7efe76f139a5821d99e7b
weihuan1111 <99949392+weihuan1111@users.noreply.github.com>
2025-02-14 11:42:54 +0800

    Update test_thirdreality_sensor.lua

--- drivers/SmartThings/zigbee-humidity-sensor/src/test/test_thirdreality_sensor.lua
+++ drivers/SmartThings/zigbee-humidity-sensor/src/test/test_thirdreality_sensor.lua
@@ -2,6 +2,7 @@ local test = require "integration_test"
 local t_utils = require "integration_test.utils"
 local zigbee_test_utils = require "integration_test.zigbee_test_utils"
 local clusters = require "st.zigbee.zcl.clusters"
+local capabilities = require "st.capabilities"
 
 local PowerConfiguration = clusters.PowerConfiguration
 local TemperatureMeasurement = clusters.TemperatureMeasurement
