8d370c0baab39e9ef0ff83594a50bf20cc04e6fc
weihuan1111 <99949392+weihuan1111@users.noreply.github.com>
2025-03-04 14:45:34 +0800

    Update test_thirdreality_watering_kit.lua

--- drivers/SmartThings/zigbee-watering-kit/src/test/test_thirdreality_watering_kit.lua
+++ drivers/SmartThings/zigbee-watering-kit/src/test/test_thirdreality_watering_kit.lua
@@ -3,6 +3,7 @@ local t_utils = require "integration_test.utils"
 local zigbee_test_utils = require "integration_test.zigbee_test_utils"
 local clusters = require "st.zigbee.zcl.clusters"
 local capabilities = require "st.capabilities"
+local data_types = require "st.zigbee.data_types"
 
 local IASZone = clusters.IASZone
 local OnOff = clusters.OnOff
@@ -90,6 +91,34 @@ test.register_message_test(
       message = mock_device:generate_test_message("main", capabilities.hardwareFault.hardwareFault.clear())
     }
   }
+)
+
+test.register_coroutine_test(
+  "Reported fanspeed should be handled: 10",
+  function()
+    local attr_report_data = {
+      { 0x0000, data_types.Uint16.ID, 2}
+    }
+    test.socket.zigbee:__queue_receive({
+      mock_device.id,
+      zigbee_test_utils.build_attribute_report(mock_device, 0xFFF2, attr_report_data, 0x1407)
+    })
+    test.socket.capability:__expect_send(mock_device:generate_test_message("main", capabilities.fanSpeed.fanSpeed(10)))
+  end
+)
+
+test.register_coroutine_test(
+  "Reported fanspeed should be handled: 30",
+  function()
+    local attr_report_data = {
+      { 0x0000, data_types.Uint16.ID, 30}
+    }
+    test.socket.zigbee:__queue_receive({
+      mock_device.id,
+      zigbee_test_utils.build_attribute_report(mock_device, 0xFFF2, attr_report_data, 0x1407)
+    })
+    test.socket.capability:__expect_send(mock_device:generate_test_message("main", capabilities.fanSpeed.fanSpeed(30)))
+  end
 )
 
 test.run_registered_tests()
