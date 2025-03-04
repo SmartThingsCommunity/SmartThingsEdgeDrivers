e69ca67cf398e0b17f6e30153919b73c7b6d9848
weihuan1111 <99949392+weihuan1111@users.noreply.github.com>
2025-03-04 11:31:37 +0800

    Update init.lua

--- drivers/SmartThings/zigbee-watering-kit/src/thirdreality/init.lua
+++ drivers/SmartThings/zigbee-watering-kit/src/thirdreality/init.lua
@@ -15,8 +15,29 @@ local W_TIME = "watering-time"
 local W_INTERVAL = "watering-interval"
 
 local function device_added(driver, device)
-    device:emit_event(capabilities.fanSpeed.fanSpeed(10))
-    device:emit_event(capabilities.mode.mode(0))
+    device:emit_event(capabilities.hardwareFault.hardwareFault.clear())
+    device:emit_component_event(device.profile.components[W_TIME], capabilities.fanSpeed.fanSpeed(10))
+    device:emit_component_event(device.profile.components[W_INTERVAL], capabilities.fanSpeed.fanSpeed(0))
+end
+
+local generate_event_from_zone_status = function(driver, device, zone_status, zb_rx)
+    local event
+    if zone_status:is_alarm1_set() then
+      event = capabilities.hardwareFault.hardwareFault.detected()
+    else
+      event = capabilities.hardwareFault.hardwareFault.clear()
+    end
+    if event ~= nil then
+      device:emit_event(event)
+    end
+end
+
+local ias_zone_status_attr_handler = function(driver, device, zone_status, zb_rx)
+    generate_event_from_zone_status(driver, device, zone_status, zb_rx)
+end
+
+local ias_zone_status_change_handler = function(driver, device, zb_rx)
+    generate_event_from_zone_status(driver, device, zb_rx.body.zcl_body.zone_status, zb_rx)
 end
 
 local function custom_write_attribute(device, cluster, attribute, data_type, value, mfg_code)
@@ -37,39 +58,56 @@ local function set_watering_time(device, speed)
           data_types.Uint16, watering_time, nil))
 end
 
-local function set_watering_interval(device, speed)
-    local watering_interval = speed
+local function set_watering_interval(device, interval)
     device:send(custom_write_attribute(device, THIRDREALITY_WATERING_CLUSTER, WATERING_INTERVAL,
-          data_types.Uint8, watering_interval, nil))
+          data_types.Uint8, interval, nil))
 end
 
 local function fan_speed_handler(driver, device, command)
-    set_watering_time(device, command.args.speed)
-end
-
-local function mode_handler(driver, device, command)
-    set_watering_interval(device, command.args.speed)
+    if command.component == W_TIME then
+        set_watering_time(device, command.args.speed)
+    elseif command.component == W_INTERVAL then
+        set_watering_interval(device, command.args.speed)
+    end
 end
 
 local function watering_time_handler(driver, device, value, zb_rx)
     local fan_speed_value = value.value
-    --device:emit_component_event(device.profile.components[W_TIME], capabilities.fanSpeed.fanSpeed(fan_speed_value))
-    device:emit_event(capabilities.fanSpeed.fanSpeed(fan_speed_value))
+    device:emit_component_event(device.profile.components[W_TIME], capabilities.fanSpeed.fanSpeed(fan_speed_value))
 end
 
 local function watering_interval_handler(driver, device, value, zb_rx)
-    local interval_value = value.value
-    device:emit_event(capabilities.mode.mode(interval_value))
+    local fan_speed_value = value.value
+    device:emit_component_event(device.profile.components[W_INTERVAL], capabilities.fanSpeed.fanSpeed(fan_speed_value))
 end
 
+local function do_refresh(driver, device)
+    device:refresh()
+    device:send(cluster_base.read_manufacturer_specific_attribute(device, THIRDREALITY_WATERING_CLUSTER, WATERING_TIME, 0x1407))
+    device:send(cluster_base.read_manufacturer_specific_attribute(device, THIRDREALITY_WATERING_CLUSTER, WATERING_INTERVAL, 0x1407))
+end
+
+local function do_configure(driver, device)
+    device:configure()
+    device:send(device_management.build_bind_request(device, THIRDREALITY_WATERING_CLUSTER, driver.environment_info.hub_zigbee_eui), 1)
+    do_refresh(driver, device)
+end
 
 local thirdreality_device_handler = {
     NAME = "ThirdReality Smart Watering Kit",
     zigbee_handlers = {
         attr = {
+            [IASZone.ID] = {
+                [ZONE_STATUS_ATTR.ID] = ias_zone_status_attr_handler
+            },
             [THIRDREALITY_WATERING_CLUSTER] = {
                 [WATERING_TIME] = watering_time_handler,
                 [WATERING_INTERVAL] = watering_interval_handler
+            }
+        },
+        cluster = {
+            [IASZone.ID] = {
+              [IASZone.client.commands.ZoneStatusChangeNotification.ID] = ias_zone_status_change_handler
             }
         }
     },
@@ -77,12 +115,13 @@ local thirdreality_device_handler = {
         [capabilities.fanSpeed.ID] = {
             [capabilities.fanSpeed.commands.setFanSpeed.NAME] = fan_speed_handler
         },
-        [capabilities.mode.ID] = {
-            [capabilities.mode.commands.setMode.NAME] = mode_handler
+        [capabilities.refresh.ID] = {
+            [capabilities.refresh.commands.refresh.NAME] = do_refresh,
         }
     },
     lifecycle_handlers = {
-        added = device_added
+        added = device_added,
+        doConfigure = do_configure
     },
     can_handle = function(opts, driver, device, ...)
       return device:get_manufacturer() == "Third Reality, Inc" and device:get_model() == "3RWK0148Z"
