c8b93bad56311358be325209e54c9cc8ec065cea
mh-zwave <mh@z-wave.eu>
2025-03-31 09:13:59 +0200

    remove unused imports

--- drivers/SmartThings/zwave-electric-meter/src/test/test_aeotec_home_energy_meter_gen8_1_phase.lua
+++ drivers/SmartThings/zwave-electric-meter/src/test/test_aeotec_home_energy_meter_gen8_1_phase.lua
@@ -12,7 +12,6 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-local st_utils = require "st.utils"
 local test = require "integration_test"
 local capabilities = require "st.capabilities"
 local zw = require "st.zwave"
