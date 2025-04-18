de187aa25bf14985ca60ff2a8b2d1ac7758a87ae
Alec Lorimer <alec.lorimer@smartthings.com>
2025-04-18 13:24:24 -0500

    CHAD-14927: Fixing tests to line up with `test.disable_startup_messages()` change

--- drivers/SmartThings/zwave-switch/src/test/test_qubino_din_dimmer.lua
+++ drivers/SmartThings/zwave-switch/src/test/test_qubino_din_dimmer.lua
@@ -323,6 +323,14 @@ test.register_message_test(
       channel = "capability",
       direction = "send",
       message = mock_device:generate_test_message("main", capabilities.powerMeter.power({ value = 55, unit = "W" }))
+    },
+    {
+      channel = "devices",
+      direction = "send",
+      message = {
+        "register_native_capability_attr_handler",
+        { device_uuid = mock_device.id, capability_id = "powerMeter", capability_attr_id = "power" }
+      }
     }
   }
 )
