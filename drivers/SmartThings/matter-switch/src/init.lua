019cc319e794334d63c7e12bc932b6f558d1029a
Nick DeBoom <nick.deboom@smartthings.com>
2025-05-13 13:26:36 -0500

    Move remaining button functionality to buttons.lua
    This PR already moved configure_buttons to buttons.lua, so it might be a
    good place to move the remaining functionality for buttons to this file.

--- drivers/SmartThings/matter-switch/src/init.lua
+++ drivers/SmartThings/matter-switch/src/init.lua
@@ -13,11 +13,11 @@
 -- limitations under the License.
 
 local capabilities = require "st.capabilities"
+local color_utils = require "color_utils"
 local log = require "log"
 local clusters = require "st.matter.clusters"
 local im = require "st.matter.interaction_model"
 local MatterDriver = require "st.matter.driver"
-local lua_socket = require "socket"
 local utils = require "st.utils"
 local buttons = require "buttons"
 local device_lib = require "st.device"
@@ -61,6 +61,15 @@ local LEVEL_BOUND_RECEIVED = "__level_bound_received"
 local LEVEL_MIN = "__level_min"
 local LEVEL_MAX = "__level_max"
 local COLOR_MODE = "__color_mode"
+local TEMP_BOUND_RECEIVED = "__temp_bound_received"
+local TEMP_MIN = "__temp_min"
+local TEMP_MAX = "__temp_max"
+
+local AQARA_MANUFACTURER_ID = 0x115F
+local AQARA_CLIMATE_SENSOR_W100_ID = 0x2004
+
+-- this is the number of buttons for which we have a static profile already made
+local STATIC_BUTTON_PROFILE_SUPPORTED = {1, 2, 3, 4, 5, 6, 7, 8}
 
 local updated_fields = {
   { current_field_name = "__component_to_endpoint_map_button", updated_field_name = COMPONENT_TO_ENDPOINT_MAP },
@@ -286,42 +295,6 @@ local function set_poll_report_timer_and_schedule(device, is_cumulative_report)
   end
 end
 
-local START_BUTTON_PRESS = "__start_button_press"
-local TIMEOUT_THRESHOLD = 10 --arbitrary timeout
-local HELD_THRESHOLD = 1
--- this is the number of buttons for which we have a static profile already made
-local STATIC_BUTTON_PROFILE_SUPPORTED = {1, 2, 3, 4, 5, 6, 7, 8}
-
--- Some switches will send a MultiPressComplete event as part of a long press sequence. Normally the driver will create a
--- button capability event on receipt of MultiPressComplete, but in this case that would result in an extra event because
--- the "held" capability event is generated when the LongPress event is received. The IGNORE_NEXT_MPC flag is used
--- to tell the driver to ignore MultiPressComplete if it is received after a long press to avoid this extra event.
-local IGNORE_NEXT_MPC = "__ignore_next_mpc"
-
--- These are essentially storing the supported features of a given endpoint
--- TODO: add an is_feature_supported_for_endpoint function to matter.device that takes an endpoint
-local EMULATE_HELD = "__emulate_held" -- for non-MSR (MomentarySwitchRelease) devices we can emulate this on the software side
-local SUPPORTS_MULTI_PRESS = "__multi_button" -- for MSM devices (MomentarySwitchMultiPress), create an event on receipt of MultiPressComplete
-local INITIAL_PRESS_ONLY = "__initial_press_only" -- for devices that support MS (MomentarySwitch), but not MSR (MomentarySwitchRelease)
-
-local TEMP_BOUND_RECEIVED = "__temp_bound_received"
-local TEMP_MIN = "__temp_min"
-local TEMP_MAX = "__temp_max"
-
-local AQARA_MANUFACTURER_ID = 0x115F
-local AQARA_CLIMATE_SENSOR_W100_ID = 0x2004
-
---helper function to create list of multi press values
-local function create_multi_press_values_list(size, supportsHeld)
-  local list = {"pushed", "double"}
-  if supportsHeld then table.insert(list, "held") end
-  -- add multi press values of 3 or greater to the list
-  for i=3, size do
-    table.insert(list, string.format("pushed_%dx", i))
-  end
-  return list
-end
-
 local function tbl_contains(array, value)
   for _, element in ipairs(array) do
     if element == value then
@@ -337,23 +310,6 @@ end
 
 local function set_field_for_endpoint(device, field, endpoint, value, additional_params)
   device:set_field(string.format("%s_%d", field, endpoint), value, additional_params)
-end
-
-local function init_press(device, endpoint)
-  set_field_for_endpoint(device, START_BUTTON_PRESS, endpoint, lua_socket.gettime(), {persist = false})
-end
-
-local function emulate_held_event(device, ep)
-  local now = lua_socket.gettime()
-  local press_init = get_field_for_endpoint(device, START_BUTTON_PRESS, ep) or now -- if we don't have an init time, assume instant release
-  if (now - press_init) < TIMEOUT_THRESHOLD then
-    if (now - press_init) > HELD_THRESHOLD then
-      device:emit_event_for_endpoint(ep, capabilities.button.button.held({state_change = true}))
-    else
-      device:emit_event_for_endpoint(ep, capabilities.button.button.pushed({state_change = true}))
-    end
-  end
-  set_field_for_endpoint(device, START_BUTTON_PRESS, ep, nil, {persist = false})
 end
 
 local function convert_huesat_st_to_matter(val)
@@ -524,36 +480,6 @@ local function find_child(parent, ep_id)
   return parent:get_child_by_parent_assigned_key(string.format("%d", ep_id))
 end
 
-local function build_button_component_map(device, main_endpoint, button_eps)
-  -- create component mapping on the main profile button endpoints
-  table.sort(button_eps)
-  local component_map = {}
-  component_map["main"] = main_endpoint
-  for component_num, ep in ipairs(button_eps) do
-    if ep ~= main_endpoint then
-      local button_component = "button"
-      if #button_eps > 1 then
-        button_component = button_component .. component_num
-      end
-      component_map[button_component] = ep
-    end
-  end
-  device:set_field(COMPONENT_TO_ENDPOINT_MAP, component_map, {persist = true})
-end
-
-local function build_button_profile(device, main_endpoint, num_button_eps)
-  local profile_name = string.gsub(num_button_eps .. "-button", "1%-", "") -- remove the "1-" in a device with 1 button ep
-  if device_type_supports_button_switch_combination(device, main_endpoint) then
-    profile_name = "light-level-" .. profile_name
-  end
-  local battery_supported = #device:get_endpoints(clusters.PowerSource.ID, {feature_bitmap = clusters.PowerSource.types.PowerSourceFeature.BATTERY}) > 0
-  if battery_supported then -- battery profiles are configured later, in power_source_attribute_list_handler
-    device:send(clusters.PowerSource.attributes.AttributeList:read(device))
-  else
-    device:try_update_metadata({profile = profile_name})
-  end
-end
-
 local function build_child_switch_profiles(driver, device, main_endpoint)
   local num_switch_server_eps = 0
   local parent_child_device = false
@@ -618,10 +544,10 @@ local function initialize_buttons_and_switches(driver, device, main_endpoint)
   local profile_found = false
   local button_eps = device:get_endpoints(clusters.Switch.ID, {feature_bitmap=clusters.Switch.types.SwitchFeature.MOMENTARY_SWITCH})
   if tbl_contains(STATIC_BUTTON_PROFILE_SUPPORTED, #button_eps) then
-    build_button_profile(device, main_endpoint, #button_eps)
+    buttons.build_profile(device, #button_eps, device_type_supports_button_switch_combination(device, main_endpoint))
     -- All button endpoints found will be added as additional components in the profile containing the main_endpoint.
     -- The resulting endpoint to component map is saved in the COMPONENT_TO_ENDPOINT_MAP field
-    build_button_component_map(device, main_endpoint, button_eps)
+    buttons.build_component_map(device, main_endpoint, button_eps)
     buttons.configure(device)
     profile_found = true
   end
@@ -1019,8 +945,6 @@ local level_bounds_handler_factory = function(minOrMax)
   end
 end
 
-local color_utils = require "color_utils"
-
 local function x_attr_handler(driver, device, ib, response)
   if device:get_field(COLOR_MODE) == HUE_SAT_COLOR_MODE then
     return
@@ -1127,39 +1051,6 @@ local function energy_report_handler_factory(is_cumulative_report)
   end
 end
 
-local function initial_press_event_handler(driver, device, ib, response)
-  if get_field_for_endpoint(device, SUPPORTS_MULTI_PRESS, ib.endpoint_id) then
-    -- Receipt of an InitialPress event means we do not want to ignore the next MultiPressComplete event
-    -- or else we would potentially not create the expected button capability event
-    set_field_for_endpoint(device, IGNORE_NEXT_MPC, ib.endpoint_id, nil)
-  elseif get_field_for_endpoint(device, INITIAL_PRESS_ONLY, ib.endpoint_id) then
-    device:emit_event_for_endpoint(ib.endpoint_id, capabilities.button.button.pushed({state_change = true}))
-  elseif get_field_for_endpoint(device, EMULATE_HELD, ib.endpoint_id) then
-    -- if our button doesn't differentiate between short and long holds, do it in code by keeping track of the press down time
-    init_press(device, ib.endpoint_id)
-  end
-end
-
--- if the device distinguishes a long press event, it will always be a "held"
--- there's also a "long release" event, but this event is required to come first
-local function long_press_event_handler(driver, device, ib, response)
-  device:emit_event_for_endpoint(ib.endpoint_id, capabilities.button.button.held({state_change = true}))
-  if get_field_for_endpoint(device, SUPPORTS_MULTI_PRESS, ib.endpoint_id) then
-    -- Ignore the next MultiPressComplete event if it is sent as part of this "long press" event sequence
-    set_field_for_endpoint(device, IGNORE_NEXT_MPC, ib.endpoint_id, true)
-  end
-end
-
-local function short_release_event_handler(driver, device, ib, response)
-  if not get_field_for_endpoint(device, SUPPORTS_MULTI_PRESS, ib.endpoint_id) then
-    if get_field_for_endpoint(device, EMULATE_HELD, ib.endpoint_id) then
-      emulate_held_event(device, ib.endpoint_id)
-    else
-      device:emit_event_for_endpoint(ib.endpoint_id, capabilities.button.button.pushed({state_change = true}))
-    end
-  end
-end
-
 local function active_power_handler(driver, device, ib, response)
   if ib.data.value then
     local watt_value = ib.data.value / CONVERSION_CONST_MILLIWATT_TO_WATT
@@ -1184,86 +1075,6 @@ local function valve_level_attr_handler(driver, device, ib, response)
   if ib.data.value then
     device:emit_event_for_endpoint(ib.endpoint_id, capabilities.level.level(ib.data.value))
   end
-end
-
-local function multi_press_complete_event_handler(driver, device, ib, response)
-  -- in the case of multiple button presses
-  -- emit number of times, multiple presses have been completed
-  if ib.data and not get_field_for_endpoint(device, IGNORE_NEXT_MPC, ib.endpoint_id) then
-    local press_value = ib.data.elements.total_number_of_presses_counted.value
-    --capability only supports up to 6 presses
-    if press_value < 7 then
-      local button_event = capabilities.button.button.pushed({state_change = true})
-      if press_value == 2 then
-        button_event = capabilities.button.button.double({state_change = true})
-      elseif press_value > 2 then
-        button_event = capabilities.button.button(string.format("pushed_%dx", press_value), {state_change = true})
-      end
-
-      device:emit_event_for_endpoint(ib.endpoint_id, button_event)
-    else
-      log.info(string.format("Number of presses (%d) not supported by capability", press_value))
-    end
-  end
-  set_field_for_endpoint(device, IGNORE_NEXT_MPC, ib.endpoint_id, nil)
-end
-
-local function battery_percent_remaining_attr_handler(driver, device, ib, response)
-  if ib.data.value then
-    device:emit_event(capabilities.battery.battery(math.floor(ib.data.value / 2.0 + 0.5)))
-  end
-end
-
-local function battery_charge_level_attr_handler(driver, device, ib, response)
-  if ib.data.value == clusters.PowerSource.types.BatChargeLevelEnum.OK then
-    device:emit_event(capabilities.batteryLevel.battery.normal())
-  elseif ib.data.value == clusters.PowerSource.types.BatChargeLevelEnum.WARNING then
-    device:emit_event(capabilities.batteryLevel.battery.warning())
-  elseif ib.data.value == clusters.PowerSource.types.BatChargeLevelEnum.CRITICAL then
-    device:emit_event(capabilities.batteryLevel.battery.critical())
-  end
-end
-
-local function power_source_attribute_list_handler(driver, device, ib, response)
-  local profile_name = ""
-
-  local button_eps = device:get_endpoints(clusters.Switch.ID, {feature_bitmap=clusters.Switch.types.SwitchFeature.MOMENTARY_SWITCH})
-  for _, attr in ipairs(ib.data.elements) do
-    -- Re-profile the device if BatPercentRemaining (Attribute ID 0x0C) or
-    -- BatChargeLevel (Attribute ID 0x0E) is present.
-    if attr.value == 0x0C then
-      profile_name = "button-battery"
-      break
-    elseif attr.value == 0x0E then
-      profile_name = "button-batteryLevel"
-      break
-    end
-  end
-  if profile_name ~= "" then
-    if #button_eps > 1 then
-      profile_name = string.format("%d-", #button_eps) .. profile_name
-    end
-
-    if device.manufacturer_info.vendor_id == AQARA_MANUFACTURER_ID and
-       device.manufacturer_info.product_id == AQARA_CLIMATE_SENSOR_W100_ID then
-      profile_name = profile_name .. "-temperature-humidity"
-    end
-    device:try_update_metadata({ profile = profile_name })
-  end
-end
-
-local function max_press_handler(driver, device, ib, response)
-  local max = ib.data.value or 1 --get max number of presses
-  device.log.debug("Device supports "..max.." presses")
-  -- capability only supports up to 6 presses
-  if max > 6 then
-    log.info("Device supports more than 6 presses")
-    max = 6
-  end
-  local MSL = device:get_endpoints(clusters.Switch.ID, {feature_bitmap=clusters.Switch.types.SwitchFeature.MOMENTARY_SWITCH_LONG_PRESS})
-  local supportsHeld = tbl_contains(MSL, ib.endpoint_id)
-  local values = create_multi_press_values_list(max, supportsHeld)
-  device:emit_event_for_endpoint(ib.endpoint_id, capabilities.button.supportedButtonValues(values, {visibility = {displayed = false}}))
 end
 
 local function info_changed(driver, device, event, args)
@@ -1446,12 +1257,12 @@ local matter_driver_template = {
         [clusters.ValveConfigurationAndControl.attributes.CurrentLevel.ID] = valve_level_attr_handler
       },
       [clusters.PowerSource.ID] = {
-        [clusters.PowerSource.attributes.AttributeList.ID] = power_source_attribute_list_handler,
-        [clusters.PowerSource.attributes.BatChargeLevel.ID] = battery_charge_level_attr_handler,
-        [clusters.PowerSource.attributes.BatPercentRemaining.ID] = battery_percent_remaining_attr_handler,
+        [clusters.PowerSource.attributes.AttributeList.ID] = buttons.power_source_attribute_list_handler,
+        [clusters.PowerSource.attributes.BatChargeLevel.ID] = buttons.battery_charge_level_attr_handler,
+        [clusters.PowerSource.attributes.BatPercentRemaining.ID] = buttons.battery_percent_remaining_attr_handler,
       },
       [clusters.Switch.ID] = {
-        [clusters.Switch.attributes.MultiPressMax.ID] = max_press_handler
+        [clusters.Switch.attributes.MultiPressMax.ID] = buttons.max_press_handler
       },
       [clusters.RelativeHumidityMeasurement.ID] = {
         [clusters.RelativeHumidityMeasurement.attributes.MeasuredValue.ID] = humidity_attr_handler
@@ -1469,10 +1280,10 @@ local matter_driver_template = {
     },
     event = {
       [clusters.Switch.ID] = {
-        [clusters.Switch.events.InitialPress.ID] = initial_press_event_handler,
-        [clusters.Switch.events.LongPress.ID] = long_press_event_handler,
-        [clusters.Switch.events.ShortRelease.ID] = short_release_event_handler,
-        [clusters.Switch.events.MultiPressComplete.ID] = multi_press_complete_event_handler
+        [clusters.Switch.events.InitialPress.ID] = buttons.initial_press_event_handler,
+        [clusters.Switch.events.LongPress.ID] = buttons.long_press_event_handler,
+        [clusters.Switch.events.ShortRelease.ID] = buttons.short_release_event_handler,
+        [clusters.Switch.events.MultiPressComplete.ID] = buttons.multi_press_complete_event_handler
       }
     },
     fallback = matter_handler,
