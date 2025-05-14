609687e006c9bd06ce7bbdfd0343896f1bcd4c25
cjswedes <carter.swedal@smartthings.com>
2025-05-14 15:07:55 -0500

    Matter native attr registration test fixes

--- drivers/SmartThings/matter-switch/src/test/test_electrical_sensor.lua
+++ drivers/SmartThings/matter-switch/src/test/test_electrical_sensor.lua
@@ -725,6 +725,14 @@ test.register_message_test(
       }
     },
     {
+      channel = "devices",
+      direction = "send",
+      message = {
+        "register_native_capability_attr_handler",
+        { device_uuid = mock_device.id, capability_id = "switchLevel", capability_attr_id = "level" }
+      }
+    },
+    {
       channel = "capability",
       direction = "send",
       message = mock_device:generate_test_message("main", capabilities.switchLevel.level(20))
@@ -741,7 +749,15 @@ test.register_message_test(
       channel = "capability",
       direction = "send",
       message = mock_device:generate_test_message("main", capabilities.switch.switch.on())
-    }
+    },
+    {
+      channel = "devices",
+      direction = "send",
+      message = {
+        "register_native_capability_attr_handler",
+        { device_uuid = mock_device.id, capability_id = "switch", capability_attr_id = "switch" }
+      }
+    },
   }
 )
 
