29ea378ee17adf684681ebcab834ea9e3c50a163
Jan Jakubiszyn/SmartThings Integrations (BJ.jakubiszynS) /SRPOL/Engineer/Samsung Electronics <j.jakubiszyn@samsung.com>
2025-05-15 15:46:56 +0200

    Bosch Button Contact test cleanup

--- drivers/SmartThings/matter-sensor/src/test/test_matter_bosch_button_contact.lua
+++ drivers/SmartThings/matter-sensor/src/test/test_matter_bosch_button_contact.lua
@@ -46,9 +46,7 @@ local mock_device = test.mock_device.build_test_matter_device(
                                     clusters.Switch.types.SwitchFeature.MOMENTARY_SWITCH_MULTI_PRESS |
                                     clusters.Switch.types.SwitchFeature.MOMENTARY_SWITCH_LONG_PRESS,
                             cluster_type = "SERVER",
-
                         },
-
                     }
                 }
             }
@@ -70,15 +68,8 @@ local function test_init()
     test.socket.matter:__expect_send({mock_device.id, subscribe_request})
     test.mock_device.add_test_device(mock_device)
     test.socket.device_lifecycle:__queue_receive({ mock_device.id, "added" })
-
     mock_device:set_field("__initial_press_only_2", true, {persist = true})
-
-
-
     test.socket.matter:__expect_send({mock_device.id, clusters.Switch.attributes.MultiPressMax:read(mock_device)})
-
-
-
 end
 
 test.set_test_init_function(test_init)
@@ -294,6 +285,5 @@ test.register_message_test(
             }
         }
 )
+
 test.run_registered_tests()
-
-
