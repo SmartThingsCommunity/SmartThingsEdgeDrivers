a70b2a4c60a67364d2a188178dcf36188631a497
Steven Green <steven.green@smartthings.com>
2025-07-08 12:17:02 -0700

    CHAD-15418 workaround
    endpoint ids change from numbers to strings shortly after install. This also fixes some incorrectly-addressed fields for kickstarter sensors.

--- drivers/SmartThings/zigbee-switch/src/test/test_zll_rgb_bulb.lua
+++ drivers/SmartThings/zigbee-switch/src/test/test_zll_rgb_bulb.lua
@@ -35,7 +35,7 @@ local mock_device = test.mock_device.build_test_zigbee_device(
         manufacturer = "IKEA of Sweden",
         model = "TRADFRI bulb E27 CWS opal 600lm",
         server_clusters = { 0x0006, 0x0008, 0x0300 },
-        profile = 0xC05E,
+        profile_id = 0xC05E,
       }
     }
   }
