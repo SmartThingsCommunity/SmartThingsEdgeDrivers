d0109da24d99202868e7a488207fea7b43e1f8b0
Steven Green <steven.green@smartthings.com>
2025-07-25 17:25:34 -0700

    fixup

--- drivers/SmartThings/zigbee-switch/src/zll-polling/init.lua
+++ drivers/SmartThings/zigbee-switch/src/zll-polling/init.lua
@@ -17,9 +17,9 @@ local constants = require "st.zigbee.constants"
 local clusters = require "st.zigbee.zcl.clusters"
 
 local function zll_profile(opts, driver, device, zb_rx, ...)
-  local endpoint = device.zigbee_endpoints and
+  local endpoint = device.zigbee_endpoints ~= nil and
     (device.zigbee_endpoints[device.fingerprinted_endpoint_id] or device.zigbee_endpoints[tostring(device.fingerprinted_endpoint_id)])
-  if (endpoint and endpoint.profile_id == constants.ZLL_PROFILE_ID) then
+  if (endpoint ~= nil and endpoint.profile_id == constants.ZLL_PROFILE_ID) then
     local subdriver = require("zll-polling")
     return true, subdriver
   else
