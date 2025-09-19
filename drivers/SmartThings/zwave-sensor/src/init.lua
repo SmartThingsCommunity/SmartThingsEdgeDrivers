d9d911ca27090a268db80522067022be7dce4116
janbajc <63789839+janbajc@users.noreply.github.com>
2025-09-19 07:31:28 +0200

    Update init.lua

--- drivers/SmartThings/zwave-sensor/src/init.lua
+++ drivers/SmartThings/zwave-sensor/src/init.lua
@@ -153,7 +153,6 @@ local driver_template = {
     lazy_load_if_possible("timed-tamper-clear"),
     lazy_load_if_possible("wakeup-no-poll"),
     lazy_load_if_possible("apiv6_bugfix"),
-    lazy_load_if_possible("shelly-wave-motion")
   },
   lifecycle_handlers = {
     added = added_handler,
