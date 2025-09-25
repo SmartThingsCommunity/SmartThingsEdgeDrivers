5a0c5abf46cfda591d3ac3071a90dece5f007857
wkhenon <dev.kwalker@gmail.com>
2025-09-25 07:34:04 -0400

    ZLL: use native / default handlers

--- drivers/SmartThings/zigbee-switch/src/zll-dimmer-bulb/init.lua
+++ drivers/SmartThings/zigbee-switch/src/zll-dimmer-bulb/init.lua
@@ -152,57 +152,11 @@ local function device_added(driver, device)
   device:refresh()
 end
 
-local function handle_switch_on(driver, device, cmd)
-  device:send(OnOff.commands.On(device))
-
-  device.thread:call_with_delay(2, function(d)
-    device:refresh()
-  end)
-end
-
-local function handle_switch_off(driver, device, cmd)
-  device:send(OnOff.commands.Off(device))
-
-  device.thread:call_with_delay(2, function(d)
-    device:refresh()
-  end)
-end
-
-local function handle_set_level(driver, device, cmd)
-  local level = math.floor(cmd.args.level / 100.0 * 254)
-  device:send(Level.commands.MoveToLevelWithOnOff(device, level, cmd.args.rate or 0xFFFF))
-
-  device.thread:call_with_delay(2, function(d)
-    device:refresh()
-  end)
-end
-
-local function handle_set_color_temperature(driver, device, cmd)
-  colorTemperature_defaults.set_color_temperature(driver, device, cmd)
-
-  local function query_device()
-    device:refresh()
-  end
-  device.thread:call_with_delay(2, query_device)
-end
-
 local zll_dimmer_bulb = {
   NAME = "ZLL Dimmer Bulb",
   lifecycle_handlers = {
     doConfigure = do_configure,
     added = device_added
-  },
-  capability_handlers = {
-    [capabilities.switch.ID] = {
-      [capabilities.switch.commands.on.NAME] = handle_switch_on,
-      [capabilities.switch.commands.off.NAME] = handle_switch_off
-    },
-    [capabilities.switchLevel.ID] = {
-      [capabilities.switchLevel.commands.setLevel.NAME] = handle_set_level
-    },
-    [capabilities.colorTemperature.ID] = {
-      [capabilities.colorTemperature.commands.setColorTemperature.NAME] = handle_set_color_temperature
-    }
   },
   sub_drivers = {
     require("zll-dimmer-bulb/ikea-xy-color-bulb")
