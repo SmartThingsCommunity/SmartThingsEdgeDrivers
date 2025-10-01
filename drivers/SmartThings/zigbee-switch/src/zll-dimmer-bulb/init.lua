d77a934f2cd1fe288e4c469b1f9bfa12615f11fe
wkhenon <dev.kwalker@gmail.com>
2025-10-01 16:27:39 -0400

    fix: Luacheck linter (pt. 2)

--- drivers/SmartThings/zigbee-switch/src/zll-dimmer-bulb/init.lua
+++ drivers/SmartThings/zigbee-switch/src/zll-dimmer-bulb/init.lua
@@ -12,8 +12,6 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-local clusters = require "st.zigbee.zcl.clusters"
-
 local ZLL_DIMMER_BULB_FINGERPRINTS = {
   ["AduroSmart Eria"] = {
     ["ZLL-DimmableLight"] = true,
