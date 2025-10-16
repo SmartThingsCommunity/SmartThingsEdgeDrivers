afc707690460fbd6a84339c8edf5e9ce5bd4d142
Harrison Carter <harrison.carter@smartthings.com>
2025-10-16 12:32:50 -0500

    add handling for Power Topology cluster

--- drivers/SmartThings/matter-switch/src/switch_utils/device_configuration.lua
+++ drivers/SmartThings/matter-switch/src/switch_utils/device_configuration.lua
@@ -29,10 +29,12 @@ function SwitchDeviceConfiguration.assign_profile_for_onoff_ep(device, server_on
 
   local generic_profile = fields.device_type_profile_map[primary_dt_id]
 
-  if is_child_device and (
-    server_onoff_ep_id == switch_utils.get_product_override_field(device, "ep_id") or
-    generic_profile == switch_utils.get_product_override_field(device, "initial_profile")
-  ) then
+  local static_electrical_tags = switch_utils.get_field_for_endpoint(device, fields.ELECTRICAL_TAGS, server_onoff_ep_id)
+  if static_electrical_tags ~= nil then
+    generic_profile = string.gsub(generic_profile, "-binary", "") .. static_electrical_tags
+  end
+
+  if is_child_device and generic_profile == switch_utils.get_product_override_field(device, "initial_profile") then
     generic_profile = switch_utils.get_product_override_field(device, "target_profile") or generic_profile
   end
 
@@ -50,20 +52,17 @@ function SwitchDeviceConfiguration.create_child_devices(driver, device, server_o
   for idx, ep_id in ipairs(server_onoff_ep_ids) do
     device_num = device_num + 1
     if ep_id ~= default_endpoint_id then -- don't create a child device that maps to the main endpoint
-      local name = string.format("%s %d", device.label, device_num)
-      local child_profile = SwitchDeviceConfiguration.assign_profile_for_onoff_ep(device, ep_id, true)
-      driver:try_create_device({
-        type = "EDGE_CHILD",
-        label = name,
-        profile = child_profile,
-        parent_device_id = device.id,
-        parent_assigned_child_key = string.format("%d", ep_id),
-        vendor_provided_label = name
-      })
-      if idx == 1 and string.find(child_profile, "energy") then
-        -- when energy management is defined in the root endpoint(0), replace it with the first switch endpoint and process it.
-        device:set_field(fields.ENERGY_MANAGEMENT_ENDPOINT, ep_id, {persist = true})
-      end
+      local label_and_name = string.format("%s %d", device.label, device_num)
+      driver:try_create_device(
+        {
+          type = "EDGE_CHILD",
+          label = label_and_name,
+          profile = SwitchDeviceConfiguration.assign_profile_for_onoff_ep(device, ep_id, true),
+          parent_device_id = device.id,
+          parent_assigned_child_key = string.format("%d", ep_id),
+          vendor_provided_label = label_and_name
+        }
+      )
     end
   end
 
@@ -145,9 +144,20 @@ end
 
 -- [[ PROFILE MATCHING AND CONFIGURATIONS ]] --
 
+local function profiling_data_still_required(device)
+  for _, field in pairs(fields.profiling_data) do
+    if device:get_field(field) == nil then
+      return true -- data still required if a field is nil
+    end
+  end
+  return false
+end
+
 function DeviceConfiguration.match_profile(driver, device)
+  if profiling_data_still_required(device) then return end
+
   local default_endpoint_id = switch_utils.find_default_endpoint(device)
-  local updated_profile = nil
+  local updated_profile
 
   if #embedded_cluster_utils.get_endpoints(device, clusters.ValveConfigurationAndControl.ID) > 0 then
     updated_profile = "water-valve"
@@ -165,16 +175,7 @@ function DeviceConfiguration.match_profile(driver, device)
   if switch_utils.tbl_contains(server_onoff_ep_ids, default_endpoint_id) then
     updated_profile = SwitchDeviceConfiguration.assign_profile_for_onoff_ep(device, default_endpoint_id)
     local generic_profile = function(s) return string.find(updated_profile or "", s, 1, true) end
-    if generic_profile("plug-binary") or generic_profile("plug-level") then
-      if switch_utils.check_switch_category_vendor_overrides(device) then
-        updated_profile = string.gsub(updated_profile, "plug", "switch")
-      else
-        local electrical_tags = ""
-        if #embedded_cluster_utils.get_endpoints(device, clusters.ElectricalPowerMeasurement.ID) > 0 then electrical_tags = electrical_tags .. "-power" end
-        if #embedded_cluster_utils.get_endpoints(device, clusters.ElectricalEnergyMeasurement.ID) > 0 then electrical_tags = electrical_tags .. "-energy-powerConsumption" end
-        if electrical_tags ~= "" then updated_profile = string.gsub(updated_profile, "-binary", "") .. electrical_tags end
-      end
-    elseif generic_profile("light-color-level") and #device:get_endpoints(clusters.FanControl.ID) > 0 then
+    if generic_profile("light-color-level") and #device:get_endpoints(clusters.FanControl.ID) > 0 then
       updated_profile = "light-color-level-fan"
     elseif generic_profile("light-level") and #device:get_endpoints(clusters.OccupancySensing.ID) > 0 then
       updated_profile = "light-level-motion"
--- drivers/SmartThings/matter-switch/src/test/test_electrical_sensor_tree.lua
+++ drivers/SmartThings/matter-switch/src/test/test_electrical_sensor_tree.lua
@@ -1,15 +1,32 @@
--- Copyright © 2024 SmartThings, Inc.
--- Licensed under the Apache License, Version 2.0
+-- Copyright 2025 SmartThings
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
 
 local test = require "integration_test"
 local capabilities = require "st.capabilities"
 local clusters = require "st.matter.clusters"
 local t_utils = require "integration_test.utils"
+local uint32 = require "st.matter.data_types.Uint32"
 local version = require "version"
 
 if version.api < 11 then
   clusters.ElectricalEnergyMeasurement = require "embedded_clusters.ElectricalEnergyMeasurement"
   clusters.ElectricalPowerMeasurement = require "embedded_clusters.ElectricalPowerMeasurement"
+  clusters.PowerTopology = require "embedded_clusters.PowerTopology"
+end
+
+if version.api < 16 then
+  clusters.Descriptor = require "embedded_clusters.Descriptor"
 end
 
 local mock_device = test.mock_device.build_test_matter_device({
@@ -33,6 +50,7 @@ local mock_device = test.mock_device.build_test_matter_device({
       clusters = {
         { cluster_id = clusters.ElectricalEnergyMeasurement.ID, cluster_type = "SERVER", feature_map = 14, },
         { cluster_id = clusters.ElectricalPowerMeasurement.ID, cluster_type = "SERVER", feature_map = 0, },
+        { cluster_id = clusters.PowerTopology.ID, cluster_type = "SERVER", feature_map = 2, }, -- TREE_TOPOLOGY
       },
       device_types = {
         { device_type_id = 0x0510, device_type_revision = 1 }, -- Electrical Sensor
@@ -45,55 +63,32 @@ local mock_device = test.mock_device.build_test_matter_device({
         {cluster_id = clusters.LevelControl.ID, cluster_type = "SERVER", feature_map = 2}
       },
       device_types = {
-        { device_type_id = 0x010B, device_type_revision = 1 }, -- Dimmable Plug In Unit
-      }
-    }
-  },
-})
-
-
-local mock_device_periodic = test.mock_device.build_test_matter_device({
-  profile = t_utils.get_profile_definition("plug-energy-powerConsumption.yml"),
-  manufacturer_info = {
-    vendor_id = 0x0000,
-    product_id = 0x0000,
-  },
-  endpoints = {
-    {
-      endpoint_id = 0,
-      clusters = {
-        { cluster_id = clusters.Basic.ID, cluster_type = "SERVER" },
-      },
-      device_types = {
-        { device_type_id = 0x0016, device_type_revision = 1 } -- RootNode
+        { device_type_id = 0x010B, device_type_revision = 1 }, -- OnOff Dimmable Plug
       }
     },
     {
-      endpoint_id = 1,
+      endpoint_id = 3,
       clusters = {
-        { cluster_id = clusters.ElectricalEnergyMeasurement.ID, cluster_type = "SERVER", feature_map = 10, },
+        { cluster_id = clusters.ElectricalEnergyMeasurement.ID, cluster_type = "SERVER", feature_map = 14, },
+        { cluster_id = clusters.PowerTopology.ID, cluster_type = "SERVER", feature_map = 2, }, -- TREE_TOPOLOGY
       },
       device_types = {
-        { device_type_id = 0x0510, device_type_revision = 1 } -- Electrical Sensor
+        { device_type_id = 0x0510, device_type_revision = 1 }, -- Electrical Sensor
       }
     },
     {
-      endpoint_id = 2,
+      endpoint_id = 4,
       clusters = {
         { cluster_id = clusters.OnOff.ID, cluster_type = "SERVER", cluster_revision = 1, feature_map = 0, },
+        { cluster_id = clusters.LevelControl.ID, cluster_type = "SERVER", feature_map = 2},
       },
       device_types = {
-        { device_type_id = 0x010A, device_type_revision = 1 }, -- On Off Plug In Unit
+        { device_type_id = 0x010B, device_type_revision = 1 }, -- OnOff Dimmable Plug
       }
-    }
+    },
   },
 })
 
-local subscribed_attributes_periodic = {
-  clusters.OnOff.attributes.OnOff,
-  clusters.ElectricalEnergyMeasurement.attributes.PeriodicEnergyImported,
-  clusters.ElectricalEnergyMeasurement.attributes.CumulativeEnergyImported,
-}
 local subscribed_attributes = {
   clusters.OnOff.attributes.OnOff,
   clusters.LevelControl.attributes.CurrentLevel,
@@ -134,42 +129,22 @@ local cumulative_report_val_39 = {
   reactive_energy = 0
 }
 
-local periodic_report_val_23 = {
-  energy = 23000,
-  start_timestamp = 0,
-  end_timestamp = 0,
-  start_systime = 0,
-  end_systime = 0,
-  apparent_energy = 0,
-  reactive_energy = 0
-}
-
 local function test_init()
+  test.mock_device.add_test_device(mock_device)
   local subscribe_request = subscribed_attributes[1]:subscribe(mock_device)
   for i, cluster in ipairs(subscribed_attributes) do
       if i > 1 then
           subscribe_request:merge(cluster:subscribe(mock_device))
       end
   end
+  test.socket.device_lifecycle:__queue_receive({ mock_device.id, "added" })
+  local read_req = clusters.Descriptor.attributes.PartsList:read(mock_device.id, 1)
+  read_req:merge(clusters.Descriptor.attributes.PartsList:read(mock_device.id, 3))
+  test.socket.matter:__expect_send({ mock_device.id, read_req })
   test.socket.matter:__expect_send({ mock_device.id, subscribe_request })
-  test.mock_device.add_test_device(mock_device)
+  test.socket.matter:__expect_send({ mock_device.id, subscribe_request })
 end
 test.set_test_init_function(test_init)
-
-local function test_init_periodic()
-  test.mock_device.add_test_device(mock_device_periodic)
-  local subscribe_request = subscribed_attributes_periodic[1]:subscribe(mock_device_periodic)
-  for i, cluster in ipairs(subscribed_attributes_periodic) do
-    if i > 1 then
-        subscribe_request:merge(cluster:subscribe(mock_device_periodic))
-    end
-  end
-  test.socket.matter:__expect_send({ mock_device_periodic.id, subscribe_request })
-  test.socket.device_lifecycle:__queue_receive({ mock_device_periodic.id, "added" })
-  test.socket.matter:__expect_send({ mock_device_periodic.id, subscribe_request })
-  test.socket.device_lifecycle:__queue_receive({ mock_device_periodic.id, "init" })
-  test.socket.matter:__expect_send({ mock_device_periodic.id, subscribe_request })
-end
 
 test.register_message_test(
 	"On command should send the appropriate commands",
@@ -323,105 +298,25 @@ test.register_coroutine_test(
     end
 )
 
-test.register_message_test(
-  "Periodic Energy as subordinate to Cumulative Energy measurement should not generate any messages",
-  {
-    {
-      channel = "matter",
-      direction = "receive",
-      message = {
-        mock_device.id,
-        clusters.ElectricalEnergyMeasurement.server.attributes.PeriodicEnergyImported:build_test_report_data(mock_device, 1, periodic_report_val_23)
-      }
-    },
-    {
-      channel = "matter",
-      direction = "receive",
-      message = {
-        mock_device.id,
-        clusters.ElectricalEnergyMeasurement.server.attributes.PeriodicEnergyImported:build_test_report_data(mock_device, 1, periodic_report_val_23)
-      }
-    },
-  }
-)
-
-test.register_coroutine_test(
-  "Periodic Energy measurement should generate correct messages",
-    function()
-      test.mock_time.advance_time(901) -- move time 15 minutes past 0 (this can be assumed to be true in practice in all cases)
-      test.socket.matter:__queue_receive(
-        {
-          mock_device_periodic.id,
-          clusters.ElectricalEnergyMeasurement.server.attributes.PeriodicEnergyImported:build_test_report_data(
-            mock_device_periodic, 1, periodic_report_val_23
-          )
-        }
-      )
-      test.socket.capability:__expect_send(
-        mock_device_periodic:generate_test_message("main", capabilities.energyMeter.energy({value = 23.0, unit="Wh"}))
-      )
-      test.socket.capability:__expect_send(
-        mock_device_periodic:generate_test_message("main", capabilities.powerConsumptionReport.powerConsumption({
-          start = "1970-01-01T00:00:00Z",
-          ["end"] = "1970-01-01T00:15:00Z",
-          deltaEnergy = 0.0,
-          energy = 23.0
-        }))
-      )
-      test.socket.matter:__queue_receive(
-        {
-          mock_device_periodic.id,
-          clusters.ElectricalEnergyMeasurement.server.attributes.PeriodicEnergyImported:build_test_report_data(
-            mock_device_periodic, 1, periodic_report_val_23
-          )
-        }
-      )
-      test.socket.capability:__expect_send(
-        mock_device_periodic:generate_test_message("main", capabilities.energyMeter.energy({value = 46.0, unit="Wh"}))
-      )
-      test.wait_for_events()
-      test.mock_time.advance_time(2000)
-      test.socket.matter:__queue_receive(
-        {
-          mock_device_periodic.id,
-          clusters.ElectricalEnergyMeasurement.server.attributes.PeriodicEnergyImported:build_test_report_data(
-            mock_device_periodic, 1, periodic_report_val_23
-          )
-        }
-      )
-      test.socket.capability:__expect_send(
-        mock_device_periodic:generate_test_message("main", capabilities.energyMeter.energy({value = 69.0, unit="Wh"}))
-      )
-      test.socket.capability:__expect_send(
-        mock_device_periodic:generate_test_message("main", capabilities.powerConsumptionReport.powerConsumption({
-          start = "1970-01-01T00:15:01Z",
-          ["end"] = "1970-01-01T00:48:20Z",
-          deltaEnergy = 46.0,
-          energy = 69.0
-        }))
-      )
-    end,
-    { test_init = test_init_periodic }
-)
-
 test.register_coroutine_test(
   "Test profile change on init for Electrical Sensor device type",
   function()
+
     test.socket.device_lifecycle:__queue_receive({ mock_device.id, "doConfigure" })
-    mock_device:expect_metadata_update({ profile = "plug-level-power-energy-powerConsumption" })
     mock_device:expect_metadata_update({ provisioning_state = "PROVISIONED" })
+    test.wait_for_events()
+    test.socket.matter:__queue_receive({ mock_device.id, clusters.Descriptor.attributes.PartsList:build_test_report_data(mock_device, 1, {uint32(2)})})
+    test.socket.matter:__queue_receive({ mock_device.id, clusters.Descriptor.attributes.PartsList:build_test_report_data(mock_device, 3, {uint32(4)})})
+    mock_device:expect_metadata_update({ profile = "plug-level-power-energy-powerConsumption" })
+    mock_device:expect_device_create({
+      type = "EDGE_CHILD",
+      label = "nil 2",
+      profile = "plug-level-energy-powerConsumption",
+      parent_device_id = mock_device.id,
+      parent_assigned_child_key = string.format("%d", 4)
+    })
   end,
   { test_init = test_init }
-)
-
-test.register_coroutine_test(
-  "Test profile change on init for only Periodic Electrical Sensor device type",
-  function()
-    test.socket.device_lifecycle:__queue_receive({ mock_device_periodic.id, "doConfigure" })
-    mock_device_periodic:expect_metadata_update({ profile = "plug-energy-powerConsumption" })
-    mock_device_periodic:expect_metadata_update({ provisioning_state = "PROVISIONED" })
-  end,
-  { test_init = test_init_periodic }
 )
 
 test.register_message_test(
