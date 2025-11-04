cec569c7c2a963ee9348c1ba125ab068699e5f65
Nick DeBoom <nick.deboom@smartthings.com>
2025-11-04 16:10:41 -0600

    remove unneeded spaces

--- drivers/SmartThings/matter-switch/src/sub_drivers/aqara_cube/can_handle.lua
+++ drivers/SmartThings/matter-switch/src/sub_drivers/aqara_cube/can_handle.lua
@@ -1,7 +1,7 @@
 -- Copyright 2025 SmartThings, Inc.
 -- Licensed under the Apache License, Version 2.0
 
-return function(opts, driver, device )
+return function(opts, driver, device)
   if device.network_type == require("st.device").NETWORK_TYPE_MATTER then
     local name = string.format("%s", device.manufacturer_info.product_name)
     if string.find(name, "Aqara Cube T1 Pro") then
--- drivers/SmartThings/matter-switch/src/sub_drivers/eve_energy/can_handle.lua
+++ drivers/SmartThings/matter-switch/src/sub_drivers/eve_energy/can_handle.lua
@@ -1,7 +1,7 @@
 -- Copyright 2025 SmartThings, Inc.
 -- Licensed under the Apache License, Version 2.0
 
-return function(opts, driver, device )
+return function(opts, driver, device)
   local EVE_MANUFACTURER_ID = 0x130A
   if device.network_type == require("st.device").NETWORK_TYPE_MATTER and
     device.manufacturer_info.vendor_id == EVE_MANUFACTURER_ID then
