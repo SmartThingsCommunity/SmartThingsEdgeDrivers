4a790ec0dbf297bd9c5703a6db95c72e351590e6
Harrison Carter <137556605+hcarter-775@users.noreply.github.com>
2025-11-05 11:21:41 -0600

    Matter Switch: Update Directory Names and Copyright Information (#2524)
    * update Matter Switch file organization
    
    * add copyright to embedded clusters

--- drivers/SmartThings/matter-switch/src/test/test_matter_multi_button_motion.lua
+++ drivers/SmartThings/matter-switch/src/test/test_matter_multi_button_motion.lua
@@ -1,3 +1,6 @@
+-- Copyright © 2025 SmartThings, Inc.
+-- Licensed under the Apache License, Version 2.0
+
 local test = require "integration_test"
 local capabilities = require "st.capabilities"
 local t_utils = require "integration_test.utils"
