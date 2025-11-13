d723c873887d6acba882e3d1a1cf3ccd3f98ec91
Alec Lorimer <alec.lorimer@smartthings.com>
2025-11-13 01:34:44 -0600

    CHAD-17042:Lazy loading v2 of sub drivers and copyright updates

---
+++ drivers/SmartThings/zigbee-motion-sensor/src/frient/fingerprints.lua
@@ -0,0 +1,10 @@
+-- Copyright 2025 SmartThings, Inc.
+-- Licensed under the Apache License, Version 2.0
+
+local FRIENT_DEVICE_FINGERPRINTS = {
+  { mfr = "frient A/S", model = "MOSZB-140"},
+  { mfr = "frient A/S", model = "MOSZB-141"},
+  { mfr = "frient A/S", model = "MOSZB-153"}
+}
+
+return FRIENT_DEVICE_FINGERPRINTS
