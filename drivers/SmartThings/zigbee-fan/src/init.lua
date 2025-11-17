bab6f23ce2ba76ce83a281a0c82a95109e448777
Alec Lorimer <alec.lorimer@smartthings.com>
2025-11-17 15:25:36 -0600

    zigbee-fan:[automated] lazy loading changes

--- drivers/SmartThings/zigbee-fan/src/init.lua
+++ drivers/SmartThings/zigbee-fan/src/init.lua
@@ -1,16 +1,6 @@
--- Copyright 2024 SmartThings
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---     http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
+-- Copyright 2025 SmartThings, Inc.
+-- Licensed under the Apache License, Version 2.0
+
 
 local capabilities = require "st.capabilities"
 local ZigbeeDriver = require "st.zigbee"
@@ -32,9 +22,7 @@ local zigbee_fan_driver = {
     capabilities.switchLevel,
     capabilities.fanspeed
   },
-  sub_drivers = {
-    require("fan-light")
-  },
+  sub_drivers = require("sub_drivers"),
   lifecycle_handlers = {
     init = device_init
   },
