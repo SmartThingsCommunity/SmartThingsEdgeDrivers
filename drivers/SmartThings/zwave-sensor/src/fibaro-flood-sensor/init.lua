56083499d5f1591fc7d945a5487da1edb39e8d10
Alec Lorimer <alec.lorimer@smartthings.com>
2025-11-17 15:25:27 -0600

    CHAD-17092: zwave-sensor lazy loading of sub-drivers

--- drivers/SmartThings/zwave-sensor/src/fibaro-flood-sensor/init.lua
+++ drivers/SmartThings/zwave-sensor/src/fibaro-flood-sensor/init.lua
@@ -1,16 +1,7 @@
--- Copyright 2022 SmartThings
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---     http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
+-- Copyright 2022 SmartThings, Inc.
+-- Licensed under the Apache License, Version 2.0
+
+
 
 local capabilities = require "st.capabilities"
 --- @type st.zwave.CommandClass
@@ -25,17 +16,6 @@ local SensorBinary = (require "st.zwave.CommandClass.SensorBinary")({ version = 
 
 local preferences = require "preferences"
 local configurations = require "configurations"
-
-local FIBARO_MFR_ID = 0x010F
-local FIBARO_FLOOD_PROD_TYPES = { 0x0000, 0x0B00 }
-
-local function can_handle_fibaro_flood_sensor(opts, driver, device, ...)
-  if device:id_match(FIBARO_MFR_ID, FIBARO_FLOOD_PROD_TYPES, nil) then
-    local subdriver = require("fibaro-flood-sensor")
-    return true, subdriver
-  else return false end
-end
-
 
 local function basic_set_handler(self, device, cmd)
   local value = cmd.args.target_value and cmd.args.target_value or cmd.args.value
@@ -96,7 +76,7 @@ local fibaro_flood_sensor = {
   lifecycle_handlers = {
     doConfigure = do_configure
   },
-  can_handle = can_handle_fibaro_flood_sensor
+  can_handle = require("fibaro-flood-sensor.can_handle"),
 }
 
 return fibaro_flood_sensor
