58de43e1941183e03db917df39c6dbf78107d208
Alec Lorimer <alec.lorimer@smartthings.com>
2025-11-17 15:25:09 -0600

    CHAD-17079: zigbee-valve lazy load sub-drivers

--- drivers/SmartThings/zigbee-valve/src/init.lua
+++ drivers/SmartThings/zigbee-valve/src/init.lua
@@ -1,16 +1,6 @@
--- Copyright 2022 SmartThings
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---     http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
+-- Copyright 2022 SmartThings, Inc.
+-- Licensed under the Apache License, Version 2.0
+
 
 local ZigbeeDriver = require "st.zigbee"
 local defaults = require "st.zigbee.defaults"
@@ -51,10 +41,7 @@ local zigbee_valve_driver_template = {
   lifecycle_handlers = {
     added = device_added
   },
-  sub_drivers = {
-    require("sinope"),
-    require("ezex")
-  },
+  sub_drivers = require("sub_drivers"),
   health_check = false,
 }
 
