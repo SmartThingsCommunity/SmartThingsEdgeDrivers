9009301be1d51ab7ea7986951735ab4ce0f57160
Alec Lorimer <alec.lorimer@smartthings.com>
2025-11-17 15:25:31 -0600

    zigbee-humidity-sensor:[automated] lazy loading changes

---
+++ drivers/SmartThings/zigbee-humidity-sensor/src/frient-sensor/fingerprints.lua
@@ -0,0 +1,9 @@
+-- Copyright 2025 SmartThings, Inc.
+-- Licensed under the Apache License, Version 2.0
+
+local FRIENT_TEMP_HUMUDITY_SENSOR_FINGERPRINTS = {
+  { mfr = "frient A/S", model = "HMSZB-110" },
+  { mfr = "frient A/S", model = "HMSZB-120" }
+}
+
+return FRIENT_TEMP_HUMUDITY_SENSOR_FINGERPRINTS
