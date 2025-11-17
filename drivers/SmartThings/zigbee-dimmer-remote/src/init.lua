bf17929604dbffdeea62acfff9fd0b3bc571c9b4
Alec Lorimer <alec.lorimer@smartthings.com>
2025-11-17 15:25:20 -0600

    zigbee-dimmer-remote:[automated] lazy loading changes

--- drivers/SmartThings/zigbee-dimmer-remote/src/init.lua
+++ drivers/SmartThings/zigbee-dimmer-remote/src/init.lua
@@ -1,16 +1,6 @@
--- Copyright 2022 SmartThings
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---     http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
+-- Copyright 2025 SmartThings, Inc.
+-- Licensed under the Apache License, Version 2.0
+
 
 local capabilities = require "st.capabilities"
 local ZigbeeDriver = require "st.zigbee"
@@ -40,9 +30,7 @@ local zigbee_dimmer_remote_driver_template = {
     lifecycle_handlers = {
       init = device_init,
     },
-    sub_drivers = { require("zigbee-accessory-dimmer"), require("zigbee-battery-accessory-dimmer")},
-    health_check = false,
-}
+    sub_drivers = require("sub_drivers"),
 
 defaults.register_for_default_handlers(zigbee_dimmer_remote_driver_template, zigbee_dimmer_remote_driver_template.supported_capabilities)
 local zigbee_dimmer_remote = ZigbeeDriver("zigbee_dimmer_remote", zigbee_dimmer_remote_driver_template)
