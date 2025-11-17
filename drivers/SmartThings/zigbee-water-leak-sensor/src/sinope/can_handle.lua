7fdf4e1f5dc71870d129571db28e6cbe56455446
Alec Lorimer <alec.lorimer@smartthings.com>
2025-11-17 15:25:10 -0600

    zigbee-water-leak-sensor:[automated] lazy loading changes

---
+++ drivers/SmartThings/zigbee-water-leak-sensor/src/sinope/can_handle.lua
@@ -0,0 +1,12 @@
+-- Copyright 2025 SmartThings, Inc.
+-- Licensed under the Apache License, Version 2.0
+
+local is_sinope_water_sensor = function(opts, driver, device)
+  if device:get_manufacturer() == SINOPE_TECHNOLOGIES_MFR_STRING then
+    return true, require("sinope")
+  else
+    return false
+  end
+end
+
+return is_sinope_water_sensor
