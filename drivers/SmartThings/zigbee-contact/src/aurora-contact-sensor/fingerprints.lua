bd7b627e1091a4388e030cf0ca3a76102ceba9da
Alec Lorimer <alec.lorimer@smartthings.com>
2025-11-17 15:25:05 -0600

    CHAD-17065: zigbee-contact lazy loading subdrivers

---
+++ drivers/SmartThings/zigbee-contact/src/aurora-contact-sensor/fingerprints.lua
@@ -0,0 +1,9 @@
+-- Copyright 2025 SmartThings, Inc.
+-- Licensed under the Apache License, Version 2.0
+
+local AURORA_CONTACT_FINGERPRINTS = {
+  { mfr = "Aurora", model = "DoorSensor50AU" }, -- Aurora Smart Door/Window Sensor
+  { mfr = "Aurora", model = "WindowSensor51AU" }   -- Aurora Smart Door/Window Sensor
+}
+
+return AURORA_CONTACT_FINGERPRINTS
