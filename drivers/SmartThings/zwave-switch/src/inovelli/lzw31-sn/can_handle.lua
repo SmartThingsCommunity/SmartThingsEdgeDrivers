37691d50b13b618f7ce058743a5ce9c9972d873d
Inovelli <37669481+InovelliUSA@users.noreply.github.com>
2025-11-26 13:44:19 -0700

    WWSTCERT-8360 Inovelli VZW32-SN: Adding support for this device for the WWST program (#2371)
    * Add support for Inovelli mmWave switch
    
    * Fix for measurement unit being cm (not mm)
    
    * Configure illuminance reporting and fix p101-106 unit incorrect
    
    * adjusting lux reporting. remove p117 as mmwave param and updated its options
    
    * initializing values for occupancy, illuminance, and binding to occupancy cluster
    
    * add ability to reset energy meter
    
    * add ability to reset energy meter
    
    * adjusting some default parameters and adding ota image notify for firmware update process during certification
    
    * adding missing OTAUpgrade declaration
    
    * Inovelli - adding support for vzw32-sn for wwst
    
    * accidentally included some vzm32-sn (zigbee) changes in this branch. Removing them
    
    * some updates from linter results. Notification turns on when color changed. Update the app UI
    
    * Removing unsupported capabilities
    
    * fixing mmwave reset command
    
    * remove some preferences. Prevent extra events being sent for child device during init
    
    * add copyright info
    
    * remove firmware update capability
    
    * remove unused configuration handler and notification class
    
    * removing on/off and level handlers
    
    * removing version report features
    
    * add unit tests
    
    * updating notification profile as requested by ST
    
    * update unit test for different rgbw profile
    
    * remove versionget command
    
    * fix lua errors
    
    * some more fixes for test unit files
    
    * missed an unused variable
    
    * modify unit test files. Changes to device init to be more efficient
    
    * Remove unused code
    
    * linter fixes
    
    * remove unused init function
    
    * fixing linter error
    
    * removing firmware update from rgbw bulb. Adding custom refresh handler
    
    * modify refresh command so it doesn't kill the driver
    
    * consolidating drivers. Updating some unit tests
    
    * Fixing some linter warnings
    
    * fix inovelli button test error
    
    * fix linter warning on button test
    
    * fix color init for lzw31 black switch
    
    * better naming of driver files
    
    * will updated supportedValues for held and down_hold events. This can likely be removed in the future
    
    * linter warnings fixed
    
    * linter warnings fixed
    
    * fix minor formatting issues and copyright issue
    
    ---------
    
    Co-authored-by: InovelliUSA <tech@inovelli.com>

--- drivers/SmartThings/zwave-switch/src/inovelli/lzw31-sn/can_handle.lua
+++ drivers/SmartThings/zwave-switch/src/inovelli/lzw31-sn/can_handle.lua
@@ -3,19 +3,17 @@
 
 local INOVELLI_MANUFACTURER_ID = 0x031E
 local INOVELLI_LZW31SN_PRODUCT_TYPE = 0x0001
-local INOVELLI_LZW31_PRODUCT_TYPE = 0x0003
 local INOVELLI_DIMMER_PRODUCT_ID = 0x0001
 
-local function can_handle_inovelli_led(opts, driver, device, ...)
+local function can_handle_lzw31sn(opts, driver, device, ...)
   if device:id_match(
     INOVELLI_MANUFACTURER_ID,
-    {INOVELLI_LZW31SN_PRODUCT_TYPE, INOVELLI_LZW31_PRODUCT_TYPE},
+    INOVELLI_LZW31SN_PRODUCT_TYPE,
     INOVELLI_DIMMER_PRODUCT_ID
   ) then
-    local subdriver = require("inovelli-LED")
-    return true, subdriver
+    return true, require("inovelli.lzw31-sn")
   end
   return false
 end
 
-return can_handle_inovelli_led
+return can_handle_lzw31sn
