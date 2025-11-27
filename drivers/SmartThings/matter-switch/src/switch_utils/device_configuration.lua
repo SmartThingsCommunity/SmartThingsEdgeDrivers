23045f18aeae5742992757a1574a843568752149
Jan Jakubiszyn/SmartThings Integrations (BJ.jakubiszynS) /SRPOL/Engineer/Samsung Electronics <j.jakubiszyn@samsung.com>
2025-11-27 15:46:08 +0100

    Implementing changes from the code review: edit of the device profile name and capability order, edit of the match_profile's condition

--- drivers/SmartThings/matter-switch/src/switch_utils/device_configuration.lua
+++ drivers/SmartThings/matter-switch/src/switch_utils/device_configuration.lua
@@ -193,7 +193,7 @@ function DeviceConfiguration.match_profile(driver, device)
     if generic_profile("light-color-level") and #device:get_endpoints(clusters.FanControl.ID) > 0 then
       updated_profile = "light-color-level-fan"
     elseif generic_profile("light-level") and #device:get_endpoints(clusters.OccupancySensing.ID) > 0 then
-      updated_profile = "light-level-motion"
+      updated_profile = switch_utils.get_product_override_field(device, "target_profile") or "light-level-motion"
     elseif generic_profile("plug-binary") or generic_profile("plug-level") then
       if switch_utils.check_switch_category_vendor_overrides(device) then
         updated_profile = string.gsub(updated_profile, "plug", "switch")
