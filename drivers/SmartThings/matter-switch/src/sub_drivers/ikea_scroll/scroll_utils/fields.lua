a5bc31b9dde500439e5fbf8c8a4084aae702604c
Harrison Carter <harrison.carter@smartthings.com>
2025-12-03 11:49:15 -0600

    update comment

--- drivers/SmartThings/matter-switch/src/sub_drivers/ikea_scroll/scroll_utils/fields.lua
+++ drivers/SmartThings/matter-switch/src/sub_drivers/ikea_scroll/scroll_utils/fields.lua
@@ -11,7 +11,8 @@ IkeaScrollFields.ENDPOINT_POWER_SOURCE = 0
 -- Switch Endpoints used for basic press functionality
 IkeaScrollFields.ENDPOINTS_PRESS = {3, 6, 9}
 
--- Required Events for the ENDPOINTS_PRESS. Ignore InitialPress since this slows handling.
+-- Required Events for the ENDPOINTS_PRESS. Remove the default subscription to
+-- InitialPress since this is a MultiPress device and InitialPress will be ignored.
 IkeaScrollFields.switch_press_subscribed_events = {
   clusters.Switch.events.MultiPressComplete.ID,
   clusters.Switch.events.LongPress.ID,
