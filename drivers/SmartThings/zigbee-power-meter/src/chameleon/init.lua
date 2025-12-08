b5ea336829081f1d0b762d5b67815090bc631592
peter_wadsworth <peter.wadsworth@chameleontechnology.co.uk>
2025-12-08 16:05:59 +0000

    ct_clap_driver_wwsm Added longer copyright at top of our init.lua file removed comment about tempeature and battery removed battery level handler to main init.lua file under capabilities.battery, temperature syayed as its cluster 0x0402 not the normal temperature cluster and didn't update the temperature

--- drivers/SmartThings/zigbee-power-meter/src/chameleon/init.lua
+++ drivers/SmartThings/zigbee-power-meter/src/chameleon/init.lua
@@ -1,5 +1,16 @@
--- Copyright 2025 SmartThings, Inc.
--- Licensed under the Apache License, Version 2.0
+-- Copyright 2025 SmartThings
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
 
 local clusters = require "st.zigbee.zcl.clusters"
 local capabilities = require "st.capabilities"
@@ -12,7 +23,6 @@ local ZIGBEE_FINGERPRINT = {
   {model = "CT101xxxx" }
 }
 
--- temperature: 0.5C, battery level remaining: 1%
 local configuration = {
   {
     cluster = TemperatureMeasurement.ID,
@@ -81,9 +91,9 @@ local ct_clamp_battery_temperature_handler = {
   NAME = "ct_clamp_battery_temperature_handler",
   zigbee_handlers = {
     attr = {
-      [PowerConfiguration.ID] = {
-        [PowerConfiguration.attributes.BatteryPercentageRemaining.ID] = battery_level_handler
-      },
+      --[PowerConfiguration.ID] = {
+      --  [PowerConfiguration.attributes.BatteryPercentageRemaining.ID] = battery_level_handler
+      --},
       [TemperatureMeasurement.ID] = {
         [TemperatureMeasurement.attributes.CurrentTemperature.ID] = temperature_handler
       }
