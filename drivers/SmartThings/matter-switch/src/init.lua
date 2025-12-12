9be37239fb3b93769a3c6ee93e160c544890b773
Harrison Carter <harrison.carter@smartthings.com>
2025-12-12 12:49:46 -0600

    add support for fan as a parent device

--- drivers/SmartThings/matter-switch/src/init.lua
+++ drivers/SmartThings/matter-switch/src/init.lua
@@ -59,14 +59,15 @@ function SwitchLifecycleHandlers.driver_switched(driver, device)
 end
 
 function SwitchLifecycleHandlers.info_changed(driver, device, event, args)
-  if device.profile.id ~= args.old_st_store.profile.id then
+  if device.profile.id ~= args.old_st_store.profile.id or device:get_field(fields.MODULAR_PROFILE_UPDATED) then
+    device:set_field(fields.MODULAR_PROFILE_UPDATED, nil)
     if device.network_type == device_lib.NETWORK_TYPE_MATTER then
       device:subscribe()
       button_cfg.configure_buttons(device,
         device:get_endpoints(clusters.Switch.ID, {feature_bitmap=clusters.Switch.types.SwitchFeature.MOMENTARY_SWITCH})
       )
     elseif device.network_type == device_lib.NETWORK_TYPE_CHILD then
-      switch_utils.update_subscriptions(device:get_parent_device()) -- parent device required to scan through EPs and update subscriptions
+      switch_utils.update_subscriptions(device:get_parent_device()) -- parent device required to scan through EPs and update subscriptions      
     end
   end
 
--- drivers/SmartThings/matter-switch/src/switch_utils/fields.lua
+++ drivers/SmartThings/matter-switch/src/switch_utils/fields.lua
@@ -42,6 +42,7 @@ SwitchFields.DEVICE_TYPE_ID = {
   DIMMABLE_PLUG_IN_UNIT = 0x010B,
   DOORBELL = 0x0143,
   ELECTRICAL_SENSOR = 0x0510,
+  FAN = 0x002B,
   GENERIC_SWITCH = 0x000F,
   MOUNTED_ON_OFF_CONTROL = 0x010F,
   MOUNTED_DIMMABLE_LOAD_CONTROL = 0x0110,
@@ -150,6 +151,8 @@ SwitchFields.ELECTRICAL_SENSOR_EPS = "__electrical_sensor_eps"
 --- used in tandem with an EP ID. Stores the required electrical tags "-power", "-energy-powerConsumption", etc.
 --- for an Electrical Sensor EP with a "primary" endpoint, used during device profling.
 SwitchFields.ELECTRICAL_TAGS = "__electrical_tags"
+
+SwitchFields.MODULAR_PROFILE_UPDATED = "__modular_profile_updated"
 
 SwitchFields.profiling_data = {
   POWER_TOPOLOGY = "__power_topology",
