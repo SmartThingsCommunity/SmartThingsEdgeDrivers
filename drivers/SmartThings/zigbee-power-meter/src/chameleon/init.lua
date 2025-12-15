c940d2d05683e9055dddcfc7478b5033916a6067
peter_wadsworth <peter.wadsworth@chameleontechnology.co.uk>
2025-12-15 15:13:00 +0000

    ct_clamp_driver_wwsm remove battery percentage config, and use defaults remove battery_level_handler as usinf the capabilites function remove log function in temperature handler adjust spacing in is_chameleon_ct_clamp function

--- drivers/SmartThings/zigbee-power-meter/src/chameleon/init.lua
+++ drivers/SmartThings/zigbee-power-meter/src/chameleon/init.lua
@@ -31,41 +31,21 @@ local configuration = {
     maximum_interval = 3600,
     data_type = TemperatureMeasurement.attributes.CurrentTemperature.base_type,
     reportable_change = 1
-  },
-  {
-    cluster = PowerConfiguration.ID,
-    attribute = PowerConfiguration.attributes.BatteryPercentageRemaining.ID,
-    minimum_interval = 30,
-    maximum_interval = 3600,
-    data_type = PowerConfiguration.attributes.BatteryPercentageRemaining.base_type,
-    reportable_change = 2
   }
 }
 
 local is_chameleon_ct_clamp = function(opts, driver, device)
   for _, fingerprint in ipairs(ZIGBEE_FINGERPRINT) do
-      if device:get_model() == fingerprint.model then
-         return true
-      end
+    if device:get_model() == fingerprint.model then
+      return true
+    end
   end
   return false
-end
-
-local function battery_level_handler(driver, device, value, _zb_rx)
-  if type(value.value) == "number" then
-    local number = value.value/2
-    local integer_result = math.floor(number)
-    device:emit_event(capabilities.battery.battery(integer_result))
-  else
-      log.error("Invalid battery level value received: " .. tostring(value.value))
-  end
 end
 
 local function temperature_handler(driver, device, value, _zb_rx)
   if type(value.value) == "number" then
     device:emit_event(capabilities.temperatureMeasurement.temperature({ value = value.value, unit = "C" }))
-  else
-      log.error("Invalid temperature value received: " .. tostring(value.value))
   end
 end
 
