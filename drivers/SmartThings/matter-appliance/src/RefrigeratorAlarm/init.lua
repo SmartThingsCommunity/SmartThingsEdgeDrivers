5da406dc72153d4fe05ccce92ba99faa4cfe59b7
Alec Lorimer <alec.lorimer@smartthings.com>
2025-12-15 15:22:12 -0600

    CHAD-17152: Lazy loading of matter-appliance sub-drivers

--- drivers/SmartThings/matter-appliance/src/RefrigeratorAlarm/init.lua
+++ drivers/SmartThings/matter-appliance/src/RefrigeratorAlarm/init.lua
@@ -1,3 +1,7 @@
+-- Copyright 2025 SmartThings, Inc.
+-- Licensed under the Apache License, Version 2.0
+
+
 local RefrigeratorAlarmServerAttributes = require "RefrigeratorAlarm.server.attributes"
 local RefrigeratorAlarmTypes = require "RefrigeratorAlarm.types"
 
