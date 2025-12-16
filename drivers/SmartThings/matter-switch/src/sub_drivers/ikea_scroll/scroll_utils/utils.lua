dfa35d3f2c47ba66b876fddbdd28394d1d0732c8
Harrison Carter <harrison.carter@smartthings.com>
2025-12-16 11:47:00 -0600

    initial knob and stateless capability support

--- drivers/SmartThings/matter-switch/src/sub_drivers/ikea_scroll/scroll_utils/utils.lua
+++ drivers/SmartThings/matter-switch/src/sub_drivers/ikea_scroll/scroll_utils/utils.lua
@@ -8,11 +8,24 @@ local scroll_fields = require "sub_drivers.ikea_scroll.scroll_utils.fields"
 local IkeaScrollUtils = {}
 
 -- override subscribe function to prevent subscribing to additional events from the main driver
+-- and to subscribe to extra events for particular endpoints
 function IkeaScrollUtils.subscribe(device)
   local subscribe_request = im.InteractionRequest(im.InteractionRequest.RequestType.SUBSCRIBE, {})
-  for _, ep_press in ipairs(scroll_fields.ENDPOINTS_PRESS) do
+  for _, ep_push in ipairs(scroll_fields.ENDPOINTS_PUSH) do
     for _, switch_event in ipairs(scroll_fields.switch_press_subscribed_events) do
-      local ib = im.InteractionInfoBlock(ep_press, clusters.Switch.ID, nil, switch_event)
+      local ib = im.InteractionInfoBlock(ep_push, clusters.Switch.ID, nil, switch_event)
+      subscribe_request:with_info_block(ib)
+    end
+  end
+  for _, ep_up in ipairs(scroll_fields.ENDPOINTS_UP_SCROLL) do
+    for _, switch_event in ipairs(scroll_fields.switch_scroll_subscribed_events) do
+      local ib = im.InteractionInfoBlock(ep_up, clusters.Switch.ID, nil, switch_event)
+      subscribe_request:with_info_block(ib)
+    end
+  end
+  for _, ep_down in ipairs(scroll_fields.ENDPOINTS_DOWN_SCROLL) do
+    for _, switch_event in ipairs(scroll_fields.switch_scroll_subscribed_events) do
+      local ib = im.InteractionInfoBlock(ep_down, clusters.Switch.ID, nil, switch_event)
       subscribe_request:with_info_block(ib)
     end
   end
