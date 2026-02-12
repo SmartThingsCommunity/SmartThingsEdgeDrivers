a5c4d0f6c7990a88ebd5b98c3ac6d660fa5c5d76
cjswedes <carter.swedal@smartthings.com>
2026-02-12 13:10:33 -0600

    Fixup tests for new native handlers in 60

--- drivers/SmartThings/zigbee-switch/src/test/test_all_capability_zigbee_bulb.lua
+++ drivers/SmartThings/zigbee-switch/src/test/test_all_capability_zigbee_bulb.lua
@@ -233,6 +233,14 @@ test.register_message_test(
         channel = "capability",
         direction = "send",
         message = mock_device:generate_test_message("main", capabilities.colorControl.hue(0))
+      },
+      {
+        channel = "devices",
+        direction = "send",
+        message = {
+          "register_native_capability_attr_handler",
+          { device_uuid = mock_device.id, capability_id = "colorControl", capability_attr_id = "hue" }
+        }
       }
     }
 )
@@ -250,6 +258,14 @@ test.register_message_test(
         channel = "capability",
         direction = "send",
         message = mock_device:generate_test_message("main", capabilities.colorControl.saturation(50))
+      },
+      {
+        channel = "devices",
+        direction = "send",
+        message = {
+          "register_native_capability_attr_handler",
+          { device_uuid = mock_device.id, capability_id = "colorControl", capability_attr_id = "saturation" }
+        }
       }
     }
 )
