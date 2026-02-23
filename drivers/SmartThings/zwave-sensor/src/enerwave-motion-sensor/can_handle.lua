3eb5766bfc02ce08b02dace355b35ec363e2b581
Alec Lorimer <alec.lorimer@smartthings.com>
2026-02-23 11:39:51 -0600

    hotfix: Removed redundant loading of drivers in can_handle's with multiple loads

--- drivers/SmartThings/zwave-sensor/src/enerwave-motion-sensor/can_handle.lua
+++ drivers/SmartThings/zwave-sensor/src/enerwave-motion-sensor/can_handle.lua
@@ -5,7 +5,7 @@ local function can_handle_enerwave_motion_sensor(opts, driver, device, cmd, ...)
   local ENERWAVE_MFR = 0x011A
   if device.zwave_manufacturer_id == ENERWAVE_MFR then
     local subdriver = require("enerwave-motion-sensor")
-    return true, subdriver, require("enerwave-motion-sensor")
+    return true, subdriver
   else return false end
 end
 
