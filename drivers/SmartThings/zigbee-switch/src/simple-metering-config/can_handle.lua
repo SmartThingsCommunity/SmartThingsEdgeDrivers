b81fa6bfc80b71edc6715725c3c6d2a63a85ca58
jack <j.long1@ledvance.com>
2026-03-04 18:42:13 +0800

    fixing the init.lua multiplier/divisor handle ,fixing the commit conflict

--- drivers/SmartThings/zigbee-switch/src/simple-metering-config/can_handle.lua
+++ drivers/SmartThings/zigbee-switch/src/simple-metering-config/can_handle.lua
@@ -1,7 +1,7 @@
 -- Copyright 2025 SmartThings, Inc.
 -- Licensed under the Apache License, Version 2.0
 
-return function(opts, driver, device, ...)
+return function(opts, driver, device)
   local FINGERPRINTS = require("simple-metering-config.fingerprints")
   for _, fingerprint in ipairs(FINGERPRINTS) do
     if device:get_manufacturer() == fingerprint.mfr and device:get_model() == fingerprint.model then
--- drivers/SmartThings/zigbee-switch/src/simple-metering-config/init.lua
+++ drivers/SmartThings/zigbee-switch/src/simple-metering-config/init.lua
@@ -3,30 +3,7 @@
 
 local capabilities = require "st.capabilities"
 local zigbee_constants = require "st.zigbee.constants"
-local SimpleMetering = require "st.zigbee.cluster".clusters.SimpleMetering
 local configurations = require "configurations"
-
-local function energy_meter_handler(driver, device, value, zb_rx)
-  local raw_value = value.value
-
-  if type(raw_value) ~= "number" or raw_value < 0 then
-    return
-  end
-
-  local divisor = device:get_field(zigbee_constants.SIMPLE_METERING_DIVISOR_KEY) or 100
-  local multiplier = device:get_field(zigbee_constants.SIMPLE_METERING_MULTIPLIER_KEY) or 1
-
-  if divisor == 0 then
-    return
-  end
-
-  local calculated_value = (raw_value * multiplier) / divisor
-
-  device:emit_event_for_endpoint(
-    zb_rx.address_header.src_endpoint.value,
-    capabilities.energyMeter.energy({ value = calculated_value, unit = "kWh" })
-  )
-end
 
 local function device_init(driver, device)
   device:set_field(zigbee_constants.SIMPLE_METERING_MULTIPLIER_KEY, 1, {persist = true})
@@ -38,19 +15,6 @@ local simple_metering_config_subdriver = {
   supported_capabilities = {
     capabilities.energyMeter,
     capabilities.powerMeter
-  },
-  zigbee_handlers = {
-    attr = {
-      [SimpleMetering.ID] = {
-        [SimpleMetering.attributes.CurrentSummationDelivered.ID] = energy_meter_handler,
-        [SimpleMetering.attributes.Multiplier.ID] = function(driver, device, value, zb_rx)
-          device:set_field(zigbee_constants.SIMPLE_METERING_MULTIPLIER_KEY, value.value, {persist = true})
-        end,
-        [SimpleMetering.attributes.Divisor.ID] = function(driver, device, value, zb_rx)
-          device:set_field(zigbee_constants.SIMPLE_METERING_DIVISOR_KEY, value.value, {persist = true})
-        end
-      }
-    }
   },
   lifecycle_handlers = {
     init = configurations.power_reconfig_wrapper(device_init)
