7963cfe24944e6bc4a077fff83ae1d5829a75473
nickolas-deboom <158304111+nickolas-deboom@users.noreply.github.com>
2026-03-09 12:16:16 -0500

    Matter Camera: Support additional setStream attributes (#2786)
    The camera sub-driver lacks handling for the resolution, type, label, and
    viewport aguments of the videoStreamSettings.setStream command. This change
    checks if any of these attributes have changed and deallocates the stream
    before reallocating a new stream with the updated parameters. Additionally, the
    viewport aspect of videoStreamSettings, which was not previously being set
    correctly, is now set based on the DPTZStreams attribute of the CAVSULM
    cluster.

--- drivers/SmartThings/matter-switch/src/sub_drivers/camera/camera_utils/fields.lua
+++ drivers/SmartThings/matter-switch/src/sub_drivers/camera/camera_utils/fields.lua
@@ -13,7 +13,7 @@ CameraFields.SUPPORTED_RESOLUTIONS = "__supported_resolutions"
 CameraFields.MAX_RESOLUTION = "__max_resolution"
 CameraFields.MIN_RESOLUTION = "__min_resolution"
 CameraFields.TRIGGERED_ZONES = "__triggered_zones"
-CameraFields.VIEWPORT = "__viewport"
+CameraFields.DPTZ_VIEWPORTS = "__dptz_viewports"
 
 CameraFields.PAN_IDX = "PAN"
 CameraFields.TILT_IDX = "TILT"
@@ -46,5 +46,19 @@ CameraFields.ABS_ZOOM_MAX = 100
 CameraFields.ABS_ZOOM_MIN = 1
 CameraFields.ABS_VOL_MAX = 254.0
 CameraFields.ABS_VOL_MIN = 0.0
+
+-- Define defaults for allocating new streams. Note that these are the same values use by the hub.
+CameraFields.video_stream_defaults = {
+  codec = clusters.CameraAvStreamManagement.types.VideoCodecEnum.H264,
+  min_frame_rate = 30,
+  max_frame_rate = 60,
+  min_resolution = clusters.CameraAvStreamManagement.types.VideoResolutionStruct({width = 320, height = 240}),
+  max_resolution = clusters.CameraAvStreamManagement.types.VideoResolutionStruct({width = 1920, height = 1080}),
+  min_bitrate = 10000,
+  max_bitrate = 2000000,
+  key_frame_interval = 4000,
+  watermark_enabled = false,
+  on_screen_display_enabled = false
+}
 
 return CameraFields
