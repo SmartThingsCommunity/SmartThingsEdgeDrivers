5eb340c1de23a2b2f462e3ef34d374a4e816db0d
Harrison Carter <137556605+hcarter-775@users.noreply.github.com>
2026-03-24 14:30:22 -0500

    Merge pull request #2812 from SmartThingsCommunity/update/profile-check-logic
    Matter: Improve Profile Checking in infoChanged handler

--- drivers/SmartThings/matter-switch/src/switch_utils/utils.lua
+++ drivers/SmartThings/matter-switch/src/switch_utils/utils.lua
@@ -325,6 +325,50 @@ function utils.create_multi_press_values_list(size, supportsHeld)
   return list
 end
 
+--- Deeply compare two values.
+--- Handles metatables. Can optionally ignore cycle checking and/or function differences.
+---
+--- @param a any
+--- @param b any
+--- @param opts table|nil { ignore_functions = boolean, ignore_cycles = boolean }
+--- @param seen table|nil
+--- @return boolean
+function utils.deep_equals(a, b, opts, seen)
+  if a == b then return true end -- same object
+  if type(a) ~= type(b) then return false end -- different type
+  if type(a) == "function" and opts and opts.ignore_functions then return true end
+  if type(a) ~= "table" then return false end -- same type but not table, thus was already compared
+
+  -- check for cycles in table references and preserve reference topology.
+  if not (opts and opts.ignore_cycles) then
+    seen = seen or {}
+    seen[a] = seen[a] or {}
+    if seen[a][b] then
+      return seen[a][b]
+    end
+    seen[a][b] = true
+  end
+
+  -- Compare keys/values from a
+  for k, v in next, a do
+    if not utils.deep_equals(v, rawget(b, k), opts, seen) then
+      return false
+    end
+  end
+
+  -- Ensure b doesn't have extra keys
+  for k in next, b do
+    if rawget(a, k) == nil then
+      return false
+    end
+  end
+
+  -- Compare metatables
+  local mt_a = getmetatable(a)
+  local mt_b = getmetatable(b)
+  return utils.deep_equals(mt_a, mt_b, opts, seen)
+end
+
 function utils.detect_bridge(device)
   return #utils.get_endpoints_by_device_type(device, fields.DEVICE_TYPE_ID.AGGREGATOR) > 0
 end
