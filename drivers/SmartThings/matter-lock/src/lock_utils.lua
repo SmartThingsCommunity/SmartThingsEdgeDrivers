edfba7e5004731486a88d61024e6657df674294b
HunsupJung <59987061+HunsupJung@users.noreply.github.com>
2026-03-25 23:49:02 +0900

    Support DPS feature (#2750)
    Signed-off-by: Hunsup Jung <hunsup.jung@samsung.com>
    Co-authored-by: Harrison Carter <harrison.carter@smartthings.com>

--- drivers/SmartThings/matter-lock/src/lock_utils.lua
+++ drivers/SmartThings/matter-lock/src/lock_utils.lua
@@ -45,8 +45,7 @@ local lock_utils = {
   COMMAND_REQUEST_ID = "commandRequestId",
   MODULAR_PROFILE_UPDATED = "__MODULAR_PROFILE_UPDATED",
   ALIRO_READER_CONFIG_UPDATED = "aliroReaderConfigUpdated",
-  LATEST_DOOR_LOCK_FEATURE_MAP = "latestDoorLockFeatureMap",
-  IS_MODULAR_PROFILE = "isModularProfile"
+  LATEST_DOOR_LOCK_FEATURE_MAP = "latestDoorLockFeatureMap"
 }
 local capabilities = require "st.capabilities"
 local json = require "st.json"
