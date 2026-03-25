17cc6e26b0db206e8d900b25992824181c53a6e0
HunsupJung <59987061+HunsupJung@users.noreply.github.com>
2026-03-25 11:18:28 +0900

    Support keypair generation (#2815)
    Conflict:
      drivers/SmartThings/matter-lock/src/test/test_matter_lock_modular.lua
    
    Conflict due to a test that is removed by this change.
    
    Signed-off-by: Hunsup Jung <hunsup.jung@samsung.com>

--- drivers/SmartThings/matter-lock/src/test/test_aqara_matter_lock.lua
+++ drivers/SmartThings/matter-lock/src/test/test_aqara_matter_lock.lua
@@ -7,6 +7,7 @@ test.set_rpc_version(0)
 local capabilities = require "st.capabilities"
 local t_utils = require "integration_test.utils"
 local clusters = require "st.matter.clusters"
+local cluster_base = require "st.matter.cluster_base"
 
 local mock_device = test.mock_device.build_test_matter_device({
   profile = t_utils.get_profile_definition("lock-user-pin.yml"),
@@ -42,6 +43,7 @@ local mock_device = test.mock_device.build_test_matter_device({
   }
 })
 
+local DoorLockFeatureMapAttr = {ID = 0xFFFC, cluster = clusters.DoorLock.ID}
 local function test_init()
   test.disable_startup_messages()
   -- subscribe request
@@ -52,6 +54,7 @@ local function test_init()
   subscribe_request:merge(clusters.DoorLock.attributes.MaxPINCodeLength:subscribe(mock_device))
   subscribe_request:merge(clusters.DoorLock.attributes.MinPINCodeLength:subscribe(mock_device))
   subscribe_request:merge(clusters.DoorLock.attributes.RequirePINforRemoteOperation:subscribe(mock_device))
+  subscribe_request:merge(cluster_base.subscribe(mock_device, nil, DoorLockFeatureMapAttr.cluster, DoorLockFeatureMapAttr.ID))
   subscribe_request:merge(clusters.DoorLock.events.LockOperation:subscribe(mock_device))
   subscribe_request:merge(clusters.DoorLock.events.DoorLockAlarm:subscribe(mock_device))
   subscribe_request:merge(clusters.DoorLock.events.LockUserChange:subscribe(mock_device))
