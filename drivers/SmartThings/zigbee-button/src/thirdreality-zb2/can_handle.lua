f642c070f748dc2a15bf642c6bb6b524010d697a
weihuan1111 <99949392+weihuan1111@users.noreply.github.com>
2026-04-10 17:11:58 +0800

    Update can_handle.lua

--- drivers/SmartThings/zigbee-button/src/thirdreality-zb2/can_handle.lua
+++ drivers/SmartThings/zigbee-button/src/thirdreality-zb2/can_handle.lua
@@ -2,11 +2,10 @@
 -- Licensed under the Apache License, Version 2.0
 
 local function thirdreality_zb2_can_handle(opts, driver, device, ...)
-    if device:get_manufacturer() == "Third Reality, Inc" and device:get_model() == "3RSB01085Z" then
-      return true, require("thirdreality-zb2")
-    end
-    return false
+  if device:get_manufacturer() == "Third Reality, Inc" and device:get_model() == "3RSB01085Z" then
+    return true, require("thirdreality-zb2")
   end
-  
-  return thirdreality_zb2_can_handle
-  
+  return false
+end
+
+return thirdreality_zb2_can_handle
