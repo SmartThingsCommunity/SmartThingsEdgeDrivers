a12ad1d940649509b975db813c13b8a80b9e9a13
weihuan <huan.wei@thirdreality.ai>
2026-04-23 08:39:50 +0800

    update
    Change-Id: I926b3ae72f59f37186100a447c64471f45c461dd

--- drivers/SmartThings/zigbee-switch/src/thirdreality-presence-sensor-r3/init.lua
+++ drivers/SmartThings/zigbee-switch/src/thirdreality-presence-sensor-r3/init.lua
@@ -1,7 +1,6 @@
 local zcl_clusters = require "st.zigbee.zcl.clusters"
 local capabilities = require "st.capabilities"
 local OccupancySensing = zcl_clusters.OccupancySensing
-local log = require "log"
 
 local THIRDREALITY_TVOC_CLUSTER = 0x042E
 local THIRDREALITY_TVOC_VALUE = 0x0000
@@ -11,17 +10,7 @@ local function occupancy_attr_handler(driver, device, occupancy, zb_rx)
 end
 
 local function tvoc_attr_handler(driver, device, value, zb_rx)
-    local voc_value
-    if type(value) == "number" then
-        voc_value = value
-    elseif type(value) == "table" and value.mantissa and value.exponent then
-        voc_value = (1 + value.mantissa) * (2 ^ value.exponent)
-    else
-        log.error("Unsupported VOC value: " .. tostring(value))
-        return
-    end
-    voc_value = math.floor(voc_value + 0.5)
-    device:emit_event(capabilities.tvocMeasurement.tvocLevel({value = voc_value, unit = "ppb"}))
+    device:emit_event_for_endpoint(zb_rx.address_header.src_endpoint.value, capabilities.tvocMeasurement.tvocLevel({ value = value.value, unit = "ppb" }))
 end
 
 local added_handler = function(self, device)
