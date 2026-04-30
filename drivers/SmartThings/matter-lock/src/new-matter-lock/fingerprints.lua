972d2750436742af960c44e16811ac31864c64c6
Hunsup Jung <hunsup.jung@samsung.com>
2026-04-30 17:02:15 +0900

    Add Allegion(Schlage) products to allowed list
    Signed-off-by: Hunsup Jung <hunsup.jung@samsung.com>

--- drivers/SmartThings/matter-lock/src/new-matter-lock/fingerprints.lua
+++ drivers/SmartThings/matter-lock/src/new-matter-lock/fingerprints.lua
@@ -34,6 +34,8 @@ local NEW_MATTER_LOCK_PRODUCTS = {
   {0x1421, 0x0042}, -- Kwikset Halo Select Plus
   {0x1421, 0x0081}, -- Kwikset Aura Reach
   {0x1236, 0xa538}, -- Schlage Sense Pro
+  {0x1236, 0x3800}, -- Schlage
+  {0x1236, 0xA738} -- Schlage
 }
 
 return NEW_MATTER_LOCK_PRODUCTS
