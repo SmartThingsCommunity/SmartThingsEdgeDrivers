10148041a44cdd56dbade8606ce6241be7de9cf4
Harrison Carter <harrison.carter@smartthings.com>
2026-05-05 11:52:29 -0500

    update can_handle, consolidate test files

--- drivers/SmartThings/matter-switch/src/sub_drivers/third_reality_garage_door/can_handle.lua
+++ drivers/SmartThings/matter-switch/src/sub_drivers/third_reality_garage_door/can_handle.lua
@@ -1,13 +1,10 @@
 -- Copyright 2026 SmartThings, Inc.
 -- Licensed under the Apache License, Version 2.0
 
-local device_lib = require "st.device"
+local utils = require "switch_utils.utils"
 
 return function(opts, driver, device)
-  local THIRD_REALITY_GARAGE_DOOR_FINGERPRINT = { vendor_id = 0x1407, product_id = 0x1098 }
-  if device.network_type == device_lib.NETWORK_TYPE_MATTER and
-    device.manufacturer_info.vendor_id == THIRD_REALITY_GARAGE_DOOR_FINGERPRINT.vendor_id and
-    device.manufacturer_info.product_id == THIRD_REALITY_GARAGE_DOOR_FINGERPRINT.product_id then
+  if utils.get_product_override_field(device, "is_third_reality_garage_door") then
     return true, require("sub_drivers.third_reality_garage_door")
   end
   return false
