04212d42a1edd83c221d51946cba8b79fa3fdcbe
Woofser <rizner@outlook.com>
2026-05-14 15:03:07 -0500

    Add new fingerprint for Sombra Shades model

--- drivers/SmartThings/zigbee-window-treatment/src/sombra/fingerprints.lua
+++ drivers/SmartThings/zigbee-window-treatment/src/sombra/fingerprints.lua
@@ -3,7 +3,7 @@
 
 local ZIGBEE_WINDOW_SHADE_FINGERPRINTS = {
   { mfr = "Sombra Shades", model = "WM25/L-Z" },
-  { mfr = "Sombra Shades", model = "SS25/L-Z" }
+  { mfr = "Sombra Shades", model = "SOMBRA/Z-M" }
 }
 
 return ZIGBEE_WINDOW_SHADE_FINGERPRINTS
