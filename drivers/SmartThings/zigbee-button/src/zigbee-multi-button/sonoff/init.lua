4cb461d8a5b127e24f558c581c11501d0cacc725
Oniums <237791949@qq.com>
2026-05-18 18:13:36 +0800

    Removed the SONOFF entry from the generic supported values table and moved its button metadata initialization into the SONOFF sub-driver.

--- drivers/SmartThings/zigbee-button/src/zigbee-multi-button/sonoff/init.lua
+++ drivers/SmartThings/zigbee-button/src/zigbee-multi-button/sonoff/init.lua
@@ -2,9 +2,12 @@
 -- Licensed under the Apache License, Version 2.0
 
 local capabilities = require "st.capabilities"
+local button_utils = require "button_utils"
 
 local SONOFF_CLUSTER_ID = 0xFC12
 local SONOFF_ATTR_ID = 0x0000
+local SONOFF_SUPPORTED_BUTTON_VALUES = { "pushed", "double", "held", "pushed_3x" }
+local SONOFF_NUMBER_OF_BUTTONS = 4
 
 local EVENT_MAP = {
   [0x01] = capabilities.button.button.pushed,
@@ -12,6 +15,19 @@ local EVENT_MAP = {
   [0x03] = capabilities.button.button.held,
   [0x04] = capabilities.button.button.pushed_3x
 }
+
+local function added_handler(self, device)
+  for _, component in pairs(device.profile.components) do
+    local number_of_buttons = component.id == "main" and SONOFF_NUMBER_OF_BUTTONS or 1
+    device:emit_component_event(component,
+      capabilities.button.supportedButtonValues(SONOFF_SUPPORTED_BUTTON_VALUES, { visibility = { displayed = false } }))
+    device:emit_component_event(component,
+      capabilities.button.numberOfButtons({ value = number_of_buttons }, { visibility = { displayed = false } }))
+  end
+
+  button_utils.emit_event_if_latest_state_missing(device, "main", capabilities.button,
+    capabilities.button.button.NAME, capabilities.button.button.pushed({ state_change = false }))
+end
 
 local function sonoff_attr_handler(driver, device, value, zb_rx)
   local attr_val = value.value
@@ -30,6 +46,9 @@ end
 
 local sonoff_handler = {
   NAME = "SONOFF Multi-Button Handler",
+  lifecycle_handlers = {
+    added = added_handler
+  },
   zigbee_handlers = {
     attr = {
       [SONOFF_CLUSTER_ID] = {
