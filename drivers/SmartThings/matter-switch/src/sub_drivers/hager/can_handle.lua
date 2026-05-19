eae58ac4ba000dc7148712b22e79185fa505fa37
Jan Jakubiszyn/SmartThings Development (BJ.jakubiszynS) /SRPOL/Engineer/Samsung Electronics <j.jakubiszyn@samsung.com>
2026-05-19 11:58:23 +0200

    Further optimization, dropping redundant custom handlers

--- drivers/SmartThings/matter-switch/src/sub_drivers/hager/can_handle.lua
+++ drivers/SmartThings/matter-switch/src/sub_drivers/hager/can_handle.lua
@@ -2,8 +2,7 @@
 -- Licensed under the Apache License, Version 2.0
 return function(opts, driver, device, ...)
     local device_lib = require "st.device"
-    local fields = require "switch_utils.fields"
-    local vendor_overrides = fields.vendor_overrides
+    local vendor_overrides = require "switch_utils.fields".vendor_overrides
 
     if device.network_type == device_lib.NETWORK_TYPE_CHILD then
         local parent = device:get_parent_device()
@@ -11,7 +10,7 @@ return function(opts, driver, device, ...)
                 and parent.network_type == device_lib.NETWORK_TYPE_MATTER
                 and vendor_overrides[0x1285][parent.manufacturer_info.product_id]
         then
-            return true, require("sub_drivers.Hager")
+            return true, require("sub_drivers.hager")
         end
         return false
     end
@@ -19,7 +18,7 @@ return function(opts, driver, device, ...)
             and device.manufacturer_info.vendor_id == 0x1285
             and vendor_overrides[0x1285][device.manufacturer_info.product_id]
     then
-        return true, require("sub_drivers.Hager")
+        return true, require("sub_drivers.hager")
     end
     return false
 end
