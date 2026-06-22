74a35850a978561ecd81870f42601d95accce793
Harrison Carter <harrison.carter@smartthings.com>
2026-06-22 17:30:27 -0500

    fix indentation issues

--- drivers/SmartThings/matter-sensor/src/sub_drivers/aqara_fp400/can_handle.lua
+++ drivers/SmartThings/matter-sensor/src/sub_drivers/aqara_fp400/can_handle.lua
@@ -2,11 +2,11 @@
 -- Licensed under the Apache License, Version 2.0
 
 local function is_aqara_fp400(opts, driver, device)
-    local sensor_utils = require "sensor_utils.utils"
-    if sensor_utils.get_product_override_field(device, "is_aqara_fp400") then
-        return true, require("sub_drivers.aqara_fp400")
-    end
-    return false
+  local sensor_utils = require "sensor_utils.utils"
+  if sensor_utils.get_product_override_field(device, "is_aqara_fp400") then
+    return true, require("sub_drivers.aqara_fp400")
+  end
+  return false
 end
 
 return is_aqara_fp400
