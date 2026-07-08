bfdd298f6fc6d041b879bdb71f9292d8ff24862d
Hunsup Jung <hunsup.jung@samsung.com>
2026-07-08 14:20:42 +0900

    Revert some part of can_handle
    Signed-off-by: Hunsup Jung <hunsup.jung@samsung.com>

--- drivers/SmartThings/matter-lock/src/unmigrated-matter-lock/can_handle.lua
+++ drivers/SmartThings/matter-lock/src/unmigrated-matter-lock/can_handle.lua
@@ -2,13 +2,12 @@
 -- Licensed under the Apache License, Version 2.0
 
 local capabilities = require "st.capabilities"
-local lock_utils = require "lock_utils"
 
 local function is_unmigrated_matter_lock_product(opts, driver, device)
-  local lock_codes_copy_required = device:get_field(lock_utils.LOCK_CODES_COPY_REQUIRED) or nil
+  local is_migrated = device:get_latest_state("main", capabilities.lockCodes.ID, capabilities.lockCodes.migrated.NAME) or nil
   if device:supports_capability(capabilities.lockAlarm) then
     return false
-  elseif device:supports_capability(capabilities.lockCodes) and lock_codes_copy_required == true then
+  elseif device:supports_capability(capabilities.lockCodes) and is_migrated == true then
     return false
   else
     return true, require("unmigrated-matter-lock")
