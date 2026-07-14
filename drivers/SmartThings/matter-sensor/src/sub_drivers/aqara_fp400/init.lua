5f83682f608b165a3c0be638572f39b8afb828a0
Harrison Carter <harrison.carter@smartthings.com>
2026-07-14 11:11:41 -0500

    add missing component, update comp2ep mapping

--- drivers/SmartThings/matter-sensor/src/sub_drivers/aqara_fp400/init.lua
+++ drivers/SmartThings/matter-sensor/src/sub_drivers/aqara_fp400/init.lua
@@ -4,28 +4,28 @@
 local capabilities = require "st.capabilities"
 local sensor_utils = require "sensor_utils.utils"
 
+local COMPONENT_TO_ENDPOINT_MAP = "__COMPONENT_TO_ENDPOINT_MAP"
+
 local function endpoint_to_component(device, endpoint_id)
-  local ENDPOINT_TO_COMPONENT_MAP = {
-    [3] = "sensor1",
-    [4] = "sensor2",
-    [5] = "sensor3",
-    [6] = "sensor4",
-    [7] = "sensor5",
-    [8] = "sensor6",
-    [9] = "sensor7",
-  }
-  return ENDPOINT_TO_COMPONENT_MAP[endpoint_id] or "main"
+  local map = device:get_field(COMPONENT_TO_ENDPOINT_MAP) or {}
+  for component_id, ep_id in pairs(map) do
+    if ep_id == endpoint_id then return component_id end
+  end
+  return "main"
 end
 
 local function match_profile(device)
   local enabled_optional_component_capabilities = {}
+  local component_to_endpoint_map = {}
   for _, ep in ipairs(device.endpoints) do
     -- since EP0 is the root node, EP1 is a permanent Presence Sensor, and EP2 is a permanent Light Sensor
     if ep.endpoint_id > 2 then
+        component_to_endpoint_map[string.format("sensor%d", ep.endpoint_id - 2)] = ep.endpoint_id
       -- ex. sensor1, sensor2, etc. for EP3, EP4, etc.
       table.insert(enabled_optional_component_capabilities,{ string.format("sensor%d", ep.endpoint_id - 2), { capabilities.presenceSensor.ID } })
     end
   end
+  device:set_field(COMPONENT_TO_ENDPOINT_MAP, component_to_endpoint_map, { persist = true })
   device:try_update_metadata({profile = "aqara-fp400", optional_component_capabilities = enabled_optional_component_capabilities })
 end
 
@@ -44,6 +44,7 @@ end
 function Fp400LifecycleHandlers.info_changed(driver, device, event, args)
   if not sensor_utils.deep_equals(args.old_st_store.endpoints, device.endpoints) then
     match_profile(device)
+    device:subscribe()
   end
 end
 
