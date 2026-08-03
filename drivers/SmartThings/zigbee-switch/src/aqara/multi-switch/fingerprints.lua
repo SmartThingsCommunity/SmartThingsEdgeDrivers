1b273e751a5bfa640829a9805ad2db29e17b53ee
seojune <seojune@aqara.kr>
2026-08-04 08:58:06 +0900

    correct incorrect model information

--- drivers/SmartThings/zigbee-switch/src/aqara/multi-switch/fingerprints.lua
+++ drivers/SmartThings/zigbee-switch/src/aqara/multi-switch/fingerprints.lua
@@ -9,7 +9,7 @@ return {
   { mfr = "LUMI",  model = "lumi.switch.b2laus01", children = 2, child_profile = "aqara-switch-child" },
   { mfr = "LUMI",  model = "lumi.switch.l2aeu1",   children = 2, child_profile = "aqara-switch-child" },
   { mfr = "LUMI",  model = "lumi.switch.n2aeu1",   children = 2, child_profile = "aqara-switch-child" },
-  { mfr = "LUMI",  model = "lumi.switch.b4nacn01", children = 2, child_profile = "aqara-switch-child" },
+  { mfr = "LUMI",  model = "lumi.switch.b2nacn01", children = 2, child_profile = "aqara-switch-child" },
   { mfr = "LUMI",  model = "lumi.switch.b3n01",    children = 3, child_profile = "aqara-switch-child" },
   { mfr = "Aqara", model = "lumi.switch.acn047",   children = 2, child_profile = "aqara-switch-child" }
 }
