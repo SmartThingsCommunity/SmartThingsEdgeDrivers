0909220cb671b6e6ef6e1681457c28307c1eccd2
wzh <1090741189@qq.com>
2026-08-26 10:45:59 +0800

    Load-SNZB-02M-handlers-during-lazy-load

--- drivers/SmartThings/zigbee-humidity-sensor/src/sonoff/SNZB-02M/can_handle.lua
+++ drivers/SmartThings/zigbee-humidity-sensor/src/sonoff/SNZB-02M/can_handle.lua
@@ -2,7 +2,11 @@
 -- Licensed under the Apache License, Version 2.0
 
 local function can_handle(opts, driver, device, ...)
-  return device:get_model() == "SNZB-02M"
+  if device:get_model() == "SNZB-02M" then
+    return true, require("sonoff.SNZB-02M")
+  end
+
+  return false
 end
 
 return can_handle
